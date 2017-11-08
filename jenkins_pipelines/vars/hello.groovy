95b3a47f66cb6a8bab9f4238d2b24b3506af9240
MalloZup <dmaiocchi@suse.com>
2017-11-08 12:15:30 +0100

    experimental

--- jenkins_pipelines/vars/hello.groovy
+++ jenkins_pipelines/vars/hello.groovy
@@ -1,3 +1,7 @@
+#!/usr/bin/env groovy
+
+
+
 // Copyright 2017 SUSE LINUX GmbH, Nuernberg, Germany.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,5 +15,17 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import com.suse.manager
 
+
+class hello implements Serializable {
+    private String name
+    def setName(value) {
+        name = value
+    }
+    def getName() {
+        name
+    }
+    def caution(message) {
+        echo "Hello, ${name}! CAUTION: ${message}"
+    }
+}
