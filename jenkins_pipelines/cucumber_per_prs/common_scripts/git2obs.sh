9883d1e00c13616359edfecdfa29543b9a855112
MalloZup <dmaiocchi@suse.com>
2018-03-12 19:59:58 +0100

    fix script gitpath

--- jenkins_pipelines/cucumber_per_prs/common_scripts/git2obs.sh
+++ jenkins_pipelines/cucumber_per_prs/common_scripts/git2obs.sh
@@ -5,9 +5,9 @@ set -e
 export OSCRC="$HOME/jenkins/.oscrc"
 export OBS_PROJ=$1
 export TEST=1
-
+SPACEWALK="$2/spacewalk*"
 # go and build the packages
-rel-eng/build-packages-for-obs.sh
+$SPACEWALK/rel-eng/build-packages-for-obs.sh
 
 # .. now submitt what's collected in "$WORKSPACE/SRPMS"
-rel-eng/push-packages-to-obs.sh
+$SPACEWALK/rel-eng/push-packages-to-obs.sh
