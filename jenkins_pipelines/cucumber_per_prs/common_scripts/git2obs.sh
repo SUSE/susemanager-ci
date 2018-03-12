9c41a1fa2a2a5aa987da3ac94191e54f29bb6cfe
MalloZup <dmaiocchi@suse.com>
2018-03-12 20:04:00 +0100

    change dir

--- jenkins_pipelines/cucumber_per_prs/common_scripts/git2obs.sh
+++ jenkins_pipelines/cucumber_per_prs/common_scripts/git2obs.sh
@@ -7,7 +7,8 @@ export OBS_PROJ=$1
 export TEST=1
 SPACEWALK="$2/spacewalk*"
 # go and build the packages
-$SPACEWALK/rel-eng/build-packages-for-obs.sh
+cd $SPACEWALK
+rel-eng/build-packages-for-obs.sh
 
 # .. now submitt what's collected in "$WORKSPACE/SRPMS"
-$SPACEWALK/rel-eng/push-packages-to-obs.sh
+rel-eng/push-packages-to-obs.sh
