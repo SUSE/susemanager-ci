b038fa1eddda1c9ee2b280e71aa309886204978a
MalloZup <dmaiocchi@suse.com>
2018-03-20 10:06:45 +0100

    fixup

--- jenkins_pipelines/cucumber_per_prs/common_scripts/change_controlnode.sh
+++ jenkins_pipelines/cucumber_per_prs/common_scripts/change_controlnode.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-PR_BRANCH=$(cat "/.gitarro_vars" | grep "GITARRO_PR_BRANCH:" | cut -d ":" -f2 | xargs)
+WORKSPACE=$3
+PR_BRANCH=$(cat "$WORKSPACE/.gitarro_vars" | grep "GITARRO_PR_BRANCH:" | cut -d ":" -f2 | xargs)
 MAINTF=$1
 BRANCH=$2
 sed -i "s/$BRANCH/$PR_BRANCH/g" $MAINTF
