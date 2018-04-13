0930b676d317b242667cfa05e383e923a9ce7c50
MalloZup <dmaiocchi@suse.com>
2018-04-13 16:25:21 +0200

    use right postfix

--- jenkins_pipelines/manager_prs/tests_files/sql_schema_filename.py
+++ jenkins_pipelines/manager_prs/tests_files/sql_schema_filename.py
@@ -3,20 +3,15 @@
 import os
 import sys
 
-endings = ['.sql', '.sql.postgresql', '.sql.oracle', '.deps']
 spacedirs = ['schema/spacewalk/common/', 'schema/spacewalk/postgres/', 'schema/spacewalk/oracle/', 'schema/spacewalk/upgrade/']
-gitignore = ['.gitignore']
-
 
 for spacedir in spacedirs:
   print("checking sql for directory {0}".format(spacedir))
-  print("")
   for root, dirs, files in os.walk(spacedir):
       for file in files:
-          if file.endswith(gitignore):
-            continue 
-          for postfix in endings:
-              if not file.endswith(postfix):
-                      print("file dont have postfix: {0}".format(postfix))
+          if file.endswith('Makefile') or file.endswith('.gitignore'):
+                    continue
+          if not (file.endswith('.sql') or file.endswith('.sql.postgresql') or file.endswith('.sql.oracle') or file.endswith('.deps')) :
+                      print("following file doesn't have a valid postfix")
                       print(os.path.join(root, file))
                       sys.exit(1)
