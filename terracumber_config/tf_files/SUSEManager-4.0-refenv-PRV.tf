0c6a4ed11a334c0b53f123647b6752eb4ecaf06b
Tino Calancha <ccalancha@suse.com>
2020-06-09 12:01:51 +0200

    Use image sles15sp2 for min-build and proxy
    - terracumber_config/tf_files/SUSEManager-4.0-PRV.tf
    - terracumber_config/tf_files/SUSEManager-4.0-refenv-NUE.tf
    - terracumber_config/tf_files/SUSEManager-4.0-refenv-PRV.tf
    
    This completes 'use sle15sp2 as a build host a retail terminal initial vm'
    (5640b5cc7d0150f96f5e383a2ae346fd8a2b9469)

--- terracumber_config/tf_files/SUSEManager-4.0-refenv-PRV.tf
+++ terracumber_config/tf_files/SUSEManager-4.0-refenv-PRV.tf
@@ -81,7 +81,7 @@ module "base" {
   name_prefix       = "suma-ref40-"
   use_avahi         = false
   domain            = "prv.suse.net"
-  images            = ["centos7", "sles15sp1", "ubuntu1804"]
+  images            = ["centos7", "sles15sp1", "sles15sp2", "ubuntu1804"]
   mirror            = "minima-mirror.prv.suse.net"
   use_mirror_images = true
 
@@ -144,7 +144,7 @@ module "min-build" {
   base_configuration      = module.base.configuration
   product_version         = "4.0-nightly"
   name                    = "min-build"
-  image                   = "sles15sp1"
+  image                   = "sles15sp2"
   server_configuration    = module.srv.configuration
 
   provider_settings = {
