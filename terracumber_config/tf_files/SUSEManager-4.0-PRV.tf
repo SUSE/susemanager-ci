d9972f858293d5003e6f431bd93e5b1905cc9d91
Eric Bischoff <ebischoff@suse.com>
2020-09-12 20:21:02 +0200

    Disable CentOS 7 until salt issue is resolved

--- terracumber_config/tf_files/SUSEManager-4.0-PRV.tf
+++ terracumber_config/tf_files/SUSEManager-4.0-PRV.tf
@@ -152,14 +152,15 @@ module "cucumber_testsuite" {
         mac = "52:54:00:00:00:04"
       }
     }
-    redhat-minion = {
-      image = "centos7o"
-      provider_settings = {
-        mac = "52:54:00:00:00:05"
-        // Openscap cannot run with less than 1.25 GB of RAM
-        memory = 1280
-      }
-    }
+    # WORKAROUND disabled until salt problem is resolved
+    # redhat-minion = {
+    #   image = "centos7o"
+    #   provider_settings = {
+    #     mac = "52:54:00:00:00:05"
+    #     // Openscap cannot run with less than 1.25 GB of RAM
+    #     memory = 1280
+    #   }
+    # }
     debian-minion = {
       provider_settings = {
         mac = "52:54:00:00:00:08"
