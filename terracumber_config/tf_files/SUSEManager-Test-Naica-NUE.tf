4de788ee9dc1424225509c8e81b48382e182af9f
Eric Bischoff <ebischoff@suse.com>
2020-09-12 21:36:21 +0200

    Image sles12sp3o does not exist

--- terracumber_config/tf_files/SUSEManager-Test-Naica-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Test-Naica-NUE.tf
@@ -92,7 +92,7 @@ module "cucumber_testsuite" {
   cc_username = var.SCC_USER
   cc_password = var.SCC_PASSWORD
 
-  images = ["centos7o", "opensuse150o", "sles12sp3o", "sles12sp4o", "ubuntu1804o"]
+  images = ["centos7o", "opensuse150o", "sles12sp3", "sles12sp4o", "ubuntu1804o"]
 
   use_avahi    = false
   name_prefix  = "suma-testnaica-"
@@ -129,14 +129,14 @@ module "cucumber_testsuite" {
       }
     }
     suse-client = {
-      image = "sles12sp3o"
+      image = "sles12sp3"
       name = "cli-sles12"
       provider_settings = {
         mac = "AA:B2:93:00:01:01"
       }
     }
     suse-minion = {
-      image = "sles12sp3o"
+      image = "sles12sp3"
       name = "min-sles12"
       provider_settings = {
         mac = "AA:B2:93:00:01:02"
