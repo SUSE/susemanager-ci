d00da1a500395cab5c1c968eed74ed12f97b3c8e
Oscar Barrios <obarrios@suse.com>
2021-03-12 16:34:03 +0100

    BV: Missing the import of the JSON parameter content

--- jenkins_pipelines/environments/common/pipeline-build-validation.groovy
+++ jenkins_pipelines/environments/common/pipeline-build-validation.groovy
@@ -27,6 +27,8 @@ def run(params) {
                     } else {
                         env.TERRAFORM_INIT = ''
                     }
+                    // Generate json file in the workspace
+                    writeFile file: 'custom_repositories.json', text: params.custom_repositories, encoding: "UTF-8"
                     // Run Terracumber to deploy the environment
                     sh "set +x; source /home/jenkins/.credentials set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; ./terracumber-cli ${common_params} --logfile ${resultdirbuild}/sumaform.log ${env.TERRAFORM_INIT} --taint '.*(domain|main_disk|data_disk|server_extra_nfs_mounts).*' --custom-repositories ${WORKSPACE}/custom_repositories.json --runstep provision"
                     // Generate features
