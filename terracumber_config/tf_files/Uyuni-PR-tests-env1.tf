7dd1d36e41121b63af4563487118cef8d785b90d
Oscar Barrios <obarrios@suse.com>
2021-03-23 10:23:34 +0100

    Preparation of Terraform files for our 6 environments for PR testing

---
+++ terracumber_config/tf_files/Uyuni-PR-tests-env1.tf
@@ -0,0 +1,194 @@
+// Mandatory variables for terracumber
+variable "URL_PREFIX" {
+  type = "string"
+  default = "https://ci.suse.de/view/Manager/view/Uyuni/job/uyuni-pr-tests"
+}
+
+// Not really used as this is for --runall parameter, and we run cucumber step by step
+variable "CUCUMBER_COMMAND" {
+  type = "string"
+  default = "export PRODUCT='Uyuni' && run-testsuite"
+}
+
+variable "CUCUMBER_GITREPO" {
+  type = "string"
+  default = "https://github.com/uyuni-project/uyuni.git"
+}
+
+variable "CUCUMBER_BRANCH" {
+  type = "string"
+  default = "master"
+}
+
+variable "CUCUMBER_RESULTS" {
+  type = "string"
+  default = "/root/spacewalk/testsuite"
+}
+
+variable "MAIL_SUBJECT" {
+  type = "string"
+  default = "Results Uyuni Pull Request $status: $tests scenarios ($failures failed, $errors errors, $skipped skipped, $passed passed)"
+}
+
+variable "MAIL_TEMPLATE" {
+  type = "string"
+  default = "../mail_templates/mail-template-jenkins.txt"
+}
+
+variable "MAIL_SUBJECT_ENV_FAIL" {
+  type = "string"
+  default = "Results Uyuni-Master: Environment setup failed"
+}
+
+variable "MAIL_TEMPLATE_ENV_FAIL" {
+  type = "string"
+  default = "../mail_templates/mail-template-jenkins-env-fail.txt"
+}
+
+variable "MAIL_FROM" {
+  type = "string"
+  default = "galaxy-ci@suse.de"
+}
+
+variable "MAIL_TO" {
+  type = "string"
+  default = null // Must be sent to the author of the Pull Request
+}
+
+// sumaform specific variables
+variable "SCC_USER" {
+  type = "string"
+}
+
+variable "SCC_PASSWORD" {
+  type = "string"
+}
+
+variable "GIT_USER" {
+  type = "string"
+  default = null // Not needed for master, as it is public
+}
+
+variable "GIT_PASSWORD" {
+  type = "string"
+  default = null // Not needed for master, as it is public
+}
+
+provider "libvirt" {
+  uri = "qemu+tcp://romulus.mgr.prv.suse.net/system"
+}
+
+module "cucumber_testsuite" {
+  source = "./modules/cucumber_testsuite"
+
+  product_version = "uyuni-master"
+  
+  // Cucumber repository configuration for the controller
+  git_username = var.GIT_USER
+  git_password = var.GIT_PASSWORD
+  git_repo     = var.CUCUMBER_GITREPO
+  branch       = var.CUCUMBER_BRANCH
+
+  cc_username = var.SCC_USER
+  cc_password = var.SCC_PASSWORD
+
+  images = ["centos7o", "opensuse150o", "opensuse151o", "opensuse152o", "sles15sp1o", "sles15sp2o", "ubuntu2004o"]
+
+  use_avahi    = false
+  name_prefix  = "uyuni-master-"
+  domain       = "mgr.prv.suse.net"
+  from_email   = "root@suse.de"
+
+  no_auth_registry = "registry.mgr.suse.de"
+  auth_registry      = "portus.mgr.suse.de:5000/cucutest"
+  auth_registry_username = "cucutest"
+  auth_registry_password = "cucusecret"
+  git_profiles_repo = "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles/internal_nue"
+
+  server_http_proxy = "galaxy-proxy.mgr.suse.de:3128"
+
+  host_settings = {
+    controller = {
+      provider_settings = {
+        mac = "aa:b2:92:04:00:00"
+      }
+    }
+    server = {
+      provider_settings = {
+        mac = "aa:b2:92:04:00:01"
+      }
+    }
+    proxy = {
+      provider_settings = {
+        mac = "aa:b2:92:04:00:02"
+      }
+    }
+    suse-client = {
+      image = "sles15sp1o"
+      name = "cli-sles15"
+      provider_settings = {
+        mac = "aa:b2:92:04:00:03"
+      }
+    }
+    suse-minion = {
+      image = "sles15sp1o"
+      name = "min-sles15"
+      provider_settings = {
+        mac = "aa:b2:92:04:00:04"
+      }
+    }
+    suse-sshminion = {
+      image = "sles15sp1o"
+      name = "minssh-sles15"
+      provider_settings = {
+        mac = "aa:b2:92:04:00:05"
+      }
+    }
+    redhat-minion = {
+      image = "centos7o"
+      provider_settings = {
+        mac = "aa:b2:92:04:00:06"
+        memory = 2048
+        vcpu = 2
+      }
+    }
+    debian-minion = {
+      name = "min-ubuntu2004"
+      image = "ubuntu2004o"
+      provider_settings = {
+        mac = "aa:b2:92:04:00:08"
+      }
+    }
+    build-host = {
+      image = "sles15sp2o"
+      provider_settings = {
+        mac = "aa:b2:92:04:00:09"
+      }
+    }
+    pxeboot-minion = {
+      image = "sles15sp2o"
+    }
+    kvm-host = {
+      image = "opensuse152o"
+      provider_settings = {
+        mac = "aa:b2:92:04:00:0a"
+      }
+    }
+    xen-host = {
+      image = "opensuse152o"
+      provider_settings = {
+        mac = "aa:b2:92:04:00:0b"
+      }
+    }
+  }
+  provider_settings = {
+    pool               = "ssd"
+    network_name       = null
+    bridge             = "br0"
+    additional_network = "192.168.101.0/24"
+  }
+}
+
+output "configuration" {
+  value = module.cucumber_testsuite.configuration
+}
