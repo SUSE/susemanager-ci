2ef6a13e3b3525325adc624ca116fb2080a18d25
Oscar Barrios <obarrios@suse.com>
2021-03-25 10:19:45 +0100

    PR tests: Use the new node specific for this kind of tests (#217)

--- terracumber_config/tf_files/Uyuni-PR-tests-env6.tf
+++ terracumber_config/tf_files/Uyuni-PR-tests-env6.tf
@@ -1,7 +1,7 @@
 // Mandatory variables for terracumber
 variable "URL_PREFIX" {
   type = "string"
-  default = "https://ci.suse.de/view/Manager/view/Uyuni/job/uyuni-pr-tests"
+  default = "https://ci.suse.de/view/Manager/view/Uyuni/job/uyuni-pr-ci-tests"
 }
 
 // Not really used as this is for --runall parameter, and we run cucumber step by step
