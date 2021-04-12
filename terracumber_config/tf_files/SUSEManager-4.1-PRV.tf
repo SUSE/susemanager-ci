1d54db522e586479a90413bee4860a741c8e7eb4
Julio Gonzalez Gil <jgonzalez@suse.com>
2021-04-12 13:13:22 +0200

    Rename 4.1 jobs

--- terracumber_config/tf_files/SUSEManager-4.1-PRV.tf
+++ terracumber_config/tf_files/SUSEManager-4.1-PRV.tf
@@ -1,7 +1,7 @@
 // Mandatory variables for terracumber
 variable "URL_PREFIX" {
   type = "string"
-  default = "https://ci.suse.de/view/Manager/view/Manager-4.1/job/manager-4.1-cucumber-PRV"
+  default = "https://ci.suse.de/view/Manager/view/Manager-4.1/job/manager-4.1-dev-cucumber-PRV"
 }
 
 // Not really used as this is for --runall parameter, and we run cucumber step by step
