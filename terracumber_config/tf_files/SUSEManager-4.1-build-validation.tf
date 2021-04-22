69f6098cdb6d66f8c42f53bd5f8f5ff0e6fa1881
Konstantinos Tsamis <ktsamis@suse.de>
2021-04-22 15:55:25 +0200

    Change 4.1 BV as well

--- terracumber_config/tf_files/SUSEManager-4.1-build-validation.tf
+++ terracumber_config/tf_files/SUSEManager-4.1-build-validation.tf
@@ -279,7 +279,7 @@ module "server" {
   from_email                     = "root@suse.de"
 
   //server_additional_repos
-  additional_repos = { java11 = "http://download.suse.de/ibs/home:/fstrba:/branches:/SUSE:/SLE-15:/Update/SUSE_SLE-15_Update/" }
+  additional_repos = { java11 = "https://download.opensuse.org/repositories/Java:/Factory/SLE_15_SP2/" }
 }
 
 module "proxy" {
