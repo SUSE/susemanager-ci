c4359436aa83d802302b51543bf6c60d5809b59b
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-06-30 13:39:44 +0200

    force git config for username and email

--- jenkins_pipelines/environments/common/pipeline-pull-request.groovy
+++ jenkins_pipelines/environments/common/pipeline-pull-request.groovy
@@ -48,7 +48,7 @@ def run(params) {
                             env.GIT_AUTHOR_NAME = "jenkins"
                             env.GIT_AUTHOR_EMAIL = "jenkins@a.b"
                             env.GIT_COMMITTER_EMAIL = "jenkins@a.b"
-                            sh "git config user.email "jenkins@a.b"
+                            sh "git config user.email 'jenkins@a.b'"
                             sh "git config user.name 'jenkins'"
                             //TODO: When checking out spacewalk, we will need credentials in the Jenkins Slave
                             //      Inside userRemoteConfigs add credentialsId: 'github'
