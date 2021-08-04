c2223c75c086cdc4b38be840fbdffcb50829ad99
Eric Bischoff <ebischoff@suse.com>
2021-08-04 13:27:20 +0200

    Avoid cucumber being killed by OOM killer

--- terracumber_config/tf_files/SUSEManager-4.1-build-validation.tf
+++ terracumber_config/tf_files/SUSEManager-4.1-build-validation.tf
@@ -1220,7 +1220,7 @@ module "controller" {
   name               = "ctl"
   provider_settings = {
     mac                = "aa:b2:92:42:00:48"
-    memory             = 16384
+    memory             = 24576
     vcpu               = 8
   }
   swap_file_size = null
