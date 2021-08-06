5cee7d90153edbc8b582969932cf2365449452b7
Eric Bischoff <ebischoff@suse.com>
2021-08-06 16:52:06 +0200

    Test SLES 12 SP5 too

--- terracumber_config/tf_files/SUSEManager-4.1-build-validation.tf
+++ terracumber_config/tf_files/SUSEManager-4.1-build-validation.tf
@@ -132,7 +132,7 @@ module "base_old_sle" {
   name_prefix = "suma-bv-41-"
   use_avahi   = false
   domain      = "mgr.prv.suse.net"
-  images      = [ "sles11sp4", "sles12sp4o"/*, "sles12sp5o"*/ ]
+  images      = [ "sles11sp4", "sles12sp4o", "sles12sp5o" ]
 
   // mirror = "minima-mirror-bv.mgr.prv.suse.net"
   // use_mirror_images = true
@@ -360,7 +360,7 @@ module "sles12sp4-client" {
   //sle12sp4-client_additional_repos
 
 }
-/*
+
 module "sles12sp5-client" {
   providers = {
     libvirt = libvirt.endor
@@ -384,7 +384,7 @@ module "sles12sp5-client" {
   //sle12sp5-client_additional_repos
 
 }
-*/
+
 module "sles15-client" {
   providers = {
     libvirt = libvirt.giediprime
@@ -552,7 +552,7 @@ module "sles12sp4-minion" {
   //sle12sp4-minion_additional_repos
 
 }
-/*
+
 module "sles12sp5-minion" {
   providers = {
     libvirt = libvirt.endor
@@ -576,7 +576,7 @@ module "sles12sp5-minion" {
   //sle12sp5-minion_additional_repos
 
 }
-*/
+
 module "sles15-minion" {
   providers = {
     libvirt = libvirt.giediprime
@@ -852,7 +852,7 @@ module "sles12sp4-sshminion" {
   ssh_key_path = "./salt/controller/id_rsa.pub"
   gpg_keys     = ["default/gpg_keys/galaxy.key"]
 }
-/*
+
 module "sles12sp5-sshminion" {
   providers = {
     libvirt = libvirt.endor
@@ -871,7 +871,7 @@ module "sles12sp5-sshminion" {
   ssh_key_path = "./salt/controller/id_rsa.pub"
   gpg_keys     = ["default/gpg_keys/galaxy.key"]
 }
-*/
+
 module "sles15-sshminion" {
   providers = {
     libvirt = libvirt.giediprime
@@ -1248,11 +1248,11 @@ module "controller" {
   sle12sp4_client_configuration    = module.sles12sp4-client.configuration
   sle12sp4_minion_configuration    = module.sles12sp4-minion.configuration
   sle12sp4_sshminion_configuration = module.sles12sp4-sshminion.configuration
-/*
+
   sle12sp5_client_configuration    = module.sles12sp5-client.configuration
   sle12sp5_minion_configuration    = module.sles12sp5-minion.configuration
   sle12sp5_sshminion_configuration = module.sles12sp5-sshminion.configuration
-*/
+
 /* TODO do we need the following ones at all? - ebischoff
 */
   client_configuration    = module.sles12sp4-client.configuration
