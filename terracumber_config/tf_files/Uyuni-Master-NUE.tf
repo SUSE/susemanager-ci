caced91bf5c85b69cac96a0c0bbb28809bd60b96
Konstantinos Tsamis <ktsamis@suse.de>
2021-09-13 15:48:21 +0200

    Temporarily add experimental java for testing

--- terracumber_config/tf_files/Uyuni-Master-NUE.tf
+++ terracumber_config/tf_files/Uyuni-Master-NUE.tf
@@ -116,6 +116,9 @@ module "cucumber_testsuite" {
     server = {
       provider_settings = {
         mac = "aa:b2:93:01:00:d1"
+        additional_repos = {
+            java_TEST = "https://download.suse.de/ibs/home:/fstrba:/branches:/SUSE:/SLE-15:/Update/SUSE_SLE-15_Update/x86_64/"
+        }
       }
     }
     proxy = {
