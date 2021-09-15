faed7cc4d283f85e43f5569b1380cd8900d1d144
Konstantinos Tsamis <ktsamis@suse.de>
2021-09-15 13:30:19 +0200

    Fix additional_repos in tf files

--- terracumber_config/tf_files/SUSEManager-Head-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Head-NUE.tf
@@ -116,9 +116,9 @@ module "cucumber_testsuite" {
     server = {
       provider_settings = {
         mac = "aa:b2:93:01:00:b1"
-        additional_repos = {
-            java_TEST = "http://download.suse.de/ibs/home:/fstrba:/branches:/SUSE:/SLE-15:/Update/SUSE_SLE-15_Update/"
-        }
+      }
+      additional_repos = {
+        java_TEST = "http://download.suse.de/ibs/home:/fstrba:/branches:/SUSE:/SLE-15:/Update/SUSE_SLE-15_Update/"
       }
     }
     proxy = {
