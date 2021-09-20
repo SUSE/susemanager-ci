f02afdac50e32cb72c275335568dce0ee8110539
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-09-20 13:30:04 +0200

    fix controller image
    We are not downloading opensuse152o, so better use the 15.3 image we use
    for clients.
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- terracumber_config/tf_files/Uyuni-PR-tests-env5.tf
+++ terracumber_config/tf_files/Uyuni-PR-tests-env5.tf
@@ -138,6 +138,7 @@ module "cucumber_testsuite" {
       provider_settings = {
         mac = "aa:b2:92:04:00:30"
       }
+      image = "opensuse153-ci-pr-client"
     }
     server = {
       provider_settings = {
