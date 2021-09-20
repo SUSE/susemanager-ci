d8c1339e13d5ec57b65770faa9e3c0c3d79e1b84
Oscar Barrios <obarrios@suse.com>
2021-09-20 10:20:46 +0200

    Update pipeline.groovy

--- jenkins_pipelines/environments/common/pipeline.groovy
+++ jenkins_pipelines/environments/common/pipeline.groovy
@@ -1,6 +1,6 @@
 def run(params) {
     timestamps {
-        environment_workspace = env.JOB_BASE_NAME
+        environment_workspace = "workspace/${env.JOB_BASE_NAME}"
         // Retrieve the hash commit of the last product built in OBS/IBS and previous job
         def prefix = env.JOB_BASE_NAME.split('-acceptance-tests')[0]
         if (prefix == "uyuni-master-dev") {
