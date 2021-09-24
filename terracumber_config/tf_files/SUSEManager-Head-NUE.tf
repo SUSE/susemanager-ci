c200d90cd164002e9eaee178edf96d31ae562df1
Oscar Barrios <obarrios@suse.com>
2021-09-24 13:42:07 +0200

    Remove an additional repo for Java bugfix

--- terracumber_config/tf_files/SUSEManager-Head-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Head-NUE.tf
@@ -117,9 +117,6 @@ module "cucumber_testsuite" {
       provider_settings = {
         mac = "aa:b2:93:01:00:b1"
       }
-      additional_repos = {
-        java_TEST = "http://download.suse.de/ibs/home:/fstrba:/branches:/SUSE:/SLE-15:/Update/SUSE_SLE-15_Update/"
-      }
     }
     proxy = {
       provider_settings = {
