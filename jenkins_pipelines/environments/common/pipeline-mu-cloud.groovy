b132256fe69a6bdf4660b713b48100e580c5d2e7
Maxime Noel <maxime.noel@suse.com>
2021-09-29 18:37:25 +1300

    Test minima creation

--- jenkins_pipelines/environments/common/pipeline-mu-cloud.groovy
+++ jenkins_pipelines/environments/common/pipeline-mu-cloud.groovy
@@ -35,8 +35,7 @@ def run(params) {
                         "    archs: [x86_64]"
             }
             writeFile file: "${env.resultdir}/sumaform/salt/mirror/etc/minima-customize.yaml", text: env.repositories, encoding: "UTF-8"
-            writeFile file: 'results/minima-customize.yaml', text: env.repositories, encoding: "UTF-8"
-//            sh "set +x; source /home/jenkins/.credentials set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; ./terracumber-cli ${common_params} --logfile ${resultdirbuild}/sumaform.log ${env.TERRAFORM_INIT} --taint '.*(domain|main_disk).*' --runstep provision"
+            sh "set +x; source /home/jenkins/.credentials set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; ./terracumber-cli ${common_params} --logfile ${resultdirbuild}/sumaform.log ${env.TERRAFORM_INIT} --taint '.*(domain|main_disk).*' --runstep provision"
             deployed = true
 
         }
