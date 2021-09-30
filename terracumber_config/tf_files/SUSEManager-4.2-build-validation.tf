3b77199735a6cacca210daadfc92e1f96c6c6b4f
Eric Bischoff <ebischoff@suse.com>
2021-09-30 19:18:43 +0200

    Use tunnel in bridge mode -> we can use DHCP

--- terracumber_config/tf_files/SUSEManager-4.2-build-validation.tf
+++ terracumber_config/tf_files/SUSEManager-4.2-build-validation.tf
@@ -1220,7 +1220,7 @@ module "opensuse153arm-minion" {
   name               = "min-opensuse153arm"
   image              = "opensuse153armo"
   provider_settings = {
-    mac                = "52:54:00:6e:5e:ed"
+    mac                = "aa:b2:92:42:00:ad"
     memory             = 2048
     vcpu               = 2
     xslt               = <<EOT
