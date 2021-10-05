e65508dbef7d275156010e9a004608cb68dac151
Maxime Noel <maxime.noel@suse.com>
2021-10-05 13:53:43 +1300

    Chnage upload method

--- jenkins_pipelines/environments/common/pipeline-mu-cloud.groovy
+++ jenkins_pipelines/environments/common/pipeline-mu-cloud.groovy
@@ -74,6 +74,7 @@ def run(params) {
             remote.name = 'local_mirror'
             remote.user = 'root'
             remote.password = 'linux'
+            sh "hostname"
             sh "scp -o StrictHostKeyChecking=no /home/jenkins/.ssh/testing-suma.pem ${remote.user}@${mirror_hostname_local}:/root/"
             sh "ssh -o StrictHostKeyChecking=no ${remote.user}@${mirror_hostname_local} 'chmod 0400 /root/testing-suma.pem'"
             sh "ssh -o StrictHostKeyChecking=no ${remote.user}@${mirror_hostname_local} 'scp -R -i /root/testing-suma.pem /srv/mirror ec2-user@${mirror_hostname_aws}:/srv/mirror' "
