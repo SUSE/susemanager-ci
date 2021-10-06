e8a341d06e367994360180725463841c8308fd1c
Maxime Noel <maxime.noel@suse.com>
2021-10-06 16:46:25 +1300

    Fix space error

--- jenkins_pipelines/environments/common/pipeline-mu-cloud.groovy
+++ jenkins_pipelines/environments/common/pipeline-mu-cloud.groovy
@@ -78,7 +78,7 @@ def run(params) {
             sh "users"
             sh "scp -o StrictHostKeyChecking=no /home/jenkins/.ssh/testing-suma.pem ${remote.user}@${mirror_hostname_local}:/root/"
             sh "ssh -o StrictHostKeyChecking=no ${remote.user}@${mirror_hostname_local} 'chmod 0400 /root/testing-suma.pem'"
-            sh "ssh -o StrictHostKeyChecking=no ${remote.user}@${mirror_hostname_local} 'scp -R -i /root/testing-suma.pem /srv/mirror ec2-user@${mirror_hostname_aws}:/srv/mirror' "
+            sh "ssh -o StrictHostKeyChecking=no ${remote.user}@${mirror_hostname_local} 'scp -r -i /root/testing-suma.pem /srv/mirror ec2-user@${mirror_hostname_aws}:/srv/mirror' "
         }
     }
 }
