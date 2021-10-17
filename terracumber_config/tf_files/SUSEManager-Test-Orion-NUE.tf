f2a02cbcdbea51a38b2e87a80f5c73eb1e1aecc2
Michael Calmer <mc@suse.de>
2021-10-17 10:51:17 +0200

    change ubuntu image version

--- terracumber_config/tf_files/SUSEManager-Test-Orion-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Test-Orion-NUE.tf
@@ -93,7 +93,7 @@ module "cucumber_testsuite" {
   cc_username = var.SCC_USER
   cc_password = var.SCC_PASSWORD
 
-  images = ["centos7o", "opensuse152o", "opensuse153o", "sles15sp2o", "sles15sp3o", "ubuntu1804o"]
+  images = ["centos7o", "opensuse152o", "opensuse153o", "sles15sp2o", "sles15sp3o", "ubuntu2004o"]
   //images = ["centos7o", "opensuse152o",                 "sles15sp2o", "sles15sp3o", "ubuntu2004o"]
 
   use_avahi    = false
