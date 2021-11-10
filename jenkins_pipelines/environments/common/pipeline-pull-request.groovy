14aa88eaae4d161506b040276ee8acaf5735cc8e
Julio Gonzalez Gil <jgonzalez@suse.com>
2021-11-10 10:59:55 +0100

    Use terracumber from GitHub

--- jenkins_pipelines/environments/common/pipeline-pull-request.groovy
+++ jenkins_pipelines/environments/common/pipeline-pull-request.groovy
@@ -8,7 +8,7 @@ def run(params) {
         terraform_bin_plugins = '/usr/bin'
         long_tests = true
         service_pack_migration = false
-        terracumber_gitrepo = 'https://gitlab.suse.de/juliogonzalezgil/terracumber.git'
+        terracumber_gitrepo = 'https://github.com/uyuni-project/terracumber.git'
         terracumber_ref = 'master'
         terraform_init = true
         rake_namespace = 'cucumber'
