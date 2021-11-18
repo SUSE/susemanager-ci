35a0300b2da1763ce5110fd79aea355f068681bf
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-11-18 15:49:06 +0100

    Add email to the description of the job
    This way it is easier to identify which is yours
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- jenkins_pipelines/environments/common/pipeline-pull-request.groovy
+++ jenkins_pipelines/environments/common/pipeline-pull-request.groovy
@@ -87,7 +87,7 @@ def run(params) {
             }
             stage('Build product') {
                 ws(environment_workspace){
-                    currentBuild.description =  "${builder_project}:${params.pull_request_number}<br>${params.functional_scopes}"
+                    currentBuild.description =  "${builder_project}:${params.pull_request_number}<br>${params.email_to}<br>${params.functional_scopes}"
                     if(params.must_build) {
                         dir("product") {
                             sh "[ -L /home/jenkins/jenkins-build/workspace/suma-pr${env_number}/repos ] || ln -s /storage/jenkins/repos/${env_number}/ /home/jenkins/jenkins-build/workspace/suma-pr/${env_number}/repos"
