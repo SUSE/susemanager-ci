1b23f2c5ded5912be0e3c681f4196581af956c50
Eric Bischoff <ebischoff@suse.com>
2021-11-23 20:40:48 +0100

    Put the NFS mounts script in common

--- terracumber_config/tf_files/SUSEManager-4.2-build-validation.tf
+++ terracumber_config/tf_files/SUSEManager-4.2-build-validation.tf
@@ -1324,30 +1324,7 @@ module "controller" {
 
 resource "null_resource" "server_extra_nfs_mounts" {
   provisioner "remote-exec" {
-    inline = [
-      "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repo/$RCE/RES7  /mirror/repo/$RCE/RES7  nfs  defaults  0 0' >> /etc/fstab",
-      "mount '/mirror/repo/$RCE/RES7'",
-      "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repo/$RCE/RES7-SUSE-Manager-Tools  /mirror/repo/$RCE/RES7-SUSE-Manager-Tools  nfs  defaults  0 0' >> /etc/fstab",
-      "mount '/mirror/repo/$RCE/RES7-SUSE-Manager-Tools'",
-      "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repo/$RCE/RES7-SUSE-Manager-Tools-Beta  /mirror/repo/$RCE/RES7-SUSE-Manager-Tools-Beta  nfs  defaults  0 0' >> /etc/fstab",
-      "mount '/mirror/repo/$RCE/RES7-SUSE-Manager-Tools-Beta'",
-      "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/SUSE/Updates/RES  /mirror/SUSE/Updates/RES  nfs  defaults  0 0' >> /etc/fstab",
-      "mount '/mirror/SUSE/Updates/RES'",
-      "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/SUSE/Updates/RES-CB  /mirror/SUSE/Updates/RES-CB  nfs  defaults  0 0' >> /etc/fstab",
-      "mount '/mirror/SUSE/Updates/RES-CB'",
-      "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/SUSE/Updates/RES-AS  /mirror/SUSE/Updates/RES-AS  nfs  defaults  0 0' >> /etc/fstab",
-      "mount '/mirror/SUSE/Updates/RES-AS'",
-      "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/SUSE/Products/RES  /mirror/SUSE/Products/RES  nfs  defaults  0 0' >> /etc/fstab",
-      "mount '/mirror/SUSE/Products/RES'",
-      "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS7-Uyuni-Client-Tools/CentOS_7  /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS7-Uyuni-Client-Tools/CentOS_7  nfs  defaults  0 0' >> /etc/fstab",
-      "mount /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS7-Uyuni-Client-Tools/CentOS_7",
-      "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS8-Uyuni-Client-Tools/CentOS_8  /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS8-Uyuni-Client-Tools/CentOS_8  nfs  defaults  0 0' >> /etc/fstab",
-      "mount /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS8-Uyuni-Client-Tools/CentOS_8",
-      "echo 'minima-mirror-bv3.mgr.prv.suse.net:/srv/mirror/distribution/leap/15.3  /mirror/distribution/leap/15.3  nfs  defaults  0 0' >> /etc/fstab",
-      "mount /mirror/distribution/leap/15.3",
-      "echo 'minima-mirror-bv3.mgr.prv.suse.net:/srv/mirror/repositories/systemsmanagement:/sumaform:/tools/openSUSE_Leap_15.3  /mirror/repositories/systemsmanagement:/sumaform:/tools/openSUSE_Leap_15.3  nfs  defaults  0 0' >> /etc/fstab",
-      "mount /mirror/repositories/systemsmanagement:/sumaform:/tools/openSUSE_Leap_15.3"
-    ]
+    script = "./common/nfs-mounts.sh"
     connection {
       type     = "ssh"
       user     = "root"
