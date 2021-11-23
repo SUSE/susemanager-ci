eb14962d524697c7d18b460b7ebf5465b3941126
Eric Bischoff <ebischoff@suse.com>
2021-11-23 20:32:49 +0100

    More NFS mounts

--- terracumber_config/tf_files/SUSEManager-4.2-build-validation.tf
+++ terracumber_config/tf_files/SUSEManager-4.2-build-validation.tf
@@ -1338,7 +1338,15 @@ resource "null_resource" "server_extra_nfs_mounts" {
       "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/SUSE/Updates/RES-AS  /mirror/SUSE/Updates/RES-AS  nfs  defaults  0 0' >> /etc/fstab",
       "mount '/mirror/SUSE/Updates/RES-AS'",
       "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/SUSE/Products/RES  /mirror/SUSE/Products/RES  nfs  defaults  0 0' >> /etc/fstab",
-      "mount '/mirror/SUSE/Products/RES'"
+      "mount '/mirror/SUSE/Products/RES'",
+      "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS7-Uyuni-Client-Tools/CentOS_7  /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS7-Uyuni-Client-Tools/CentOS_7  nfs  defaults  0 0' >> /etc/fstab",
+      "mount /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS7-Uyuni-Client-Tools/CentOS_7",
+      "echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS8-Uyuni-Client-Tools/CentOS_8  /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS8-Uyuni-Client-Tools/CentOS_8  nfs  defaults  0 0' >> /etc/fstab",
+      "mount /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS8-Uyuni-Client-Tools/CentOS_8",
+      "echo 'minima-mirror-bv3.mgr.prv.suse.net:/srv/mirror/distribution/leap/15.3  /mirror/distribution/leap/15.3  nfs  defaults  0 0' >> /etc/fstab",
+      "mount /mirror/distribution/leap/15.3",
+      "echo 'minima-mirror-bv3.mgr.prv.suse.net:/srv/mirror/repositories/systemsmanagement:/sumaform:/tools/openSUSE_Leap_15.3  /mirror/repositories/systemsmanagement:/sumaform:/tools/openSUSE_Leap_15.3  nfs  defaults  0 0' >> /etc/fstab",
+      "mount /mirror/repositories/systemsmanagement:/sumaform:/tools/openSUSE_Leap_15.3"
     ]
     connection {
       type     = "ssh"
