83111cec26202317a44c6f5653ffbcd78ff6d439
Eric Bischoff <ebischoff@suse.com>
2021-11-25 15:35:36 +0100

    Two new exports, and used loops

--- terracumber_config/tf_files/common/nfs-mounts.sh
+++ terracumber_config/tf_files/common/nfs-mounts.sh
@@ -1,34 +1,22 @@
 #! /bin/bash
 
-echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repo/$RCE/RES7  /mirror/repo/$RCE/RES7  nfs  defaults  0 0' >> /etc/fstab
-mount '/mirror/repo/$RCE/RES7'
+for dir in 'repo/$RCE/RES7' \
+           'repo/$RCE/RES7-SUSE-Manager-Tools' \
+           'repo/$RCE/RES7-SUSE-Manager-Tools-Beta' \
+           'SUSE/Updates/RES' \
+           'SUSE/Updates/RES-CB' \
+           'SUSE/Updates/RES-AS' \
+           'SUSE/Products/RES' \
+           'repositories/systemsmanagement:/Uyuni:/Stable:/CentOS7-Uyuni-Client-Tools/CentOS_7' \
+           'repositories/systemsmanagement:/Uyuni:/Stable:/CentOS8-Uyuni-Client-Tools/CentOS_8'; do
+  echo "minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/$dir  /mirror/$dir  nfs  defaults  0 0" >> /etc/fstab
+  mount "/mirror/$dir"
+done
 
-echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repo/$RCE/RES7-SUSE-Manager-Tools  /mirror/repo/$RCE/RES7-SUSE-Manager-Tools  nfs  defaults  0 0' >> /etc/fstab
-mount '/mirror/repo/$RCE/RES7-SUSE-Manager-Tools'
-
-echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repo/$RCE/RES7-SUSE-Manager-Tools-Beta  /mirror/repo/$RCE/RES7-SUSE-Manager-Tools-Beta  nfs  defaults  0 0' >> /etc/fstab
-mount '/mirror/repo/$RCE/RES7-SUSE-Manager-Tools-Beta'
-
-echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/SUSE/Updates/RES  /mirror/SUSE/Updates/RES  nfs  defaults  0 0' >> /etc/fstab
-mount '/mirror/SUSE/Updates/RES'
-
-echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/SUSE/Updates/RES-CB  /mirror/SUSE/Updates/RES-CB  nfs  defaults  0 0' >> /etc/fstab
-mount '/mirror/SUSE/Updates/RES-CB'
-
-echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/SUSE/Updates/RES-AS  /mirror/SUSE/Updates/RES-AS  nfs  defaults  0 0' >> /etc/fstab
-mount '/mirror/SUSE/Updates/RES-AS'
-
-echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/SUSE/Products/RES  /mirror/SUSE/Products/RES  nfs  defaults  0 0' >> /etc/fstab
-mount '/mirror/SUSE/Products/RES'
-
-echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS7-Uyuni-Client-Tools/CentOS_7  /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS7-Uyuni-Client-Tools/CentOS_7  nfs  defaults  0 0' >> /etc/fstab
-mount /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS7-Uyuni-Client-Tools/CentOS_7
-
-echo 'minima-mirror-bv2.mgr.prv.suse.net:/srv/mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS8-Uyuni-Client-Tools/CentOS_8  /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS8-Uyuni-Client-Tools/CentOS_8  nfs  defaults  0 0' >> /etc/fstab
-mount /mirror/repositories/systemsmanagement:/Uyuni:/Stable:/CentOS8-Uyuni-Client-Tools/CentOS_8
-
-echo 'minima-mirror-bv3.mgr.prv.suse.net:/srv/mirror/distribution/leap/15.3  /mirror/distribution/leap/15.3  nfs  defaults  0 0' >> /etc/fstab
-mount /mirror/distribution/leap/15.3
-
-echo 'minima-mirror-bv3.mgr.prv.suse.net:/srv/mirror/repositories/systemsmanagement:/sumaform:/tools/openSUSE_Leap_15.3  /mirror/repositories/systemsmanagement:/sumaform:/tools/openSUSE_Leap_15.3  nfs  defaults  0 0' >> /etc/fstab
-mount /mirror/repositories/systemsmanagement:/sumaform:/tools/openSUSE_Leap_15.3
+for dir in 'distribution/leap/15.3' \
+           'repositories/systemsmanagement:/sumaform:/tools/openSUSE_Leap_15.3' \
+           'SUSE/Updates/SLE-Manager-Tools/15/aarch64' \
+           'SUSE/Updates/SLE-Manager-Tools/15-BETA/aarch64'; do
+  echo "minima-mirror-bv3.mgr.prv.suse.net:/srv/mirror/$dir  /mirror/$dir  nfs  defaults  0 0" >> /etc/fstab
+  mount "/mirror/$dir"
+done
