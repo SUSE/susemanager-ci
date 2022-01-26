337cdc51a8e6cadcbef1fd00160442a7ea86c18c
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2022-01-26 15:18:37 +0100

    Use default storage pools on those hypervisors without ssd

--- terracumber_config/tf_files/Uyuni-PR-tests-env9.tf
+++ terracumber_config/tf_files/Uyuni-PR-tests-env9.tf
@@ -304,7 +304,7 @@ module "cucumber_testsuite" {
     }
   }
   provider_settings = {
-    pool               = "ssd"
+    pool               = "default"
     network_name       = null
     bridge             = "br1"
     additional_network = "192.168.109.0/24"
