5690ee8c53c0e7caf8a5496e7bbe2e95ecb68a1d
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2022-02-11 16:24:34 +0000

    TEST: Set custom branch for server to get testing Salt package

--- terracumber_config/tf_files/SUSEManager-Test-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Test-NUE.tf
@@ -135,7 +135,7 @@ module "cucumber_testsuite" {
 //        salt15sp2_server_apps_module = "http://download.suse.de/ibs/SUSE:/Maintenance:/17878/SUSE_Updates_SLE-Module-Server-Applications_15-SP2_x86_64/",
 //        sapformular = "http://download.suse.de/ibs/SUSE:/Maintenance:/17953/SUSE_Updates_SLE-Module-SUSE-Manager-Server_4.1_x86_64/",
 //        hwdata = "http://download.suse.de/ibs/SUSE:/Maintenance:/17927/SUSE_Updates_SLE-Module-SUSE-Manager-Server_4.1_x86_64/",
-       Test_repo = "http://download.suse.de/ibs/home:/PSuarezHernandez:/branches:/Devel:/Galaxy:/Manager:/Head:/Other/SLE_15_SP4/"
+       Test_repo = "http://download.suse.de/ibs/home:/PSuarezHernandez:/branches:/Devel:/Galaxy:/Manager:/Head/SLE_15_SP4/"
       }
     }
     proxy = {
