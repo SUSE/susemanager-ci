581828d4b42d957ab39d885f7c055782fc34c5dc
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2022-02-11 16:41:52 +0000

    TEST: Make TEST to use head instead of 4.2 as product

--- terracumber_config/tf_files/SUSEManager-Test-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Test-NUE.tf
@@ -91,7 +91,7 @@ provider "libvirt" {
 module "cucumber_testsuite" {
   source = "./modules/cucumber_testsuite"
 
-  product_version = "4.2-released"
+  product_version = "head"
 
   // Cucumber repository configuration for the controller
   git_username = var.GIT_USER
