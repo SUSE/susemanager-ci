9374ac45fe6fe03eac4234105906a2d169ee917e
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2022-02-15 16:13:37 +0000

    TEST: Increase server memory to 10GB and fix typo on repo

--- terracumber_config/tf_files/SUSEManager-Test-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Test-NUE.tf
@@ -127,6 +127,7 @@ module "cucumber_testsuite" {
     server = {
       provider_settings = {
         mac = "aa:b2:93:01:00:41"
+        memory = 10240
       }
       additional_repos = {
         server_stack = "http://download.suse.de/ibs/Devel:/Galaxy:/Manager:/TEST/SLE_15_SP4/"
@@ -135,7 +136,7 @@ module "cucumber_testsuite" {
 //        salt15sp2_server_apps_module = "http://download.suse.de/ibs/SUSE:/Maintenance:/17878/SUSE_Updates_SLE-Module-Server-Applications_15-SP2_x86_64/",
 //        sapformular = "http://download.suse.de/ibs/SUSE:/Maintenance:/17953/SUSE_Updates_SLE-Module-SUSE-Manager-Server_4.1_x86_64/",
 //        hwdata = "http://download.suse.de/ibs/SUSE:/Maintenance:/17927/SUSE_Updates_SLE-Module-SUSE-Manager-Server_4.1_x86_64/",
-        Test_repo = "https://download.suse.de/ibs/Devel:/Galaxy:/Manager:/salt-testing/SLE_15_SP3/"
+        Test_repo = "https://download.suse.de/ibs/Devel:/Galaxy:/Manager:/salt-testing/SLE_15_SP4/"
       }
     }
     proxy = {
