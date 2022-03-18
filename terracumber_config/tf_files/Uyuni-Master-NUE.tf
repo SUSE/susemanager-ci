f6221aaed186af5ee7e28ae6e2992609ae759bdd
Michael Calmer <mc@suse.de>
2022-03-18 09:39:20 +0100

    use mirror for uyuni-master

--- terracumber_config/tf_files/Uyuni-Master-NUE.tf
+++ terracumber_config/tf_files/Uyuni-Master-NUE.tf
@@ -115,6 +115,10 @@ module "cucumber_testsuite" {
   auth_registry_password = "cucusecret"
   git_profiles_repo = "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles/internal_nue"
 
+  // Comment the next two lines if no mirror should be used
+  mirror = "minima-mirror.mgr.suse.de"
+  use_mirror_images = true
+
   server_http_proxy = "http-proxy.mgr.suse.de:3128"
 
   host_settings = {
