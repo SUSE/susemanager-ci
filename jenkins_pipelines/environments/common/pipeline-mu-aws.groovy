75f4fd0aad774d6d6c6a31b08916a96a092cdabb
maximenoel8 <55169628+maximenoel8@users.noreply.github.com>
2022-05-11 19:06:39 +1200

    Add ami support to AWS testing (#523)
    Add possibility to deploy with build image on AWS

--- jenkins_pipelines/environments/common/pipeline-mu-aws.groovy
+++ jenkins_pipelines/environments/common/pipeline-mu-aws.groovy
@@ -9,6 +9,12 @@ def run(params) {
         junit_resultdir = "results/${BUILD_NUMBER}/results_junit"
         local_mirror_dir = "${resultdir}/sumaform-local"
         aws_mirror_dir = "${resultdir}/sumaform-aws"
+        awscli = '/usr/local/bin/aws'
+        suma43_build_url = "https://dist.suse.de/ibs/SUSE:/SLE-15-SP4:/Update:/Products:/Manager43/images/"
+
+        server_ami = null
+        proxy_ami = null
+
 
         //Deployment variables
         deployed_local = false
@@ -37,54 +43,98 @@ def run(params) {
             sh "set +x; source /home/jenkins/.credentials set -x; ./terracumber-cli ${aws_common_params} --gitrepo ${params.sumaform_gitrepo} --gitref ${params.sumaform_ref} --runstep gitsync --sumaform-backend aws"
         }
 
+
         parallel(
-                "create_local_mirror_with_mu": {
-                    stage("Create local mirror with MU") {
-                        writeFile file: "custom_repositories.json", text: params.custom_repositories, encoding: "UTF-8"
-                        mu_repositories = sh(script: "cat ${WORKSPACE}/custom_repositories.json | jq -r ' to_entries[] |  \" \\(.value)\"' | jq -r ' to_entries[] |  \" \\(.value)\"'",
+
+            "upload_latest_image": {
+                if (params.use_latest_ami_image) {
+                    stage('Clean old images') {
+                        // Get all image ami ids
+                        image_amis = sh(script: "${awscli} ec2 describe-images --filters 'Name=name,Values=SUSE-Manager-*-BYOS*' --region ${params.aws_region} | jq -r '.Images[].ImageId'",
                                 returnStdout: true)
-                        String[] REPOSITORIES_LIST = mu_repositories.split("\n")
-                        // Create simplify minima file to only synchronize MU
-                        repositories = "storage:\n" +
-                                "  type: file\n" +
-                                "  path: /srv/mirror\n" +
-                                "\n" +
-                                "http:"
-                        REPOSITORIES_LIST.each { item ->
-                            repositories = "${repositories}\n\n" +
-                                    "  - url: ${item}\n" +
-                                    "    archs: [x86_64]"
+                        // Get all snapshot ids
+                        image_snapshots = sh(script: "${awscli} ec2 describe-images --filters 'Name=name,Values=SUSE-Manager-*-BYOS*' --region ${params.aws_region} | jq -r '.Images[].BlockDeviceMappings[0].Ebs.SnapshotId'",
+                                returnStdout: true)
+
+                        String[] ami_list = image_amis.split("\n")
+                        String[] snapshot_list = image_snapshots.split("\n")
+
+                        // Deregister all BYOS images
+                        ami_list.each { ami ->
+                            if (ami) {
+                                sh(script: "${awscli} ec2 deregister-image --image-id ${ami} --region ${params.aws_region}")
+                            }
                         }
-                        writeFile file: "${local_mirror_dir}/salt/mirror/etc/minima-customize.yaml", text: repositories, encoding: "UTF-8"
-
-                        // Deploy local mirror
-                        sh "set +x; source /home/jenkins/.credentials set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; ./terracumber-cli ${local_mirror_params} --logfile ${resultdirbuild}/sumaform-local.log ${TERRAFORM_INIT} --taint '.*(domain|main_disk).*' --runstep provision --sumaform-backend libvirt"
-                        deployed_local = true
-
+                        // Delete all BYOS snapshot
+                        snapshot_list.each { snapshot ->
+                            if (snapshot) {
+                                sh(script: "${awscli} ec2 delete-snapshot --snapshot-id ${snapshot} --region ${params.aws_region}")
+                            }
+                        }
                     }
-                },
-                "create_empty_aws_mirror": {
-                    stage("Create empty AWS mirror") {
-                        env.aws_configuration = "REGION = \"${params.aws_region}\"\n" +
-                                "AVAILABILITY_ZONE = \"${params.aws_availability_zone}\"\n" +
-                                "NAME_PREFIX = \"${env.JOB_NAME}-\"\n" +
-                                "ALLOWED_IPS = [ \n"
 
-                        ALLOWED_IPS.each { ip ->
-                            env.aws_configuration = aws_configuration + "    \"${ip}\",\n"
-                        }
-
-                        env.aws_configuration = aws_configuration + "]\n"
-                        writeFile file: "${aws_mirror_dir}/terraform.tfvars", text: aws_configuration, encoding: "UTF-8"
-
-                        // Deploy empty AWS mirror
-                        sh "set +x; source /home/jenkins/.credentials set -x; source /home/jenkins/.aws set -x;source /home/jenkins/.registration set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; ./terracumber-cli ${aws_mirror_params} --logfile ${resultdirbuild}/sumaform-aws.log ${TERRAFORM_INIT} --taint '.*(domain|main_disk).*' --runstep provision --sumaform-backend aws"
-                        deployed_aws = true
-
+                    stage('Download last ami image') {
+                        sh "rm -rf ${resultdir}/images"
+                        sh "mkdir -p ${resultdir}/images"
+                        server_image_name = sh(script: "curl ${suma43_build_url} | grep -oP '<a href=\".+?\">\\K.+?(?=<)' | grep 'SUSE-Manager-Server-BYOS.*raw.xz\$'",
+                                returnStdout: true).trim()
+                        proxy_image_name = sh(script: "curl ${suma43_build_url} | grep -oP '<a href=\".+?\">\\K.+?(?=<)' | grep 'SUSE-Manager-Proxy-BYOS.*raw.xz\$'",
+                                returnStdout: true).trim()
+                        sh(script: "cd ${resultdir}/images; wget https://dist.suse.de/ibs/SUSE:/SLE-15-SP4:/Update:/Products:/Manager43/images/${server_image_name}")
+                        sh(script: "cd ${resultdir}/images; wget https://dist.suse.de/ibs/SUSE:/SLE-15-SP4:/Update:/Products:/Manager43/images/${proxy_image_name}")
+                        sh(script: "ec2uploadimg -f /home/jenkins/.ec2utils.conf -a test --backing-store ssd --machine 'x86_64' --virt-type hvm --sriov-support --ena-support --verbose --regions '${params.aws_region}' -d 'build_suma_server' --wait-count 3 -n '${server_image_name}' '${resultdir}/images/${server_image_name}'")
+                        sh(script: "ec2uploadimg -f /home/jenkins/.ec2utils.conf -a test --backing-store ssd --machine 'x86_64' --virt-type hvm --sriov-support --ena-support --verbose --regions '${params.aws_region}' -d 'build_suma_proxy' --wait-count 3 -n '${proxy_image_name}' '${resultdir}/images/${proxy_image_name}'")
+                        env.server_ami = sh(script: "${awscli} ec2 describe-images --filters 'Name=name,Values=${server_image_name}' --region ${params.aws_region}| jq -r '.Images[0].ImageId'",
+                                returnStdout: true).trim()
+                        env.proxy_ami = sh(script: "${awscli} ec2 describe-images --filters 'Name=name,Values=${proxy_image_name}' --region ${params.aws_region} | jq -r '.Images[0].ImageId'",
+                                returnStdout: true).trim()
                     }
                 }
-        )
+            },
+            "create_local_mirror_with_mu": {
+                stage("Create local mirror with MU") {
+                    writeFile file: "custom_repositories.json", text: params.custom_repositories, encoding: "UTF-8"
+                    mu_repositories = sh(script: "cat ${WORKSPACE}/custom_repositories.json | jq -r ' to_entries[] |  \" \\(.value)\"' | jq -r ' to_entries[] |  \" \\(.value)\"'",
+                            returnStdout: true)
+                    String[] REPOSITORIES_LIST = mu_repositories.split("\n")
+                    // Create simplify minima file to only synchronize MU
+                    repositories = "storage:\n" +
+                            "  type: file\n" +
+                            "  path: /srv/mirror\n" +
+                            "\n" +
+                            "http:"
+                    REPOSITORIES_LIST.each { item ->
+                        repositories = "${repositories}\n\n" +
+                                "  - url: ${item}\n" +
+                                "    archs: [x86_64]"
+                    }
+                    writeFile file: "${local_mirror_dir}/salt/mirror/etc/minima-customize.yaml", text: repositories, encoding: "UTF-8"
 
+                    // Deploy local mirror
+                    sh "set +x; source /home/jenkins/.credentials set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; ./terracumber-cli ${local_mirror_params} --logfile ${resultdirbuild}/sumaform-local.log ${TERRAFORM_INIT} --taint '.*(domain|main_disk).*' --runstep provision --sumaform-backend libvirt"
+                    deployed_local = true
+
+                }
+            },
+            "create_empty_aws_mirror": {
+                stage("Create empty AWS mirror") {
+                    env.aws_configuration = "REGION = \"${params.aws_region}\"\n" +
+                            "AVAILABILITY_ZONE = \"${params.aws_availability_zone}\"\n" +
+                            "NAME_PREFIX = \"${env.JOB_NAME}-\"\n" +
+                            "ALLOWED_IPS = [ \n"
+
+                    ALLOWED_IPS.each { ip ->
+                        env.aws_configuration = aws_configuration + "    \"${ip}\",\n"
+                    }
+                    env.aws_configuration = aws_configuration + "]\n"
+                    writeFile file: "${aws_mirror_dir}/terraform.tfvars", text: aws_configuration, encoding: "UTF-8"
+                    // Deploy empty AWS mirror
+                    sh "set +x; source /home/jenkins/.credentials set -x; source /home/jenkins/.aws/.aws set -x;source /home/jenkins/.registration set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; ./terracumber-cli ${aws_mirror_params} --logfile ${resultdirbuild}/sumaform-aws.log ${TERRAFORM_INIT} --taint '.*(domain|main_disk).*' --runstep provision --sumaform-backend aws"
+                    deployed_aws = true
+
+                }
+            }
+        )
 
         stage("Upload local mirror data to AWS mirror") {
 
@@ -115,7 +165,8 @@ def run(params) {
             sh "sed -r 's/ibs\\///g' ${WORKSPACE}/custom_repositories.json"
 
             // Deploying AWS server using MU repositories
-            sh "set +x; source /home/jenkins/.credentials set -x; source /home/jenkins/.aws set -x; source /home/jenkins/.registration set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TF_VAR_MIRROR=${env.mirror_hostname_aws_private}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; ./terracumber-cli ${aws_common_params} --logfile ${resultdirbuild}/sumaform-aws.log ${TERRAFORM_INIT} --taint '.*(domain|main_disk).*'  --custom-repositories ${WORKSPACE}/custom_repositories.json --runstep provision --sumaform-backend aws --bastion_ssh_key /home/jenkins/.ssh/testing-suma.pem"
+            sh "echo \"set +x; source /home/jenkins/.credentials set -x; source /home/jenkins/.aws/.aws set -x; source /home/jenkins/.registration set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TF_VAR_MIRROR=${env.mirror_hostname_aws_private}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; export TF_VAR_SERVER_AMI=${env.server_ami}; export TF_VAR_PROXY_AMI=${env.proxy_ami}; ./terracumber-cli ${aws_common_params} --logfile ${resultdirbuild}/sumaform-aws.log ${TERRAFORM_INIT} --taint '.*(domain|main_disk).*' --runstep provision --sumaform-backend aws --bastion_ssh_key /home/jenkins/.ssh/testing-suma.pem\""
+            sh "set +x; source /home/jenkins/.credentials set -x; source /home/jenkins/.aws/.aws set -x; source /home/jenkins/.registration set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TF_VAR_MIRROR=${env.mirror_hostname_aws_private}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; export TF_VAR_SERVER_AMI=${env.server_ami}; export TF_VAR_PROXY_AMI=${env.proxy_ami}; ./terracumber-cli ${aws_common_params} --logfile ${resultdirbuild}/sumaform-aws.log ${TERRAFORM_INIT} --taint '.*(domain|main_disk).*' --runstep provision --sumaform-backend aws --bastion_ssh_key /home/jenkins/.ssh/testing-suma.pem"
         }
     }
 }
