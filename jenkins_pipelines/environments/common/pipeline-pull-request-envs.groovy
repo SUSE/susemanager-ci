7fa65a2023e4afde60302d7600b64d50995b2eb0
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2022-05-23 09:54:36 +0200

    reserve 10 for jordi dev

--- jenkins_pipelines/environments/common/pipeline-pull-request-envs.groovy
+++ jenkins_pipelines/environments/common/pipeline-pull-request-envs.groovy
@@ -1,5 +1,5 @@
 if (env.JOB_NAME == "uyuni-prs-ci-tests-jordi") {
-    first_env = 9;
+    first_env = 10;
     last_env = 10;
     // if you change the sumaform repo or reference, you need to remove the sumaform directory from the results folder
     sumaform_gitrepo = "https://github.com/jordimassaguerpla/sumaform.git";
@@ -9,7 +9,7 @@ if (env.JOB_NAME == "uyuni-prs-ci-tests-jordi") {
         email_to = "aaaaeoayla72kj6blracdlufr4@suse.slack.com";
         pull_request_number = "master";
         first_env = 9;
-        last_env = 10;
+        last_env = 9;
         additional_repo_url = "http://minima-mirror.mgr.prv.suse.net/jordi/reference_job_additional_repo";
     } else { //not jordi, not reference
         first_env = 1;
