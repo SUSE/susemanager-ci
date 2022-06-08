22fb1f16e84d98eae0536a528593081dfbd4a050
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2022-06-08 11:21:52 +0200

    Revert "fix syntax on flaky test exclusion"
    This is creating and empty list. We need to debug this better.
    
    This reverts commit bc2499b7433ba36d07b2990e7b55f23d7ba58d22.

--- jenkins_pipelines/environments/common/pipeline-pull-request.groovy
+++ jenkins_pipelines/environments/common/pipeline-pull-request.groovy
@@ -281,9 +281,9 @@ def run(params) {
                     if(must_test && ( params.functional_scopes || params.run_all_scopes) ) {
                         def exports = ""
                         if (params.functional_scopes){
-                          exports += "export TAGS=${params.functional_scopes}, not @flaky; "
+                          exports += "export TAGS=${params.functional_scopes},~@flaky; "
                         } else {
-                          exports += "export TAGS=not @flaky; "
+                          exports += "export TAGS=~@flaky; "
                         }
                         def statusCode1 = sh script:"./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd '${exports} cd /root/spacewalk/testsuite; rake cucumber:secondary'", returnStatus:true
                         def statusCode2 = sh script:"./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd '${exports} cd /root/spacewalk/testsuite; rake ${rake_namespace}:secondary_parallelizable'", returnStatus:true
