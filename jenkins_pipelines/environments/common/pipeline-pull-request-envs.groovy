63f9883ab31013cc533d038fb250a6485f86fc35
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2022-07-15 11:46:28 +0200

    Remove secondary jobs for Pull Requests
    The experiment ended and we do not need to reserve environments for
    this. Let's get 7 and 8 envs back to the "pool" for the user jobs.
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- jenkins_pipelines/environments/common/pipeline-pull-request-envs.groovy
+++ jenkins_pipelines/environments/common/pipeline-pull-request-envs.groovy
@@ -11,22 +11,6 @@ if (env.JOB_NAME == "uyuni-prs-ci-tests-jordi") {
     first_env = 9;
     last_env = 9;
     additional_repo_url = "http://minima-mirror.mgr.prv.suse.net/jordi/reference_job_additional_repo";
-} else if (env.JOB_NAME == "uyuni-prs-ci-tests-reference-secondary") {
-    email_to = "aaaaeoayla72kj6blracdlufr4@suse.slack.com";
-    run_all_scopes = true;
-    first_env = 8;
-    last_env = 8;
-    cucumber_gitrepo = "https://github.com/uyuni-project/uyuni.git";
-    cucumber_ref = "remove_failing_tests";
-    pull_request_number = "5662";
-} else if (env.JOB_NAME == "uyuni-prs-ci-tests-reference-secondary-full") {
-    email_to = "aaaaeoayla72kj6blracdlufr4@suse.slack.com";
-    run_all_scopes = true;
-    first_env = 7;
-    last_env = 7;
-    cucumber_gitrepo = "https://github.com/uyuni-project/uyuni.git";
-    cucumber_ref = "remove_flaky_tags";
-    pull_request_number = "5663";
 } else if (env.JOB_NAME == "uyuni-prs-ci-tests-qe") {
     first_env = 6;
     last_env = 6;
