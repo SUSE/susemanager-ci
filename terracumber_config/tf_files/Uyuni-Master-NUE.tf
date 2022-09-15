02a0ae9954eee174eca5675d0dc60fc8211ea087
Eric Bischoff <ebischoff@suse.com>
2022-09-15 16:19:22 +0200

    Forgot custom download endpoint for Uyuni

--- terracumber_config/tf_files/Uyuni-Master-NUE.tf
+++ terracumber_config/tf_files/Uyuni-Master-NUE.tf
@@ -120,6 +120,7 @@ module "cucumber_testsuite" {
   use_mirror_images = true
 
   server_http_proxy = "http-proxy.mgr.suse.de:3128"
+  custom_download_endpoint = "ftp://minima-mirror.mgr.suse.de:445"
 
   host_settings = {
     controller = {
