4474f8309f02ef8ca29c358660ccd833daa0796e
Michael Calmer <mc@suse.de>
2022-10-17 14:47:52 +0200

    add opensuse154o image for controller node to missing tf files

--- terracumber_config/tf_files/Uyuni-PR-tests-env1.tf
+++ terracumber_config/tf_files/Uyuni-PR-tests-env1.tf
@@ -161,7 +161,7 @@ module "cucumber_testsuite" {
   mirror      = "minima-mirror.mgr.prv.suse.net"
   use_mirror_images = true
 
-  images = ["centos7o", "opensuse152o", "opensuse154-ci-pr", "sles15sp4o", "ubuntu2204o"]
+  images = ["centos7o", "opensuse152o", "opensuse154o", "opensuse154-ci-pr", "sles15sp4o", "ubuntu2204o"]
 
   use_avahi    = false
   name_prefix  = "suma-pr${var.ENVIRONMENT}-"
