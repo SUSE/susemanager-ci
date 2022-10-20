5c49caf591a9fb7d6ab6b3e82943278c8d0b3c69
Dominik Gedon <dgedon@suse.de>
2022-10-20 11:33:48 +0200

    Add note for updating the image matrix to config
    This will add a note to the configuration files to remind the developers
    to adjust the image matrix as well when changing/replacing images.
    
    Signed-off-by: Dominik Gedon <dgedon@suse.de>

--- terracumber_config/tf_files/Uyuni-Master-NUE.tf
+++ terracumber_config/tf_files/Uyuni-Master-NUE.tf
@@ -122,6 +122,7 @@ module "cucumber_testsuite" {
   server_http_proxy = "http-proxy.mgr.suse.de:3128"
   custom_download_endpoint = "ftp://minima-mirror.mgr.suse.de:445"
 
+  # when changing images, please also keep in mind to adjust the image matrix at the end of the README.
   host_settings = {
     controller = {
       provider_settings = {
