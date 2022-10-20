ee6177089299fdb0c4435e7f1f2d0a203d21aa07
Dominik Gedon <dgedon@suse.de>
2022-10-20 11:33:48 +0200

    Add note for updating the image matrix to config
    This will add a note to the configuration files to remind the developers
    to adjust the image matrix as well when changing/replacing images.
    
    Signed-off-by: Dominik Gedon <dgedon@suse.de>

--- terracumber_config/tf_files/SUSEManager-4.3-PRV.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-PRV.tf
@@ -121,6 +121,7 @@ module "cucumber_testsuite" {
   server_http_proxy = "http-proxy.mgr.prv.suse.net:3128"
   custom_download_endpoint = "ftp://minima-mirror.mgr.prv.suse.net:445"
 
+  # when changing images, please also keep in mind to adjust the image matrix at the end of the README.
   host_settings = {
     controller = {
       provider_settings = {
