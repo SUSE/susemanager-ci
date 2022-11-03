93b4d7de46c8f0183657b5fd46578f4503ff577b
Dominik Gedon <dgedon@suse.de>
2022-11-03 13:47:32 +0100

    Add Leap 15.4 to 4.3 BV base module
    Signed-off-by: Dominik Gedon <dgedon@suse.de>

--- terracumber_config/tf_files/SUSEManager-4.3-build-validation.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-build-validation.tf
@@ -121,7 +121,7 @@ module "base_core" {
   name_prefix = "suma-bv-43-"
   use_avahi   = false
   domain      = "mgr.prv.suse.net"
-  images      = [ "sles15sp3o", "sles15sp4o" ]
+  images      = [ "sles15sp3o", "sles15sp4o", "opensuse154o" ]
 
   mirror = "minima-mirror-bv.mgr.prv.suse.net"
   use_mirror_images = true
