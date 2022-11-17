335394f3fc9b47f6d1ebbc52784eb0692d0df182
Konstantinos Tsamis <ktsamis@suse.de>
2022-11-17 15:47:38 +0100

    Switch again to bloodymary

--- terracumber_config/tf_files/SUSEManager-4.3-PRV.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-PRV.tf
@@ -87,7 +87,7 @@ terraform {
 provider "libvirt" {
 // Switching temporarily because metropolis is down
 //  uri = "qemu+tcp://metropolis.mgr.prv.suse.net/system"
-  uri = "qemu+tcp://hugo.mgr.prv.suse.net/system"
+  uri = "qemu+tcp://bloodymary.mgr.prv.suse.net/system"
 }
 
 
