4db4ca60640f34c686a4bcaf778a5bb20ac6e08e
Eric Bischoff <ebischoff@suse.com>
2023-01-10 17:40:25 +0100

    An leftover of renaming from QA to QE

--- terracumber_config/tf_files/SUSEManager-4.2-build-validation.tf
+++ terracumber_config/tf_files/SUSEManager-4.2-build-validation.tf
@@ -1,7 +1,7 @@
 // Mandatory variables for terracumber
 variable "URL_PREFIX" {
   type = string
-  default = "https://ci.suse.de/view/Manager/view/Manager-4.2/job/manager-4.2-qa-build-validation"
+  default = "https://ci.suse.de/view/Manager/view/Manager-4.2/job/manager-4.2-qe-build-validation"
 }
 
 // Not really used as this is for --runall parameter, and we run cucumber step by step
