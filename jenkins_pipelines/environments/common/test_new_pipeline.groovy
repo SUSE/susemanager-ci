63369e989584bc8e8928b024ee58e36833860939
Maxime Noel <maxime.noel@suse.com>
2023-01-13 10:56:48 +1300

    add new pipeline

--- jenkins_pipelines/environments/common/test_new_pipeline.groovy
+++ jenkins_pipelines/environments/common/test_new_pipeline.groovy
@@ -15,10 +15,10 @@ def doDynamicParallelSteps(){
     list = ["Test-1", "Test-2", "Test-3", "Test-4", "Test-5"]
     tests = [:]
     for (element in list) {
-        tests["${f}"] = {
+        tests["${element}"] = {
             node {
-                stage("${f}") {
-                    echo '${f}'
+                stage("${element}") {
+                    echo '${element}'
                 }
             }
         }
