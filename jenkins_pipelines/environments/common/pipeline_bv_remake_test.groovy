e7d43b4f506f3ecedc300cad84284b8ce52b58aa
Maxime Noel <maxime.noel@suse.com>
2023-01-13 09:29:52 +1300

    add env variable check

--- jenkins_pipelines/environments/common/pipeline_bv_remake_test.groovy
+++ jenkins_pipelines/environments/common/pipeline_bv_remake_test.groovy
@@ -4,12 +4,25 @@ def run(params) {
         list = ["Test-1", "Test-2", "Test-3", "Test-4", "Test-5"]
 
         stage('1') {
+            minions = sh(script: "printenv | grep MINION",
+                    returnStdout: true)
+            sshminion = sh(script: "printenv | grep SSHMINION",
+                    returnStdout: true)
+            client = sh(script: "printenv | grep CLIENT",
+                    returnStdout: true)
+            String[] minion_list = minions.split("\n")
+            String[] sshminion_list = sshminion.split("\n")
+            String[] client_list = client.split("\n")
+            def node_list = []
             def tests = [:]
-            list.each{f ->
-                tests["${f}"] = {
+            node_list.addAll(minion_list,sshminion_list,client_list)
+            node_list.each{ element ->
+                tests["${element}"] = {
                     node {
-                        stage("${f}") {
-                            echo "${f}"
+                        stage("${element}") {
+                            echo "${element}"
+
+
                         }
                     }
                 }
