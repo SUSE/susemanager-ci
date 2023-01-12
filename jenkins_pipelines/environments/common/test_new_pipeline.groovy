e284176a9115b02163c2d086c4c1025fc48cf4f7
Maxime Noel <maxime.noel@suse.com>
2023-01-13 10:59:04 +1300

    reformate pipeline

--- jenkins_pipelines/environments/common/test_new_pipeline.groovy
+++ jenkins_pipelines/environments/common/test_new_pipeline.groovy
@@ -12,9 +12,9 @@ pipeline{
 }
 
 def doDynamicParallelSteps(){
-    list = ["Test-1", "Test-2", "Test-3", "Test-4", "Test-5"]
+    def list_value = ["Test-1", "Test-2", "Test-3", "Test-4", "Test-5"]
     tests = [:]
-    for (element in list) {
+    for (element in list_value ) {
         tests["${element}"] = {
             node {
                 stage("${element}") {
