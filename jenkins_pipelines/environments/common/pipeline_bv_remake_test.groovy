d7a86bb320aebbdc122a637c4f2f7c8038fc9494
Maxime Noel <maxime.noel@suse.com>
2023-01-17 11:33:39 +1300

    New pipeline

--- jenkins_pipelines/environments/common/pipeline_bv_remake_test.groovy
+++ jenkins_pipelines/environments/common/pipeline_bv_remake_test.groovy
@@ -2,40 +2,85 @@ def run(params) {
 
     timestamps {
         list = ["Test-1", "Test-2", "Test-3", "Test-4", "Test-5"]
-        def tests = [:]
-        stage('Dynamic parallel') {
-            minions = sh(script: "source /home/maxime/.profile; printenv | grep MINION || exit 0",
-                    returnStdout: true)
-            sshminion = sh(script: "source /home/maxime/.profile; printenv | grep SSHMINION || exit 0",
-                    returnStdout: true)
-            client = sh(script: "source /home/maxime/.profile; printenv | grep CLIENT || exit 0",
-                    returnStdout: true)
-            String[] minion_list = minions.split("\n")
-            String[] sshminion_list = sshminion.split("\n")
-            String[] client_list = client.split("\n")
-            echo minion_list.join(", ")
-            echo sshminion_list.join(", ")
-            echo client_list.join(", ")
-
-            def node_list = [minion_list, sshminion_list, client_list].flatten().findAll{it}
-            echo node_list.join(", ")
-            parallel(
-                    dynamic: {
-                        for (element in node_list) {
-                            minion = element.split("=")[0].toLowerCase()
-                            echo minion
-                            stage("job-${minion}") {
-                                echo minion
-                                sh "echo ${minion}"
-
-
-                            }
-                        }
-                    }
-            )
-        }
-
+//        stage('Dynamic parallel') {
+//            minions = sh(script: "source /home/maxime/.profile; printenv | grep MINION || exit 0",
+//                    returnStdout: true)
+//            sshminion = sh(script: "source /home/maxime/.profile; printenv | grep SSHMINION || exit 0",
+//                    returnStdout: true)
+//            client = sh(script: "source /home/maxime/.profile; printenv | grep CLIENT || exit 0",
+//                    returnStdout: true)
+//            String[] minion_list = minions.split("\n")
+//            String[] sshminion_list = sshminion.split("\n")
+//            String[] client_list = client.split("\n")
+//            echo minion_list.join(", ")
+//            echo sshminion_list.join(", ")
+//            echo client_list.join(", ")
+//
+//            def node_list = [minion_list, sshminion_list, client_list].flatten().findAll { it }
+//            echo node_list.join(", ")
+//
+//            node_list.each { element ->
+//                minion = element.split("=")[0].toLowerCase()
+//                tests["job-${minion}"] = {
+//                    stage("${minion}") {
+//                        echo minion
+//                        sh "echo ${minion}"
+//                    }
+//                }
+////                        parallel(
+////                    dynamic: {
+////                        for (element in node_list) {
+////                            minion = element.split("=")[0].toLowerCase()
+////                            echo minion
+////                            stage("job-${minion}") {
+////                                echo minion
+////                                sh "echo ${minion}"
+////
+////
+////                            }
+////                        }
+////                    }
+////                        )
+//                parallel tests
+//
+//            }
+//
+//        }
+        stage('CI')
+                {
+                    doDynamicParallelSteps()
+                }
     }
+
 }
 
+def doDynamicParallelSteps(){
+    def tests = [:]
+    minions = sh(script: "source /home/maxime/.profile; printenv | grep MINION || exit 0",
+            returnStdout: true)
+    sshminion = sh(script: "source /home/maxime/.profile; printenv | grep SSHMINION || exit 0",
+            returnStdout: true)
+    client = sh(script: "source /home/maxime/.profile; printenv | grep CLIENT || exit 0",
+            returnStdout: true)
+    String[] minion_list = minions.split("\n")
+    String[] sshminion_list = sshminion.split("\n")
+    String[] client_list = client.split("\n")
+    echo minion_list.join(", ")
+    echo sshminion_list.join(", ")
+    echo client_list.join(", ")
+
+    def node_list = [minion_list, sshminion_list, client_list].flatten().findAll { it }
+    echo node_list.join(", ")
+
+    node_list.each { element ->
+        minion = element.split("=")[0].toLowerCase()
+        tests["job-${minion}"] = {
+            stage("${minion}") {
+                echo minion
+                sh "echo ${minion}"
+            }
+        }
+    }
+    parallel tests
+}
 return this
