85dfa2874b60327432717c77d9a94f0ea2327023
Maxime Noel <maxime.noel@suse.com>
2023-01-18 10:30:24 +1300

    Use mian.tf to get minions

--- jenkins_pipelines/environments/common/pipeline_bv_remake_test.groovy
+++ jenkins_pipelines/environments/common/pipeline_bv_remake_test.groovy
@@ -57,15 +57,17 @@ def run(params) {
 
 def doDynamicParallelSteps(){
     def tests = [:]
-    def Set<String> nodeList = new HashSet<String>()
+    Set<String> nodeList = new HashSet<String>()
     modules = sh(script: "cd /home/maxime/jenkinsslave/workspace/SUSEManager-4.3-AWS-build-validation/results/sumaform-aws; terraform state list",
             returnStdout: true)
     String[] moduleList = modules.split("\n")
     echo moduleList.join(", ")
     moduleList.each {lane->
-        def instance = lane.split(".")[1]
-        if (instance.contain(minion) || instance.contain(client)) {
-            echo instance
+        String[] instanceList = lane.split(".")
+        echo instanceList.join(", ")
+        echo instanceList[0]
+
+        if (instanceList.contain(minion) || instanceList.contain(client)) {
             nodeList.add(instance)
         }
     }
