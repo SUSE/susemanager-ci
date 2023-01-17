92fee85ae4d3ab8d379979d3310de54b36d5bd45
Maxime Noel <maxime.noel@suse.com>
2023-01-18 10:55:03 +1300

    Use mian.tf to get minions

--- jenkins_pipelines/environments/common/pipeline_bv_remake_test.groovy
+++ jenkins_pipelines/environments/common/pipeline_bv_remake_test.groovy
@@ -64,9 +64,7 @@ def doDynamicParallelSteps(){
     moduleList.each {lane->
         echo lane
         def instanceList = lane.tokenize(".")
-        echo instanceList[1]
-        echo instanceList[1].contain(minion)
-        if (instanceList[1].contain(minion) || instanceList[1].contain(client)) {
+        if (instanceList[1].contain('minion') || instanceList[1].contain('client')) {
             echo instanceList[1]
             nodeList.add(instanceList[1])
 
