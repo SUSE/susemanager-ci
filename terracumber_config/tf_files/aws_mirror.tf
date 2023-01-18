274b192e542b0211410be515641b186e8a0b0d83
Maxime Noel <maxime.noel@suse.com>
2023-01-19 10:31:54 +1300

    Add back ACCESS key variable, ssh key are not share between account

--- terracumber_config/tf_files/aws_mirror.tf
+++ terracumber_config/tf_files/aws_mirror.tf
@@ -133,8 +133,8 @@ variable "NAME_PREFIX" {
 
 provider "aws" {
   region = var.REGION
-//  access_key = var.ACCESS_KEY
-//  secret_key = var.SECRET_KEY
+  access_key = var.ACCESS_KEY
+  secret_key = var.SECRET_KEY
 //  token      = var.TOKEN_AWS
 }
 
