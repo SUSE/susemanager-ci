5a8f99cd5fc6c1db0c6f03f12b5ce6ab4ae38079
Maxime Noel <maxime.noel@suse.com>
2023-01-19 10:59:13 +1300

    Push AWS load variable

--- jenkins_pipelines/environments/common/pipeline-mu-aws.groovy
+++ jenkins_pipelines/environments/common/pipeline-mu-aws.groovy
@@ -165,7 +165,7 @@ def run(params) {
                         env.aws_configuration = aws_configuration + "]\n"
                         writeFile file: "${aws_mirror_dir}/terraform.tfvars", text: aws_configuration, encoding: "UTF-8"
                         // Deploy empty AWS mirror
-                        sh "set +x; source /home/jenkins/.credentials set -x;source /home/jenkins/.registration set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; ./terracumber-cli ${aws_mirror_params} --logfile ${resultdirbuild}/sumaform-mirror-aws.log ${TERRAFORM_INIT} --taint '.*(domain|main_disk).*' --runstep provision --sumaform-backend aws"
+                        sh "set +x; source /home/jenkins/.credentials set -x; source /home/jenkins/.aws/.aws set -x; source /home/jenkins/.registration set -x; export TF_VAR_CUCUMBER_GITREPO=${params.cucumber_gitrepo}; export TF_VAR_CUCUMBER_BRANCH=${params.cucumber_ref}; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; ./terracumber-cli ${aws_mirror_params} --logfile ${resultdirbuild}/sumaform-mirror-aws.log ${TERRAFORM_INIT} --taint '.*(domain|main_disk).*' --runstep provision --sumaform-backend aws"
                         deployed_aws = true
 
                     }
