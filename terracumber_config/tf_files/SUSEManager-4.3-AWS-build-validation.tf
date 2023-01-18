15722e97c12e28490f0b7c62043c8c091e469dde
Maxime Noel <maxime.noel@suse.com>
2023-01-19 11:27:32 +1300

    Remove load AWS variable

--- terracumber_config/tf_files/SUSEManager-4.3-AWS-build-validation.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-AWS-build-validation.tf
@@ -143,8 +143,8 @@ variable "NAME_PREFIX" {
 
 provider "aws" {
   region     = var.REGION
-  access_key = var.ACCESS_KEY
-  secret_key = var.SECRET_KEY
+//  access_key = var.ACCESS_KEY
+//  secret_key = var.SECRET_KEY
 //  token = var.TOKEN_AWS
 }
 
