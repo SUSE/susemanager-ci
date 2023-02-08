ab6d0cab7b4cb0465c6e496bd4c9c23964e7e898
Maxime Noel <maxime.noel@suse.com>
2023-02-08 14:00:07 +1300

    Add testing remove list and unset

--- jenkins_pipelines/environments/common/test_new_pipeline.groovy
+++ jenkins_pipelines/environments/common/test_new_pipeline.groovy
@@ -13,13 +13,40 @@ pipeline{
 
 def doDynamicParallelSteps(){
     def list_value = ["Test-1", "Test-2", "Test-3", "Test-4", "Test-5"]
-    tests = [:]
-    for (element in list_value ) {
-        tests["${element}"] = {
-            node {
-                stage("${element}") {
-                    echo element
-                }
+    def minion
+    Set<String> nodeList = new HashSet<String>()
+    minions = sh(script: "source /home/maxime/.profile; printenv | grep MINION || exit 0",
+            returnStdout: true)
+    sshminion = sh(script: "source /home/maxime/.profile; printenv | grep SSHMINION || exit 0",
+            returnStdout: true)
+    client = sh(script: "source /home/maxime/.profile; printenv | grep CLIENT || exit 0",
+            returnStdout: true)
+    String[] minion_list = minions.split("\n")
+    String[] sshminion_list = sshminion.split("\n")
+    String[] client_list = client.split("\n")
+    echo minion_list.join(", ")
+    echo sshminion_list.join(", ")
+    echo client_list.join(", ")
+
+    def node_list = [minion_list, sshminion_list, client_list].flatten().findAll { it }
+    echo node_list.join(", ")
+    node_list.each { lane->
+        def instanceList = lane.tokenize("=")
+        nodeList.add(instanceList[0])
+    }
+
+    node_list.each { element ->
+
+        minion = element.split("=")[0].toLowerCase()
+        def removeElement = nodeList.remove(element.split("=")[0])
+        tests["job-${minion}"] = {
+            stage("${minion}") {
+                echo minion
+                sh "source /home/maxime/.profile; printenv | grep MINION || exit 0"
+            }
+            stage("List without ${minion}") {
+                echo minion
+                sh "source /home/maxime/.profile; unset ${removeElement}; printenv | grep MINION || exit 0"
             }
         }
     }
