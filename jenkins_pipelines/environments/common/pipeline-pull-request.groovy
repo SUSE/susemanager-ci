569997a6534df6e04bce4926f9f9196dc7162f20
Cédric Bosdonnat <cbosdonnat@suse.com>
2023-03-30 18:03:50 +0200

    Revert test commits pushed by mistake

--- jenkins_pipelines/environments/common/pipeline-pull-request.groovy
+++ jenkins_pipelines/environments/common/pipeline-pull-request.groovy
@@ -9,7 +9,7 @@ def run(params) {
         terraform_bin = '/usr/bin/terraform'
         terraform_bin_plugins = '/usr/bin'
         service_pack_migration = false
-        terracumber_gitrepo = 'https://github.com/cbosdo/terracumber.git'
+        terracumber_gitrepo = 'https://github.com/uyuni-project/terracumber.git'
         terracumber_ref = 'master'
         terraform_init = true
         rake_namespace = 'cucumber'
