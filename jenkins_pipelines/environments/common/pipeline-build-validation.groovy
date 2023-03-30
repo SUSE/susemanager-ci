2e96980ba0f9b13168d7fcc0610c828343e3ad56
Maxime Noel <maxime.noel@suse.com>
2023-03-30 16:13:34 +1300

    Fix delete error

--- jenkins_pipelines/environments/common/pipeline-build-validation.groovy
+++ jenkins_pipelines/environments/common/pipeline-build-validation.groovy
@@ -47,7 +47,7 @@ def run(params) {
                     // Generate features
                     sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd 'export BUILD_VALIDATION=true; cd /root/spacewalk/testsuite; rake utils:generate_build_validation_features'"
                     // Generate rake files
-                    sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd 'export BUILD_VALIDATION=true; cd /root/spacewalk/testsuite; rake :generate_rake_files_build_validation'"
+                    sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd 'export BUILD_VALIDATION=true; cd /root/spacewalk/testsuite; rake jenkins:generate_rake_files_build_validation'"
                     deployed = true
                 }
             }
