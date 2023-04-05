725920bb9f665e5f69531610c4902558b8555f12
Cédric Bosdonnat <cbosdonnat@suse.com>
2023-04-05 07:47:36 +0200

    k3s: add chart url

--- terracumber_config/tf_files/Uyuni-Master-K3S.tf
+++ terracumber_config/tf_files/Uyuni-Master-K3S.tf
@@ -135,6 +135,7 @@ module "cucumber_testsuite" {
       }
       runtime = "k3s"
       container_repository = "registry.opensuse.org/systemsmanagement/uyuni/master/servercontainer/containers/uyuni"
+      helm_chart_url = "oci://registry.opensuse.org//systemsmanagement/uyuni/master/servercontainer/charts/uyuni/server"
       // Your server repo providing all dependencies to run server in K3S
       // additional_repos = {
       //   server_repo = "",
