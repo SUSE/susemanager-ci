298f5495b1bb24f734736544b691fb8cd0983a66
Konstantinos Tsamis <ktsamis@suse.de>
2023-05-03 11:26:15 +0200

    Add pipeline for openqa

---
+++ jenkins_pipelines/environments/common/openqa.groovy
@@ -0,0 +1,30 @@
+def run(params) {
+    timestamps {
+          stages {
+            stage('Installation') {
+              steps {
+                script {
+                  if (params['Installation Type'] == 'Online') {
+                    sh 'ssh -v -o StrictHostKeyChecking=no root@openqa-executor.mgr.suse.de; cd /root/openqa-suma-installation; cp params-run-installation-43_online.json params-run-installation-43.json;'
+                  } else {
+                    sh 'ssh -v -o StrictHostKeyChecking=no root@openqa-executor.mgr.suse.de; cd /root/openqa-suma-installation; cp params-run-installation-43_full.json params-run-installation-43.json;'
+                  }
+                }
+              }
+            }
+            stage('Configuration of ISO_URL') {
+              steps {
+                sh "ssh -v -o StrictHostKeyChecking=no root@openqa-executor.mgr.suse.de ; cat params-run-installation-43.json | jq '. + {\"ISO_URL\": \"${params.ISO_URL}\" }' > params-run-installation-43.json"
+              }
+            }
+          }
+        }
+
+        stage('Run') {
+            // sh "ssh -v -o StrictHostKeyChecking=no root@openqa-executor.mgr.suse.de ; cd /root/openqa-suma-installation/; /root/openqa-suma-installation/run-openqa-test.sh $BUILD_NUMBER 43\n"
+            sh "ssh -v -o StrictHostKeyChecking=no root@openqa-executor.mgr.suse.de ; cd /root/openqa-suma-installation/; cat params-run-installation-43.json;
+        }
+    }
+
+
+return this
