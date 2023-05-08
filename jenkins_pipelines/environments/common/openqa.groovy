4b91fff90702a5bb9fe864b0d3722619ed7ecf06
Konstantinos Tsamis <ktsamis@suse.de>
2023-05-08 11:38:19 +0200

    Reduce complexity and use variable

--- jenkins_pipelines/environments/common/openqa.groovy
+++ jenkins_pipelines/environments/common/openqa.groovy
@@ -2,13 +2,7 @@ def run(params) {
     timestamps {
 
         stage('Parameterize') {
-            script {
-              if (params.Installation_Type == 'online') {
-                    sh "ssh -o StrictHostKeyChecking=no root@openqa-executor.mgr.suse.de \"cd /root/openqa-suma-installation; jq '. + {\\\"ISO_URL\\\": \\\"${params.ISO_URL}\\\" }' params-run-installation-43-online.json > params-run-installation-43-online.json.tmp && mv params-run-installation-43-online.json.tmp params-run-installation-43-online.json; cat params-run-installation-43-online.json;\""
-              }
-              else {
-                    sh "ssh -o StrictHostKeyChecking=no root@openqa-executor.mgr.suse.de \"cd /root/openqa-suma-installation; jq '. + {\\\"ISO_URL\\\": \\\"${params.ISO_URL}\\\" }' params-run-installation-43-offline.json > params-run-installation-43-offline.json.tmp && mv params-run-installation-43-offline.json.tmp params-run-installation-43-offline.json; cat params-run-installation-43-offline.json;\""
-              }
+            sh "ssh -o StrictHostKeyChecking=no root@openqa-executor.mgr.suse.de \"cd /root/openqa-suma-installation; jq '. + {\\\"ISO_URL\\\": \\\"${params.ISO_URL}\\\" }' params-run-installation-43-${params.Installation_Type}.json > params-run-installation-43-${params.Installation_Type}.json.tmp && mv params-run-installation-43-${params.Installation_Type}.json.tmp params-run-installation-43-${params.Installation_Type}.json; cat params-run-installation-43-${params.Installation_Type}.json;\""
             }
         }
 
