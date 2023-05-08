ac3bceb6e9a97389377714d033d16adf75279b21
Konstantinos Tsamis <ktsamis@suse.de>
2023-05-08 11:41:02 +0200

    Set up new openqa installation jobs (#853)

---
+++ jenkins_pipelines/environments/common/openqa.groovy
@@ -0,0 +1,15 @@
+def run(params) {
+    timestamps {
+
+        stage('Parameterize') {
+            sh "ssh -o StrictHostKeyChecking=no root@openqa-executor.mgr.suse.de \"cd /root/openqa-suma-installation; jq '. + {\\\"ISO_URL\\\": \\\"${params.ISO_URL}\\\" }' params-run-installation-43-${params.Installation_Type}.json > params-run-installation-43-${params.Installation_Type}.json.tmp && mv params-run-installation-43-${params.Installation_Type}.json.tmp params-run-installation-43-${params.Installation_Type}.json; cat params-run-installation-43-${params.Installation_Type}.json;\""
+        }
+
+        stage('Run') {
+            sh "ssh -o StrictHostKeyChecking=no root@openqa-executor.mgr.suse.de \"cd /root/openqa-suma-installation/; /root/openqa-suma-installation/run-openqa-test.sh $BUILD_NUMBER 43 ${params.Installation_Type}\n\""
+        }
+    }
+}
+
+
+return this
