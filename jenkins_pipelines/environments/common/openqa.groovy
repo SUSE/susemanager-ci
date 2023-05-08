b779f3e6b1690e312aa7c33a0e4f331cc7cb8f0e
Konstantinos Tsamis <ktsamis@suse.de>
2023-05-08 11:39:47 +0200

    Fix typo

--- jenkins_pipelines/environments/common/openqa.groovy
+++ jenkins_pipelines/environments/common/openqa.groovy
@@ -3,7 +3,6 @@ def run(params) {
 
         stage('Parameterize') {
             sh "ssh -o StrictHostKeyChecking=no root@openqa-executor.mgr.suse.de \"cd /root/openqa-suma-installation; jq '. + {\\\"ISO_URL\\\": \\\"${params.ISO_URL}\\\" }' params-run-installation-43-${params.Installation_Type}.json > params-run-installation-43-${params.Installation_Type}.json.tmp && mv params-run-installation-43-${params.Installation_Type}.json.tmp params-run-installation-43-${params.Installation_Type}.json; cat params-run-installation-43-${params.Installation_Type}.json;\""
-            }
         }
 
         stage('Run') {
