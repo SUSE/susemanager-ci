33b913ef724f7de04417b24a3b5adfd98c6c7e97
Maxime Noel <maxime.noel@suse.com>
2023-06-21 14:22:18 +1200

    Add variable file for PR testing

---
+++ terracumber_config/tf_files/variables/PR-TEST-variable.tf
@@ -0,0 +1,157 @@
+// Mandatory variables for terracumber
+variable "URL_PREFIX" {
+  type = string
+}
+
+// Not really used as this is for --runall parameter, and we run cucumber step by step
+variable "CUCUMBER_COMMAND" {
+  type = string
+}
+
+variable "CUCUMBER_GITREPO" {
+  type = string
+}
+
+variable "CUCUMBER_BRANCH" {
+  type = string
+}
+
+variable "CUCUMBER_RESULTS" {
+  type = string
+  default = "/root/spacewalk/testsuite"
+}
+
+variable "MAIL_SUBJECT" {
+  type = string
+}
+
+variable "MAIL_TEMPLATE" {
+  type = string
+}
+
+variable "MAIL_SUBJECT_ENV_FAIL" {
+  type = string
+  default = "Failed acceptance tests on Pull Request: Environment setup failed"
+}
+
+variable "MAIL_TEMPLATE_ENV_FAIL" {
+  type = string
+}
+
+variable "ENVIRONMENT" {
+  type = string
+}
+
+variable "HYPER" {
+  type = string
+}
+
+variable "MAIL_FROM" {
+  type = string
+  default = "galaxy-ci@suse.de"
+}
+
+variable "MAIL_TO" {
+  type = string
+  default = "galaxy-ci@suse.de"
+}
+
+// sumaform specific variables
+variable "SCC_USER" {
+  type = string
+}
+
+variable "SCC_PASSWORD" {
+  type = string
+}
+
+variable "GIT_USER" {
+  type = string
+  default = null // Not needed for master, as it is public
+}
+
+variable "GIT_PASSWORD" {
+  type = string
+  default = null // Not needed for master, as it is public
+}
+
+// Repository containing the build for the tested Uyuni Pull Request
+variable "PULL_REQUEST_REPO" {
+  type = string
+}
+
+variable "MASTER_REPO" {
+  type = string
+}
+
+variable "MASTER_OTHER_REPO" {
+  type = string
+}
+
+variable "MASTER_SUMAFORM_TOOLS_REPO" {
+  type = string
+}
+
+variable "UPDATE_REPO" {
+  type = string
+}
+
+variable "ADDITIONAL_REPO_URL" {
+  type = string
+}
+
+variable "TEST_PACKAGES_REPO" {
+  type = string
+}
+
+// Repositories containing the client tools RPMs
+variable "SLE_CLIENT_REPO" {
+  type = string
+}
+
+variable "RHLIKE_CLIENT_REPO" {
+  type = string
+}
+
+variable "DEBLIKE_CLIENT_REPO" {
+  type = string
+}
+
+variable "OPENSUSE_CLIENT_REPO" {
+  type = string
+}
+
+variable "IMAGE" {
+  type = string
+}
+
+variable "GIT_PROFILES_REPO" {
+  type = string
+}
+
+variable "IMAGES" {
+  type = list(string)
+}
+
+variable "PRODUCT_VERSION" {
+  type = list(string)
+}
+
+variable "MIRROR" {
+  type = string
+}
+
+variable "USE_MIRROR" {
+  type = boolean
+}
+
+variable "DOMAIN" {
+  type = string
+}
+
+### Related to environent solution 2 ####
+
+variable "environment_configuration" {
+  type = map(string)
+  description = "Collection of  value containing : mac addresses, hypervisor and additional network"
+}
