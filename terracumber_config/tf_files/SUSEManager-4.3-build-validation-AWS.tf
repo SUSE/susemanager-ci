db14c8347257d01a214e770c3e6b5f96d77135e3
Maxime Noel <maxime.noel@suse.com>
2023-07-31 10:41:24 +1200

    Align values

--- terracumber_config/tf_files/SUSEManager-4.3-build-validation-AWS.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-build-validation-AWS.tf
@@ -183,7 +183,7 @@ module "server" {
   publish_private_ssl_key        = false
   use_os_released_updates        = false
   disable_download_tokens        = false
-  disable_auto_bootstrap        = true
+  disable_auto_bootstrap         = true
   ssh_key_path            = "./salt/controller/id_rsa.pub"
   provider_settings = {
     instance_type = "m6a.xlarge"
