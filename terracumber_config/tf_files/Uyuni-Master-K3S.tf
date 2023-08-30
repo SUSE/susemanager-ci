56efaa5bfdb5c19f9a3c7479fcac6ec2a60607fc
mbussolotto <michele.bussolotto@suse.com>
2023-08-30 15:39:19 +0200

    add nested mac address to k3s

--- terracumber_config/tf_files/Uyuni-Master-K3S.tf
+++ terracumber_config/tf_files/Uyuni-Master-K3S.tf
@@ -230,6 +230,7 @@ module "cucumber_testsuite" {
     }
   }
   nested_vm_host = "min-nested"
+  nested_vm_mac =  "aa:b2:93:01:00:3f"
   provider_settings = {
     pool               = "ssd"
     network_name       = null
