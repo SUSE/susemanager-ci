1968c44b484cc6d324c8df97eedbac05d6ba35d2
mbussolotto <michele.bussolotto@suse.com>
2023-09-04 11:19:33 +0200

    use opensuse in hexagon tests (#945)

--- terracumber_config/tf_files/SUSEManager-Test-Hexagon-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Test-Hexagon-NUE.tf
@@ -103,7 +103,7 @@ module "cucumber_testsuite" {
   cc_username = var.SCC_USER
   cc_password = var.SCC_PASSWORD
 
-  images = ["rocky8o", "opensuse154o", "sles15sp4o", "ubuntu2204o"]
+  images = ["rocky8o", "opensuse154o", "ubuntu2204o"]
 
   use_avahi    = false
   name_prefix  = "suma-testhexagon-"
@@ -154,7 +154,7 @@ module "cucumber_testsuite" {
     }
 
     suse-minion = {
-      image = "sles15sp4o"
+      image = "opensuse154o"
       name = "min-suse"
       provider_settings = {
         mac = "aa:b2:93:01:00:56"
@@ -163,7 +163,7 @@ module "cucumber_testsuite" {
       install_salt_bundle = true
     }
     suse-sshminion = {
-      image = "sles15sp4o"
+      image = "opensuse154o"
       name = "minssh-suse"
       provider_settings = {
         mac = "aa:b2:93:01:00:58"
@@ -194,7 +194,7 @@ module "cucumber_testsuite" {
       install_salt_bundle = true
     }
     build-host = {
-      image = "sles15sp4o"
+      image = "opensuse154o"
       provider_settings = {
         mac = "aa:b2:93:01:00:5d"
         vcpu = 4
@@ -205,14 +205,16 @@ module "cucumber_testsuite" {
       install_salt_bundle = true
     }
     pxeboot-minion = {
-      image = "sles15sp4o"
+      image = "opensuse154o"
+      additional_packages = [ "venv-salt-minion" ]
+      install_salt_bundle = true
       provider_settings = {
         vcpu = 2
         memory = 2048
       }
     }
     kvm-host = {
-      image = "sles15sp4o"
+      image = "opensuse154o"
       name = "min-kvm"
       additional_grains = {
         hvm_disk_image = {
