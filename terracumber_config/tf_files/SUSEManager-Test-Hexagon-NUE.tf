4462fe5bdbd7a8720b125f982020fd629a3b5dbd
Dominik Gedon <dominik.gedon@suse.com>
2023-09-08 14:40:06 +0200

    Revert pxeboot-minion from Leap to SLES15 SP4
    Due to missing support for openSUSE Leap as underlying base OS for the
    pxeboot-minion, we revert back to SLES15 SP4.
    
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- terracumber_config/tf_files/SUSEManager-Test-Hexagon-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Test-Hexagon-NUE.tf
@@ -205,7 +205,7 @@ module "cucumber_testsuite" {
       install_salt_bundle = true
     }
     pxeboot-minion = {
-      image = "opensuse154o"
+      image = "sles15sp4o"
       additional_packages = [ "venv-salt-minion" ]
       install_salt_bundle = true
       provider_settings = {
