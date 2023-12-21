a45e58b99b24b9b68f4ec73fb8ac0b920514d849
Maxime Noel <maxime.noel@suse.com>
2023-12-22 08:51:24 +1300

    remove none related changes

--- terracumber_config/tf_files/Uyuni-Master-K3S.tf
+++ terracumber_config/tf_files/Uyuni-Master-K3S.tf
@@ -129,7 +129,6 @@ module "cucumber_testsuite" {
       provider_settings = {
         mac = "aa:b2:93:01:00:30"
       }
-      image = "opensuse155o"
     }
     server_containerized = {
       provider_settings = {
@@ -147,7 +146,6 @@ module "cucumber_testsuite" {
       }
       additional_packages = [ "venv-salt-minion" ]
       install_salt_bundle = true
-      image = "opensuse155o"
     }
     suse-minion = {
       image = "opensuse154o"
