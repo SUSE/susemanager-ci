37177446af7c3e229aa0a43c6b1b487911d3a86d
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2023-12-26 10:22:58 +0000

    Add environment for Ubuntu2204 with Salt Bundle

---
+++ terracumber_config/tf_files/Salt-Shaker-Testing-Ubuntu2204-Bundle.tf
@@ -0,0 +1,113 @@
+// Mandatory variables for terracumber
+variable "URL_PREFIX" {
+  type = string
+  default = "https://ci.suse.de/view/Manager/view/salt/job/manager-salt-shaker-products-testing-ubuntu2204-bundle"
+}
+
+// Not really used as this is for --runall parameter, and we run cucumber step by step
+variable "CUCUMBER_COMMAND" {
+  type = string
+  default = "echo EXECUTE SALT TESTS HERE"
+}
+variable "CUCUMBER_BRANCH" {
+  type = string
+  default = "master"
+}
+
+variable "CUCUMBER_RESULTS" {
+  type = string
+  default = "/root/"
+}
+
+variable "MAIL_SUBJECT" {
+  type = string
+  default = "Results Salt Shaker - products:next - Ubuntu2204 Salt Bundle $status: $tests scenarios ($failures failed, $errors errors, $skipped skipped, $passed passed)"
+}
+
+variable "MAIL_TEMPLATE" {
+  type = string
+  default = "../mail_templates/mail-template-salt-shaker.txt"
+}
+
+variable "MAIL_SUBJECT_ENV_FAIL" {
+  type = string
+  default = "Results Salt Shaker - products:next - Ubuntu2204 Salt Bundle: Environment setup failed"
+}
+
+variable "MAIL_TEMPLATE_ENV_FAIL" {
+  type = string
+  default = "../mail_templates/mail-template-salt-shaker-env-fail.txt"
+}
+
+variable "MAIL_FROM" {
+  type = string
+  default = "galaxy-ci@suse.de"
+}
+
+variable "MAIL_TO" {
+  type = string
+  default = "galaxy-ci@suse.de"
+}
+
+// sumaform specific variables
+variable "SCC_USER" {
+  type = string
+  default = null // Not needed for Salt tests
+}
+
+variable "SCC_PASSWORD" {
+  type = string
+  default = null // Not needed for Salt tests
+}
+
+variable "GIT_USER" {
+  type = string
+  default = null // Not needed for master, as it is public
+}
+
+variable "GIT_PASSWORD" {
+  type = string
+  default = null // Not needed for master, as it is public
+}
+
+terraform {
+  required_version = "1.0.10"
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "0.6.3"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu+tcp://cthulhu.mgr.suse.de/system"
+}
+
+module "base" {
+  source = "./modules/base"
+
+  cc_username = var.SCC_USER
+  cc_password = var.SCC_PASSWORD
+
+  provider_settings = {
+    pool               = "ssd"
+    network_name       = null
+    bridge             = "br0"
+  }
+
+  images = [ "ubuntu2204o" ]
+}
+
+module "salt-test-node" {
+  source             = "./modules/salt_testenv"
+  base_configuration = module.base.configuration
+
+  name               = "salt-test-node-ubuntu2204-bundle"
+  image              = "ubuntu2204o"
+  salt_obs_flavor    = "products:next"
+}
+
+output "configuration" {
+  value = module.salt-test-node.configuration
+}
