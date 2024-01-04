1e6dd99ee58195f24d077f78d4c538fe30e49ebb
Eric Bischoff <ebischoff@suse.com>
2024-01-04 14:13:43 +0100

    Fix path for sumaform backend modules

--- terracumber_config/tf_files/SUSEManager-4.3-build-validation-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-build-validation-NUE.tf
@@ -157,7 +157,7 @@ module "base_core" {
 //}
 
 module "base_s390" {
-  source = "./sumaform/backend_modules/feilong/base"
+  source = "./backend_modules/feilong/base"
 
   name_prefix = "suma-bv-43-"
   domain      = "mgr.suse.de"
@@ -787,7 +787,7 @@ module "debian12-minion" {
 //}
 
 module "sles15sp5s390-minion" {
-  source             = "./sumaform/backend_modules/feilong/host"
+  source             = "./backend_modules/feilong/host"
   base_configuration = module.base_s390.configuration
 
   name               = "min-sles15sp3s390"
@@ -1247,7 +1247,7 @@ module "debian12-sshminion" {
 //}
 
 module "sles15sp5s390-sshminion" {
-  source             = "./sumaform/backend_modules/feilong/host"
+  source             = "./backend_modules/feilong/host"
   base_configuration = module.base_s390.configuration
 
   name               = "minssh-sles15sp3s390"
