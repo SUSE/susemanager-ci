7360ff337178521ea3b13a78172b300a6e085caa
Oscar Barrios <obarrios@suse.com>
2024-01-08 11:04:35 +0100

    Keep database content inside the root partition on BV (#1063)

--- terracumber_config/tf_files/SUSEManager-4.3-build-validation-AWS.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-build-validation-AWS.tf
@@ -168,7 +168,6 @@ module "server" {
   name                       = "server"
   product_version            = "4.3-released"
   repository_disk_size       = 1500
-  database_disk_size         = 100
   server_registration_code   = var.SERVER_REGISTRATION_CODE
 
   java_debugging                 = false
