970210c436395a5e36624b4fab0f8b4ee28cc032
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2024-01-12 12:08:07 +0000

    Add Salt Shaker environments and TF files (#1069)
    * Add initial test environment and pipeline for Salt Shaker
    
    * Remove additional_network for Salt Shaker tf file
    
    * TEMP: Switch to custom branch on sumaform_ref
    
    * Add new environment for Salt Shaker SLE15SP4 with Salt Bundle
    
    * Add link to testReport in Salt Shaker email template
    
    * Add environment for Debian12 with Salt Bundle
    
    * Add environment for Ubuntu2204 with Salt Bundle
    
    * Add environment for SLES15SP1 classic and Salt Bundle
    
    * Modify salt-shaker pipeline to run all tests groups
    
    * Update environments according to latest pipeline fixes
    
    * Provide better feedback if stage is failing
    
    * Pass skiplist url as pipeline parameters and align environments
    
    * Add tf files and environments for the rest of Salt Shaker jobs
    
    * Fix VM names for Salt Shaker nodes in tf files
    
    * Remove unnecessary parameter for salt shaker environments
    
    * Change email address to send Salt Shaker results
    
    * Move Salt-Shaker-Next-AlmaLinux9-Bundle away from cthulhu to suma03
    
    * Use custom executor for Salt Shaker jobs
    
    * Create VMs for Salt Shaker in suma-03 and suma-04
    
    * Fix URL_PREFIX to point access job view Salt Shaker view
    
    * Use default instead of work in progress branches
    
    * Salt Shaker: add extra URLTrigger to some OSes
    
    * Organize Salt Shaker related files into custom directories

---
+++ jenkins_pipelines/environments/common/pipeline-salt-shaker.groovy
@@ -0,0 +1,106 @@
+def run(params) {
+    timestamps {
+        // Init path env variables
+        env.resultdir = "${WORKSPACE}/results"
+        env.resultdirbuild = "${resultdir}/${BUILD_NUMBER}"
+
+        // The junit plugin doesn't affect full paths
+        junit_resultdir = "results/${BUILD_NUMBER}/results_junit"
+        env.common_params = "--outputdir ${resultdir} --tf ${params.tf_file} --gitfolder ${resultdir}/sumaform --terraform-bin ${params.terraform_bin}"
+
+        // Start pipeline
+        deployed = false
+        try {
+            stage('Clone terracumber, susemanager-ci and sumaform') {
+                // Create a directory for  to place the directory with the build results (if it does not exist)
+                sh "mkdir -p ${resultdir}"
+                git url: params.terracumber_gitrepo, branch: params.terracumber_ref
+                dir("susemanager-ci") {
+                    checkout scm
+                }
+                // Clone sumaform
+                sh "set +x; source /home/jenkins/.credentials set -x; ./terracumber-cli ${common_params} --gitrepo ${params.sumaform_gitrepo} --gitref ${params.sumaform_ref} --runstep gitsync"
+                // Restore Terraform states from artifacts
+                if (params.use_previous_terraform_state) {
+                    copyArtifacts projectName: currentBuild.projectName, selector: specific("${currentBuild.previousBuild.number}")
+                }
+            }
+            stage('Deploy') {
+                // Provision the environment
+                if (params.terraform_init) {
+                    env.TERRAFORM_INIT = '--init'
+                } else {
+                    env.TERRAFORM_INIT = ''
+                }
+                env.TERRAFORM_TAINT = ''
+                if (params.terraform_taint) {
+                    switch(params.sumaform_backend) {
+                        case "libvirt":
+                            env.TERRAFORM_TAINT = " --taint '.*(domain|main_disk).*'";
+                            break;
+                        case "aws":
+                            env.TERRAFORM_TAINT = " --taint '.*(host).*'";
+                            break;
+                        default:
+                            println("ERROR: Unknown backend ${params.sumaform_backend}");
+                            sh "exit 1";
+                            break;
+                    }
+                }
+                retry(count: 3) {
+                    sh "set +x; source /home/jenkins/.credentials set -x; export TERRAFORM=${params.terraform_bin}; export TERRAFORM_PLUGINS=${params.terraform_bin_plugins}; ./terracumber-cli ${common_params} --logfile ${resultdirbuild}/sumaform.log ${env.TERRAFORM_INIT} ${env.TERRAFORM_TAINT} --sumaform-backend ${params.sumaform_backend} --runstep provision"
+                    deployed = true
+                }
+            }
+            stage('Salt Shaker testsuite - Unit tests') {
+	        if (!params.run_unit_tests) {
+	            echo "Skipping unit tests as they were not selected for this execution"
+		    return
+		}
+		catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
+                    sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep saltshaker --saltshaker-cmd '/usr/bin/salt-test --package-flavor ${params.salt_flavor} --skiplist ${params.skip_list_url} unit -- --core-tests --ssh-tests --slow-tests --run-expensive --run-destructive --junitxml /root/results_junit/junit-report-unit.xml -vvv --tb=native'"
+		}
+            }
+
+            stage('Salt Shaker testsuite - Integration tests') {
+	        if (!params.run_integration_tests) {
+	            echo "Skipping integration tests as they were not selected for this execution"
+		    return
+		}
+		catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
+                    sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep saltshaker --saltshaker-cmd '/usr/bin/salt-test --package-flavor ${params.salt_flavor} --skiplist ${params.skip_list_url} integration -- --core-tests --ssh-tests --slow-tests --run-expensive --run-destructive --junitxml /root/results_junit/junit-report-integration.xml -vvv --tb=native'"
+		}
+            }
+
+            stage('Salt Shaker testsuite - Functional tests') {
+	        if (!params.run_functional_tests) {
+	            echo "Skipping functional tests as they were not selected for this execution"
+		    return
+		}
+		catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
+	            sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep saltshaker --saltshaker-cmd '/usr/bin/salt-test --package-flavor ${params.salt_flavor} --skiplist ${params.skip_list_url} functional -- --core-tests --ssh-tests --slow-tests --run-expensive --run-destructive --junitxml /root/results_junit/junit-report-functional.xml -vvv --tb=native'"
+		}
+            }
+        }
+        finally {
+            stage('Save TF state') {
+                    archiveArtifacts artifacts: "results/sumaform/terraform.tfstate, results/sumaform/.terraform/**/*"
+            }
+
+            stage('Get results') {
+                def error = 0
+                if (deployed) {
+                    sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep saltshaker_getresults"
+                    junit allowEmptyResults: true, testResults: "${junit_resultdir}/*.xml"
+                }
+                // Send email
+                sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/mail.log --runstep saltshaker_mail"
+                // Clean up old results
+                sh "./clean-old-results -r ${resultdir}"
+                sh "exit ${error}"
+            }
+        }
+    }
+}
+
+return this
---
+++ terracumber_config/tf_files/salt-shaker/Salt-Shaker-Testing-SLES15SP5-Bundle.tf
@@ -0,0 +1,113 @@
+// Mandatory variables for terracumber
+variable "URL_PREFIX" {
+  type = string
+  default = "https://ci.suse.de/user/manager/my-views/view/Salt%20Shaker/job/manager-salt-shaker-products-testing-sles15sp5-bundle"
+}
+
+// Not really used as this is for --runall parameter, and we run cucumber step by step
+variable "CUCUMBER_COMMAND" {
+  type = string
+  default = "echo EXECUTE SALT TESTS HERE"
+}
+variable "CUCUMBER_BRANCH" {
+  type = string
+  default = "master"
+}
+
+variable "CUCUMBER_RESULTS" {
+  type = string
+  default = "/root/"
+}
+
+variable "MAIL_SUBJECT" {
+  type = string
+  default = "Results Salt Shaker - products:testing - SLES15SP5 Salt Bundle $status: $tests scenarios ($failures failed, $errors errors, $skipped skipped, $passed passed)"
+}
+
+variable "MAIL_TEMPLATE" {
+  type = string
+  default = "../mail_templates/mail-template-salt-shaker.txt"
+}
+
+variable "MAIL_SUBJECT_ENV_FAIL" {
+  type = string
+  default = "Results Salt Shaker - products:testing - SLES15SP5 Salt Bundle: Environment setup failed"
+}
+
+variable "MAIL_TEMPLATE_ENV_FAIL" {
+  type = string
+  default = "../mail_templates/mail-template-salt-shaker-env-fail.txt"
+}
+
+variable "MAIL_FROM" {
+  type = string
+  default = "salt-ci@suse.de"
+}
+
+variable "MAIL_TO" {
+  type = string
+  default = "salt-ci@suse.de"
+}
+
+// sumaform specific variables
+variable "SCC_USER" {
+  type = string
+  default = null // Not needed for Salt tests
+}
+
+variable "SCC_PASSWORD" {
+  type = string
+  default = null // Not needed for Salt tests
+}
+
+variable "GIT_USER" {
+  type = string
+  default = null // Not needed for master, as it is public
+}
+
+variable "GIT_PASSWORD" {
+  type = string
+  default = null // Not needed for master, as it is public
+}
+
+terraform {
+  required_version = "1.0.10"
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "0.6.3"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu+tcp://suma-03.mgr.suse.de/system"
+}
+
+module "base" {
+  source = "./modules/base"
+
+  cc_username = var.SCC_USER
+  cc_password = var.SCC_PASSWORD
+
+  provider_settings = {
+    pool               = "ssd"
+    network_name       = null
+    bridge             = "br0"
+  }
+
+  images = [ "sles15sp5o" ]
+}
+
+module "salt-shaker-products-testing" {
+  source             = "./modules/salt_testenv"
+  base_configuration = module.base.configuration
+
+  name               = "salt-shaker-products-testing-sles15sp5-bundle"
+  image              = "sles15sp5o"
+  salt_obs_flavor    = "products:testing"
+}
+
+output "configuration" {
+  value = module.salt-shaker-products-testing.configuration
+}
