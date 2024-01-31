4f805d86a01412df5184ac3bd87b028ebb609ab9
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2024-01-31 13:55:15 +0100

    Add 4.3 VM pipeline
    https://github.com/SUSE/spacewalk/issues/22625
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

---
+++ terracumber_config/tf_files/SUSEManager-4.3-VM-NUE.tf
@@ -0,0 +1,245 @@
+// Mandatory variables for terracumber
+variable "URL_PREFIX" {
+  type = string
+  default = "https://ci.suse.de/view/Manager/view/Manager-4.3/job/manager-4.3-vm-dev-acceptance-tests-NUE"
+}
+
+// Not really used as this is for --runall parameter, and we run cucumber step by step
+variable "CUCUMBER_COMMAND" {
+  type = string
+  default = "export PRODUCT='SUSE-Manager' && run-testsuite"
+}
+
+variable "CUCUMBER_GITREPO" {
+  type = string
+  default = "https://github.com/SUSE/spacewalk.git"
+}
+
+variable "CUCUMBER_BRANCH" {
+  type = string
+  default = "Manager-4.3"
+}
+
+variable "CUCUMBER_RESULTS" {
+  type = string
+  default = "/root/spacewalk/testsuite"
+}
+
+variable "MAIL_SUBJECT" {
+  type = string
+  default = "Results 4.3-NUE (backup and staging) $status: $tests scenarios ($failures failed, $errors errors, $skipped skipped, $passed passed)"
+}
+
+variable "MAIL_TEMPLATE" {
+  type = string
+  default = "../mail_templates/mail-template-jenkins-backup.txt"
+}
+
+variable "MAIL_SUBJECT_ENV_FAIL" {
+  type = string
+  default = "Results 4.3-NUE: Environment setup failed"
+}
+
+variable "MAIL_TEMPLATE_ENV_FAIL" {
+  type = string
+  default = "../mail_templates/mail-template-jenkins-backupenv-fail.txt"
+}
+
+variable "MAIL_FROM" {
+  type = string
+  default = "jenkins@suse.de"
+}
+
+variable "MAIL_TO" {
+  type = string
+  default = "galaxy-ci@suse.de"
+}
+
+// sumaform specific variables
+variable "SCC_USER" {
+  type = string
+}
+
+variable "SCC_PASSWORD" {
+  type = string
+}
+
+variable "GIT_USER" {
+  type = string
+  default = null // Not needed for master, as it is public
+}
+
+variable "GIT_PASSWORD" {
+  type = string
+  default = null // Not needed for master, as it is public
+}
+
+terraform {
+  required_version = "1.0.10"
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "0.6.3"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu+tcp://suma-08.mgr.suse.de/system"
+}
+
+module "cucumber_testsuite" {
+  source = "./modules/cucumber_testsuite"
+
+  product_version = "4.3-VM-nightly"
+
+  // Cucumber repository configuration for the controller
+  git_username = var.GIT_USER
+  git_password = var.GIT_PASSWORD
+  git_repo     = var.CUCUMBER_GITREPO
+  branch       = var.CUCUMBER_BRANCH
+
+  cc_username = var.SCC_USER
+  cc_password = var.SCC_PASSWORD
+
+  images = ["centos7o", "opensuse155o", "sles15sp4o", "ubuntu2204o", "suma43VM-ign"]
+
+  use_avahi    = false
+  name_prefix  = "suma-43-newvm-"
+  domain       = "mgr.suse.de"
+  from_email   = "root@suse.de"
+
+  no_auth_registry       = "registry.mgr.suse.de"
+  auth_registry          = "registry.mgr.suse.de:5000/cucutest"
+  auth_registry_username = "cucutest"
+  auth_registry_password = "cucusecret"
+  git_profiles_repo      = "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles/internal_nue"
+
+  mirror                   = "minima-mirror-ci-bv.mgr.suse.de"
+  use_mirror_images        = true
+  server_http_proxy        = "http-proxy.mgr.suse.de:3128"
+  custom_download_endpoint = "ftp://minima-mirror-ci-bv.mgr.suse.de:445"
+
+  # when changing images, please also keep in mind to adjust the image matrix at the end of the README.
+  host_settings = {
+    controller = {
+      provider_settings = {
+        mac = "aa:b2:93:01:00:80"
+        vcpu = 2
+        memory = 2048
+      }
+    }
+    server = {
+      provider_settings = {
+        mac = "aa:b2:93:01:00:81"
+        vcpu = 8
+        memory = 32768
+      }
+      repository_disk_size = 150
+      database_disk_size   = 50
+    }
+    proxy = {
+      provider_settings = {
+        mac = "aa:b2:93:01:00:82"
+        vcpu = 2
+        memory = 2048
+      }
+      additional_packages = [ "venv-salt-minion" ]
+      install_salt_bundle = true
+    }
+    suse-client = {
+      image = "sles15sp4o"
+      name = "cli-sles15"
+      provider_settings = {
+        mac = "aa:b2:93:01:00:84"
+        vcpu = 2
+        memory = 2048
+      }
+    }
+    suse-minion = {
+      image = "sles15sp4o"
+      name = "min-sles15"
+      provider_settings = {
+        mac = "aa:b2:93:01:00:86"
+        vcpu = 2
+        memory = 2048
+      }
+      additional_packages = [ "venv-salt-minion" ]
+      install_salt_bundle = true
+    }
+    suse-sshminion = {
+      image = "sles15sp4o"
+      name = "minssh-sles15"
+      provider_settings = {
+        mac = "aa:b2:93:01:00:88"
+        vcpu = 2
+        memory = 2048
+      }
+      additional_packages = [ "venv-salt-minion", "iptables" ]
+      install_salt_bundle = true
+    }
+    redhat-minion = {
+      image = "centos7o"
+      name = "min-centos7"
+      provider_settings = {
+        mac = "aa:b2:93:01:00:89"
+        // Since start of May we have problems with the instance not booting after a restart if there is only a CPU and only 1024Mb for RAM
+        // Also, openscap cannot run with less than 1.25 GB of RAM
+        vcpu = 2
+        memory = 2048
+      }
+      additional_packages = [ "venv-salt-minion" ]
+      install_salt_bundle = true
+    }
+    debian-minion = {
+      image = "ubuntu2204o"
+      name = "min-ubuntu2204"
+      provider_settings = {
+        mac = "aa:b2:93:01:00:8b"
+        vcpu = 2
+        memory = 2048
+      }
+      additional_packages = [ "venv-salt-minion" ]
+      install_salt_bundle = true
+    }
+    build-host = {
+      image = "sles15sp4o"
+      provider_settings = {
+        mac = "aa:b2:93:01:00:8d"
+        vcpu = 4
+        memory = 8192
+      }
+      additional_packages = [ "venv-salt-minion" ]
+      install_salt_bundle = true
+    }
+    pxeboot-minion = {
+      image = "sles15sp4o"
+      provider_settings = {
+        vcpu = 2
+        memory = 2048
+      }
+    }
+    kvm-host = {
+      image = "sles15sp4o"
+      name = "min-kvm"
+      provider_settings = {
+        mac = "aa:b2:93:01:00:8e"
+        vcpu = 4
+        memory = 8192
+      }
+      additional_packages = [ "venv-salt-minion" ]
+      install_salt_bundle = true
+    }
+  }
+  
+  provider_settings = {
+    pool = "ssd"
+    network_name = null
+    bridge = "br0"
+    additional_network = "192.168.43.0/24"
+  }
+}
+
+output "configuration" {
+  value = module.cucumber_testsuite.configuration
+}
