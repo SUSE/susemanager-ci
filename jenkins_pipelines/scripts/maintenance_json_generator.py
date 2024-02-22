afd7475a7c1574b0f3a145d23bbae870b6e6c35b
Konstantinos Tsamis <ktsamis@suse.de>
2024-02-22 14:22:51 +0100

    Add support for 5.0 in json script (#1117)
    * Add support for 5.0 in json script
    
    * Add Alma 8
    
    * Add 5.0 beta client tools and server and proxy to json script
    
    * Remove debian 10 it is EOL

--- jenkins_pipelines/scripts/maintenance_json_generator.py
+++ jenkins_pipelines/scripts/maintenance_json_generator.py
@@ -5,8 +5,8 @@ import sys
 
 import requests
 
-# Default dictionary for client tools, essentially anything that is common between SUMA versions
-defaultdict = {
+# dictionary for 4.3 client tools
+clienttools43dict = {
     "sle12sp5_client": "/SUSE_Updates_SLE-Manager-Tools_12_x86_64/",
     "sle12sp5_minion": "/SUSE_Updates_SLE-Manager-Tools_12_x86_64/",
     "sle15_client": ["/SUSE_Updates_SLE-Manager-Tools_15_x86_64/",
@@ -44,14 +44,14 @@ defaultdict = {
                         "/SUSE_Updates_SLE-Module-Basesystem_15-SP5_x86_64/",
                         "/SUSE_Updates_SLE-Module-Server-Applications_15-SP5_x86_64/"],
     "sle15sp5s390_minion": ["/SUSE_Updates_SLE-Manager-Tools_15_s390x/",
-                             "/SUSE_Updates_SLE-Module-Basesystem_15-SP5_s390x/",
-                             "/SUSE_Updates_SLE-Module-Server-Applications_15-SP5_s390x/"],
+                            "/SUSE_Updates_SLE-Module-Basesystem_15-SP5_s390x/",
+                            "/SUSE_Updates_SLE-Module-Server-Applications_15-SP5_s390x/"],
     "centos7_client": "/SUSE_Updates_RES_7-CLIENT-TOOLS_x86_64/",
     "centos7_minion": "/SUSE_Updates_RES_7-CLIENT-TOOLS_x86_64",
     "rocky8_minion": "/SUSE_Updates_RES_8-CLIENT-TOOLS_x86_64/",
+    "alma8_minion": "/SUSE_Updates_RES_8-CLIENT-TOOLS_x86_64/",
     "ubuntu2004_minion": "/SUSE_Updates_Ubuntu_20.04-CLIENT-TOOLS_x86_64/",
     "ubuntu2204_minion": "/SUSE_Updates_Ubuntu_22.04-CLIENT-TOOLS_x86_64/",
-    "debian10_minion": "/SUSE_Updates_Debian_10-CLIENT-TOOLS_x86_64/",
     "debian11_minion": "/SUSE_Updates_Debian_11-CLIENT-TOOLS_x86_64/",
     "debian12_minion": "/SUSE_Updates_Debian_12-CLIENT-TOOLS_x86_64/",
     "opensuse154arm_minion": ["/SUSE_Updates_openSUSE-SLE_15.4/",
@@ -80,7 +80,51 @@ defaultdict = {
                               "/SUSE_Updates_SLE-Module-Server-Applications_15-SP5_x86_64/"],
 }
 
-# Dictionary for SUMA 4.3 Server and Proxy, which is then added together with the common dictionary for client tools
+# dictionary for 5.0 client tools
+clienttools50betadict = {
+    "sle12sp5_client": "/SUSE_Updates_SLE-Manager-Tools_12-BETA_x86_64/",
+    "sle12sp5_minion": "/SUSE_Updates_SLE-Manager-Tools_12-BETA_x86_64/",
+    "sle15_client": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15_minion": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15sp1_client": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15sp1_minion": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15sp2_client": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15sp2_minion": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15sp3_client": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15sp3_minion": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15sp4_client": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15sp4_minion": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15sp5_client": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15sp5_minion": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/",
+    "sle15sp5s390_minion": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_s390x/",
+    "centos7_client": "/SUSE_Updates_RES_7-CLIENT-TOOLS-BETA_x86_64/",
+    "centos7_minion": "/SUSE_Updates_RES_7-CLIENT-TOOLS-BETA_x86_64",
+    "rocky8_minion": "/SUSE_Updates_RES_8-CLIENT-TOOLS-BETA_x86_64/",
+    "alma8_minion": "/SUSE_Updates_RES_8-CLIENT-TOOLS-BETA_x86_64/",
+    "ubuntu2004_minion": "/SUSE_Updates_Ubuntu_20.04-CLIENT-TOOLS-BETA_x86_64/",
+    "ubuntu2204_minion": "/SUSE_Updates_Ubuntu_22.04-CLIENT-TOOLS-BETA_x86_64/",
+    "debian11_minion": "/SUSE_Updates_Debian_11-CLIENT-TOOLS-BETA_x86_64/",
+    "debian12_minion": "/SUSE_Updates_Debian_12-CLIENT-TOOLS-BETA_x86_64/",
+    "opensuse154arm_minion": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_aarch64/",
+    "opensuse155arm_minion": "/SUSE_Updates_SLE-Manager-Tools_15-BETA_aarch64/",
+    "rhel9_minion": "/SUSE_Updates_EL_9-CLIENT-TOOLS-BETA_x86_64/",
+    "rocky9_minion": "/SUSE_Updates_EL_9-CLIENT-TOOLS-BETA_x86_64/",
+    "alma9_minion": "/SUSE_Updates_EL_9-CLIENT-TOOLS-BETA_x86_64/",
+    "oracle9_minion": "/SUSE_Updates_EL_9-CLIENT-TOOLS-BETA_x86_64/",
+    "slemicro51_minion": ["/SUSE_Updates_SLE-Manager-Tools-BETA-For-Micro_5_x86_64/",
+                          "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/"],
+    "slemicro52_minion": ["/SUSE_Updates_SLE-Manager-Tools-BETA-For-Micro_5_x86_64/",
+                          "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/"],
+    "slemicro53_minion": ["/SUSE_Updates_SLE-Manager-Tools-BETA-For-Micro_5_x86_64/",
+                          "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/"],
+    "slemicro54_minion": ["/SUSE_Updates_SLE-Manager-Tools-BETA-For-Micro_5_x86_64/",
+                          "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/"],
+    "slemicro55_minion": ["/SUSE_Updates_SLE-Manager-Tools-BETA-For-Micro_5_x86_64/",
+                          "/SUSE_Updates_SLE-Manager-Tools_15-BETA_x86_64/"],
+    "salt_migration_minion": "/SUSE_Updates_SLE-Manager-Tools_15_x86_64-BETA/"
+}
+
+# Dictionary for SUMA 4.3 Server and Proxy, which is then added together with the common dictionary for 4.3 client tools
 nodesdict43 = {
     "server": ["/SUSE_Updates_SLE-Module-SUSE-Manager-Server_4.3_x86_64/",
                "/SUSE_Updates_SLE-Product-SUSE-Manager-Server_4.3_x86_64/",
@@ -92,7 +136,58 @@ nodesdict43 = {
               "/SUSE_Updates_SLE-Module-Basesystem_15-SP4_x86_64/",
               "/SUSE_Updates_SLE-Module-Server-Applications_15-SP4_x86_64/"]
 }
-nodesdict43.update(defaultdict)
+nodesdict43.update(clienttools43dict)
+
+# 5.0 Server coming from
+# https://build.suse.de/package/view_file/SUSE:SLE-15-SP5:Update:Products:Manager50/000product/SUSE-Manager-Server.product?expand=1
+# line 25 + 71 with ":" changed to "_" 5.0 Proxy coming from
+# https://build.suse.de/package/view_file/SUSE:SLE-15-SP5:Update:Products:Manager50/000product/SUSE-Manager-Proxy.product?expand=1
+# line 25 + 71 with ":" changed to "_"
+# Only product, we are extension not module anymore
+
+# Dictionary for SUMA 5.0 Server and Proxy, which is then added together with
+# the common dictionary for 4.3 client tools and 5.0 BETA client tools. Both
+# client tools are needed until 5.0 gets its own client tools.
+nodesdict50 = {
+    "server": ["/SUSE_Products_SUSE-Manager-Server_5.0_x86_64/",
+               "/SUSE_Updates_SUSE-Manager-Server_5.0_x86_64/"],
+    "proxy": ["/SUSE_Products_SUSE-Manager-Proxy_5.0_x86_64/",
+              "/SUSE_Updates_SUSE-Manager-Proxy_5.0_x86_64/"]
+}
+
+# Merging clienttools43dict and clienttools50betadict
+# For now we need non-BETA together with BETA client tools until the split
+# between 4.3 and 5.0 for client tools happens, just before GA
+# After the split the 5.0 non-BETA client tools will have different names than
+# the 4.3 non-BETA client tools. They will not be common.
+finaldict = {}
+
+for key, value in clienttools43dict.items():
+    if key in clienttools50betadict:
+        if isinstance(value, list):
+            if isinstance(clienttools50betadict[key], list):
+                finaldict[key] = value + clienttools50betadict[key]
+            else:
+                finaldict[key] = value + [clienttools50betadict[key]]
+        else:
+            if isinstance(clienttools50betadict[key], list):
+                finaldict[key] = [value] + clienttools50betadict[key]
+            else:
+                finaldict[key] = [value, clienttools50betadict[key]]
+    else:
+        finaldict[key] = value
+
+# Merging finaldict with nodesdict50
+for key, value in nodesdict50.items():
+    if key in finaldict:
+        if isinstance(finaldict[key], list):
+            finaldict[key].extend(value)
+        else:
+            finaldict[key] = [finaldict[key]] + value
+    else:
+        finaldict[key] = value
+
+nodesdict50 = finaldict
 
 
 def parse_args():
@@ -100,7 +195,8 @@ def parse_args():
         description="This script reads the open qam-manager requests and creates a json file that can be fed in the "
                     "BV testsuite pipeline")
     parser.add_argument("-v", "--version", dest="version",
-                        help="Version of SUMA you want to run this script for, the only option for now is 43 for 4.3",
+                        help="Version of SUMA you want to run this script for, the options are 43 for 4.3 and 50 for "
+                             "5.0. The default is 43 for now",
                         default="43", action='store')
     parser.add_argument("-i", "--mi_ids", dest="mi_ids", help="MI IDs", default=None, action='store')
 
@@ -134,10 +230,11 @@ def read_requests():
 def find_valid_repos(mi_ids, version):
     if version == '43':
         dict_version = nodesdict43
+    elif version == '50':
+        dict_version = nodesdict50
     else:
         print("You have not given one of the correct options, run the script with -h to see the correct ones")
         sys.exit(1)
-
     finaldict = {}
     for node, suffixraw in dict_version.items():
         for mi_id in mi_ids:
@@ -177,6 +274,15 @@ def find_valid_repos(mi_ids, version):
                             # for each mi_id we have multiple repos sometimes for each node
                             finaldict[node] = {mi_id: repo}
 
+    # TODO Remove the following hardcoding at GA, we should start getting MIs for server and proxy
+    # Add exception for specific URL for "server" and "proxy" nodes in version 5.0
+    if version == '50':
+        # Hardcoded URLs for "server" and "proxy" nodes until we get MIs with them
+        server_url = "http://download.suse.de/ibs/SUSE:/SLE-15-SP5:/Update:/Products:/Manager50/images/repo/SUSE-Manager-Server-5.0-POOL-x86_64-Media1/"
+        proxy_url = "http://download.suse.de/ibs/SUSE:/SLE-15-SP5:/Update:/Products:/Manager50/images/repo/SUSE-Manager-Proxy-5.0-POOL-x86_64-Media1/"
+        finaldict['server'] = {'server_50': server_url}
+        finaldict['proxy'] = {'proxy_50': proxy_url}
+
     # Format into json and print
     # Check that it's not empty and save to file
     if finaldict:
@@ -204,7 +310,8 @@ def create_url(mi_id, suffix):
 def main():
     args = parse_args()
     if args.mi_ids is not None:
-        mi_ids = args.mi_ids.split(",")
+        # Ignore spaces before or after the comma
+        mi_ids = [id.strip() for id in args.mi_ids.split(",")]
     else:
         mi_ids = read_requests()
     find_valid_repos(mi_ids, args.version)
