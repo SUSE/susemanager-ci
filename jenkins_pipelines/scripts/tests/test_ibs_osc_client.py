b8673e7801d12312cff9a2f68538582b624b62c0
Paolo Invernizzi <paolo.invernizzi@suse.com>
2024-03-14 11:47:40 +0100

    BV: Add parse_xml_issue unit test

--- jenkins_pipelines/scripts/tests/test_ibs_osc_client.py
+++ jenkins_pipelines/scripts/tests/test_ibs_osc_client.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+import xml.etree.ElementTree as ET
 import unittest
 
 from json_generator.ibs_osc_client import IbsOscClient
@@ -21,6 +22,19 @@ class IbsOscClientTestCase(unittest.TestCase):
                 self.assertRaises(ValueError, ibs_client._parse_embargo_date, date_text)
             else:
                 ibs_client._parse_embargo_date(date_text)
+
+    def test_parse_xml_issue(self):
+        ibs_client: IbsOscClient = IbsOscClient()
+
+        bugzilla_issue_text: str = '<issue id="1234567" tracker="bnc"></issue>'
+        bugzilla_issue_xml: ET.Element = ET.fromstring(bugzilla_issue_text)
+        bugzilla_issue_id: str = ibs_client._parse_xml_issue(bugzilla_issue_xml)
+        self.assertEqual(bugzilla_issue_id, "bnc#1234567")
+
+        cve_issue_text: str = '<issue id="2021-00001" tracker="cve" />'
+        cve_issue_xml: ET.Element = ET.fromstring(cve_issue_text)
+        cve_issue_id: str = ibs_client._parse_xml_issue(cve_issue_xml)
+        self.assertEqual(cve_issue_id, "CVE-2021-00001")
 
 if __name__ == '__main__':
     unittest.main()
