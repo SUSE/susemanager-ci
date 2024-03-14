7f50140b45afef27d1c18295e621607f558fe9df
Paolo Invernizzi <paolo.invernizzi@suse.com>
2024-03-14 11:47:40 +0100

    BV: Add get_patchinfo_issues_ids unit test

--- jenkins_pipelines/scripts/tests/test_ibs_osc_client.py
+++ jenkins_pipelines/scripts/tests/test_ibs_osc_client.py
@@ -7,8 +7,10 @@ from json_generator.ibs_osc_client import IbsOscClient
 
 class IbsOscClientTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.ibs_client: IbsOscClient = IbsOscClient()
+
     def test_parse_embargo_date(self):
-        ibs_client: IbsOscClient = IbsOscClient()
         test_date: datetime = datetime.now(tz=timezone.utc)
 
         test_cases: list[tuple[str, bool]] = [
@@ -19,22 +21,26 @@ class IbsOscClientTestCase(unittest.TestCase):
         for date_format, expected_failure in test_cases:
             date_text: str = test_date.strftime(date_format)
             if expected_failure:
-                self.assertRaises(ValueError, ibs_client._parse_embargo_date, date_text)
+                self.assertRaises(ValueError, self.ibs_client._parse_embargo_date, date_text)
             else:
-                ibs_client._parse_embargo_date(date_text)
+                self.ibs_client._parse_embargo_date(date_text)
 
     def test_parse_xml_issue(self):
-        ibs_client: IbsOscClient = IbsOscClient()
-
         bugzilla_issue_text: str = '<issue id="1234567" tracker="bnc"></issue>'
         bugzilla_issue_xml: ET.Element = ET.fromstring(bugzilla_issue_text)
-        bugzilla_issue_id: str = ibs_client._parse_xml_issue(bugzilla_issue_xml)
+        bugzilla_issue_id: str = self.ibs_client._parse_xml_issue(bugzilla_issue_xml)
         self.assertEqual(bugzilla_issue_id, "bnc#1234567")
 
         cve_issue_text: str = '<issue id="2021-00001" tracker="cve" />'
         cve_issue_xml: ET.Element = ET.fromstring(cve_issue_text)
-        cve_issue_id: str = ibs_client._parse_xml_issue(cve_issue_xml)
+        cve_issue_id: str = self.ibs_client._parse_xml_issue(cve_issue_xml)
         self.assertEqual(cve_issue_id, "CVE-2021-00001")
+
+    def test_get_patchinfo_issues_ids(self):
+        ids: set [str] = self.ibs_client._get_patchinfo_issues_ids("./tests/testdata/_patchinfo")
+        # 4 CVE and 4 bnc expected
+        self.assertEqual(len({id for id in ids if id.startswith("CVE-")}), 4)
+        self.assertEqual(len({id for id in ids if id.startswith("bnc#")}), 4)
 
 if __name__ == '__main__':
     unittest.main()
