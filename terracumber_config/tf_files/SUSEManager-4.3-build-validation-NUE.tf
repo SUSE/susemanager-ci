fee7bc0580f243d459fc660fed11b504894423fe
maximenoel8 <55169628+maximenoel8@users.noreply.github.com>
2024-04-18 12:54:51 +1200

    add private ip for sle12 and 15 terminal (#1208)

--- terracumber_config/tf_files/SUSEManager-4.3-build-validation-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-build-validation-NUE.tf
@@ -1355,6 +1355,8 @@ module "sles12sp5-terminal" {
     manufacturer       = "Supermicro"
     product            = "X9DR3-F"
   }
+  private_ip         = 5
+  private_name       = "sle12sp5terminal"
 }
 
 module "sles15sp4-buildhost" {
@@ -1387,6 +1389,8 @@ module "sles15sp4-terminal" {
     manufacturer       = "HP"
     product            = "ProLiant DL360 Gen9"
   }
+  private_ip         = 6
+  private_name       = "sle15sp4terminal"
 }
 
 module "monitoring-server" {
