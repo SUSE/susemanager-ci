fd24eda644c69599957afbbb3bf710f19cec20cc
Maxime Noel <maxime.noel@suse.com>
2024-05-13 11:36:17 +1200

    Change images list

--- terracumber_config/tf_files/SUSEManager-5.0-build-image-AWS.tf
+++ terracumber_config/tf_files/SUSEManager-5.0-build-image-AWS.tf
@@ -146,8 +146,8 @@ module "base" {
   name_prefix              = var.NAME_PREFIX
   mirror                   = var.MIRROR
   testsuite                = true
-
-  images = ["rocky8", "opensuse155o", "sles15sp5o", "sles15sp4o", "ubuntu2204"]
+  images      = [ "opensuse155o"]
+#   images = ["rocky8", "opensuse155o", "sles15sp5o", "sles15sp4o", "ubuntu2204"]
   provider_settings = {
     availability_zone = var.AVAILABILITY_ZONE
     region            = var.REGION
