a07832235c761ccb97b9e09823634d6abe4177bc
Maxime Noel <maxime.noel@suse.com>
2024-05-29 14:52:15 +1200

    Change proxy product version

--- terracumber_config/tf_files/SUSEManager-5.0-build-image-AWS.tf
+++ terracumber_config/tf_files/SUSEManager-5.0-build-image-AWS.tf
@@ -216,7 +216,7 @@ module "proxy" {
   source                    = "./modules/proxy"
   base_configuration        = module.base.configuration
   server_configuration      = module.server.configuration
-  product_version           = "head"
+  product_version           = "4.3"
   name                      = "proxy"
   proxy_registration_code   = var.PROXY_REGISTRATION_CODE
 
