6b219a351c54d233f8e3ad472cf5c7677d0ab65d
Dominik Gedon <dominik.gedon@suse.com>
2024-06-18 11:57:11 +0200

    BV: Increase `repository_disk_size` to 3GB
    We have more products to sync and therefore we need more disk spaces
    for the 4.3 and the Uyuni BV environment.
    
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- terracumber_config/tf_files/SUSEManager-4.3-build-validation-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-build-validation-NUE.tf
@@ -177,7 +177,7 @@ module "server" {
 
   server_mounted_mirror          = "minima-mirror-ci-bv.mgr.suse.de"
   main_disk_size                 = 20
-  repository_disk_size           = 2048
+  repository_disk_size           = 3072
   database_disk_size             = 150
   java_debugging                 = false
   auto_accept                    = false
