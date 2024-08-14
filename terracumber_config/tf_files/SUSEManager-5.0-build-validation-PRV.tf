be81af0e76970386fa9ce64690d4ad3f3b14eba8
Dominik Gedon <dominik.gedon@suse.com>
2024-08-14 13:12:45 +0200

    Add missing product_version to s390 Minions
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- terracumber_config/tf_files/SUSEManager-5.0-build-validation-PRV.tf
+++ terracumber_config/tf_files/SUSEManager-5.0-build-validation-PRV.tf
@@ -871,6 +871,7 @@ module "opensuse156arm-minion" {
 module "sles15sp5s390-minion" {
   source             = "./backend_modules/feilong/host"
   base_configuration = module.base_s390.configuration
+  product_version    = "5.0-released"
 
   name               = "min-sles15sp5s390"
   image              = "s15s5-minimal-2part-xfs"
@@ -1483,6 +1484,7 @@ module "opensuse156arm-sshminion" {
 module "sles15sp5s390-sshminion" {
   source             = "./backend_modules/feilong/host"
   base_configuration = module.base_s390.configuration
+  product_version    = "5.0-released"
 
   name               = "minssh-sles15sp5s390"
   image              = "s15s5-minimal-2part-xfs"
