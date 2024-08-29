9009e442ba1eaf5fef2a3827d9d926a1f0dfff10
Dominik Gedon <dominik.gedon@suse.com>
2024-08-29 09:44:16 +0200

    Fix container URL in the HEAD CI
    Currently we see duplicate parts of the URL due to some parts will get
    auto-appended with the `mgr*` tools.
    
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- terracumber_config/tf_files/SUSEManager-Head-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Head-NUE.tf
@@ -143,7 +143,7 @@ module "cucumber_testsuite" {
       main_disk_size = 500
       login_timeout = 28800
       runtime = "podman"
-      container_repository = "registry.suse.de/devel/galaxy/manager/head/containerfile/suse/manager/5.0/x86_64"
+      container_repository = "registry.suse.de/devel/galaxy/manager/head/containerfile"
       container_tag = "latest"
     }
     proxy_containerized = {
@@ -156,7 +156,7 @@ module "cucumber_testsuite" {
       additional_packages = [ "venv-salt-minion" ]
       install_salt_bundle = true
       runtime = "podman"
-      container_repository = "registry.suse.de/devel/galaxy/manager/head/containerfile/suse/manager/5.0/x86_64"
+      container_repository = "registry.suse.de/devel/galaxy/manager/head/containerfile"
       container_tag = "latest"
     }
     suse-minion = {
