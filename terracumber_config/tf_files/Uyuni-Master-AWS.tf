42ab6156bbb6a033216217c4e1bcf8b63074c389
Michael Calmer <mc@suse.de>
2024-08-30 13:36:09 +0200

    get image from registry.opensuse.org instead from own mirror
    Reason: our mirror has a self signed certificate which is not trusted
    by default

--- terracumber_config/tf_files/Uyuni-Master-AWS.tf
+++ terracumber_config/tf_files/Uyuni-Master-AWS.tf
@@ -157,7 +157,7 @@ module "cucumber_testsuite" {
         overwrite_fqdn = "uyuni-master-srv.sumaci.aws"
       }
       runtime = "podman"
-      container_repository = "mirror.sumaci.aws/systemsmanagement/uyuni/master/containers_leap_15.6"
+      container_repository = "registry.opensuse.org/systemsmanagement/uyuni/master/containers_leap_15.6"
       container_tag = "latest"
       helm_chart_url = "oci://registry.opensuse.org/systemsmanagement/uyuni/master/charts/uyuni/server"
       login_timeout = 28800
@@ -172,7 +172,7 @@ module "cucumber_testsuite" {
       additional_packages = [ "venv-salt-minion" ]
       install_salt_bundle = true
       runtime = "podman"
-      container_repository = "mirror.sumaci.aws/systemsmanagement/uyuni/master/containers_leap_15.6"
+      container_repository = "registry.opensuse.org/systemsmanagement/uyuni/master/containers_leap_15.6"
       container_tag = "latest"
     }
     suse-minion = {
