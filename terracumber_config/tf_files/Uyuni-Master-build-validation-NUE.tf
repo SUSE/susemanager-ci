025cf13f5798681afbe230c331de3645411d6e59
Dominik Gedon <dominik.gedon@suse.com>
2024-09-09 16:08:59 +0200

    Remove openSUSE Leap 15.4 due to EOL
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- terracumber_config/tf_files/Uyuni-Master-build-validation-NUE.tf
+++ terracumber_config/tf_files/Uyuni-Master-build-validation-NUE.tf
@@ -141,7 +141,7 @@ module "base_arm" {
   name_prefix = "uyuni-bv-master-"
   use_avahi   = false
   domain      = "mgr.suse.de"
-  images      = [ "opensuse154armo", "opensuse155armo", "opensuse156armo" ]
+  images      = [ "opensuse155armo", "opensuse156armo" ]
 
   mirror = "minima-mirror-ci-bv.mgr.suse.de"
   use_mirror_images = true
@@ -549,30 +549,6 @@ module "debian12_minion" {
 
   additional_packages = [ "venv-salt-minion" ]
   install_salt_bundle = true
-}
-
-module "opensuse154arm_minion" {
-  providers = {
-    libvirt = libvirt.suma-arm
-  }
-  source             = "./modules/minion"
-  base_configuration = module.base_arm.configuration
-  product_version    = "uyuni-master"
-  name               = "nue-min-opensuse154arm"
-  image              = "opensuse154armo"
-  provider_settings = {
-    mac                = "aa:b2:93:02:01:bf"
-    overwrite_fqdn     = "uyuni-bv-master-min-opensuse154arm.mgr.suse.de"
-    memory             = 2048
-    vcpu               = 2
-    xslt               = file("../../susemanager-ci/terracumber_config/tf_files/common/tune-aarch64.xslt")
-  }
-  server_configuration = {
-    hostname = "uyuni-bv-master-pxy.mgr.suse.de"
-  }
-  auto_connect_to_master  = false
-  use_os_released_updates = false
-  ssh_key_path            = "./salt/controller/id_rsa.pub"
 }
 
 module "opensuse155arm_minion" {
@@ -1045,26 +1021,6 @@ module "debian12_ssh_minion" {
   install_salt_bundle = true
 }
 
-module "opensuse154arm_ssh_minion" {
-  providers = {
-    libvirt = libvirt.suma-arm
-  }
-  source             = "./modules/sshminion"
-  base_configuration = module.base_arm.configuration
-  product_version    = "uyuni-master"
-  name               = "nue-minssh-opensuse154arm"
-  image              = "opensuse154armo"
-  provider_settings = {
-    mac                = "aa:b2:93:02:01:df"
-    overwrite_fqdn     = "uyuni-bv-master-minssh-opensuse154arm.mgr.suse.de"
-    memory             = 2048
-    vcpu               = 2
-    xslt               = file("../../susemanager-ci/terracumber_config/tf_files/common/tune-aarch64.xslt")
-  }
-  use_os_released_updates = false
-  ssh_key_path            = "./salt/controller/id_rsa.pub"
-}
-
 module "opensuse155arm_ssh_minion" {
   providers = {
     libvirt = libvirt.suma-arm
@@ -1370,9 +1326,6 @@ module "controller" {
 
   debian12_minion_configuration    = module.debian12_minion.configuration
   debian12_sshminion_configuration = module.debian12_ssh_minion.configuration
-
-  opensuse154arm_minion_configuration    = module.opensuse154arm_minion.configuration
-  opensuse154arm_sshminion_configuration = module.opensuse154arm_ssh_minion.configuration
 
   opensuse155arm_minion_configuration    = module.opensuse155arm_minion.configuration
   opensuse155arm_sshminion_configuration = module.opensuse155arm_ssh_minion.configuration
