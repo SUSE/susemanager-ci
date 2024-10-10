d6a018a2f00f0fdbfc7c5e4e03db7187067fa7bb
Oscar Barrios <obarrios@suse.com>
2024-10-10 10:33:29 +0200

    Temporary skip rubocop tests for Ruby 3.3

--- jenkins_pipelines/manager_prs/tests_files/rubocop.sh
+++ jenkins_pipelines/manager_prs/tests_files/rubocop.sh
@@ -5,6 +5,11 @@ rubocop_file="testsuite/.rubocop.yml"
 if [[ -f "$rubocop_file" ]]; then
   # Extract the Ruby version from the file
   ruby_version=$(grep "TargetRubyVersion:" "$rubocop_file" | awk '{print $2}')
+  # Temporary skip the check for Ruby 3.3
+  if [[ "$ruby_version" == "3.3" ]]; then
+    echo "Skipping Rubocop for Ruby 3.3, until sumadockers support it."
+    exit 0
+  fi
   if [[ -n "$ruby_version" ]]; then
     rubocop.ruby"$ruby_version" -v
     cd testsuite
