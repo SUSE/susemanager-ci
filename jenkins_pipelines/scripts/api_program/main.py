5c3a3eb3e5b11e7bd0f69cd059a158b35cd0f98e
Maxime Noel <maxime.noel@suse.com>
2024-10-29 12:57:37 +1300

    delete sym link

--- jenkins_pipelines/scripts/api_program/main.py
+++ jenkins_pipelines/scripts/api_program/main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     parser.add_argument("--url", required=True, help="The URL of the SUSE Manager XML-RPC API.")
     parser.add_argument("--mode", choices=["delete_users", "delete_activation_keys", "delete_config_projects", "delete_software_channels", "delete_systems", "delete_repositories", "full_cleanup", "delete_salt_keys", "delete_known_hosts", "update_custom_repositories", "delete_distributions"], help="The mode of operation.")
     parser.add_argument("--tf-resources-to-delete", type=str, nargs='*', choices=['proxy', 'monitoring-server', 'retail'], default=[],
-                         help='List of default modules to force deletion')
+                        help='List of default modules to force deletion')
     parser.add_argument("--product_version", type=str, choices=['5.0','4.3'])
 
     args = parser.parse_args()
@@ -56,6 +56,7 @@ if __name__ == "__main__":
             logger.warning("Distribution delete to do 4.3")
         elif (args.product_version == "5.0"):
             run_ssh_command(manager_url, "rm -rf /var/lib/containers/storage/volumes/srv-www/_data/distributions/*")
+            run_ssh_command(manager_url, "rm -rf /var/lib/containers/storage/volumes/srv-www/_data/htdocs/pub/*iso")
         logger.info("Deleted distributions directory on server")
 
     elif args.mode == "update_custom_repositories":
