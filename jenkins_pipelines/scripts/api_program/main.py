717e97041d536fdfcfbc0adb10e16104b332bbd3
Maxime Noel <maxime.noel@suse.com>
2024-10-29 10:43:43 +1300

    Add delete_distributions to choices.

--- jenkins_pipelines/scripts/api_program/main.py
+++ jenkins_pipelines/scripts/api_program/main.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Manage SUSE Manager API actions.")
     parser.add_argument("--url", required=True, help="The URL of the SUSE Manager XML-RPC API.")
-    parser.add_argument("--mode", choices=["delete_users", "delete_activation_keys", "delete_config_projects", "delete_software_channels", "delete_systems", "delete_repositories", "full_cleanup", "delete_salt_keys", "delete_known_hosts", "update_custom_repositories"], help="The mode of operation.")
+    parser.add_argument("--mode", choices=["delete_users", "delete_activation_keys", "delete_config_projects", "delete_software_channels", "delete_systems", "delete_repositories", "full_cleanup", "delete_salt_keys", "delete_known_hosts", "update_custom_repositories", "delete_distributions"], help="The mode of operation.")
     parser.add_argument("--tf-resources-to-delete", type=str, nargs='*', choices=['proxy', 'monitoring-server', 'retail'], default=[],
                          help='List of default modules to force deletion')
     parser.add_argument("--product_version", type=str, choices=['5.0','4.3'])
@@ -50,7 +50,7 @@ if __name__ == "__main__":
             run_ssh_command(manager_url, "rm /var/lib/containers/storage/volumes/var-salt/_data/.ssh/known_hosts")
         logger.info("Deleted known_hosts file on server")
 
-    elif args.mode == "delete_known_hosts":
+    elif args.mode == "delete_distributions":
         ## TODO: add delete distribution for 4.3
         if (args.product_version == "4.3"):
             logger.warning("Distribution delete to do 4.3")
