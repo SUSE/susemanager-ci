3cc19050d67ce0eae7183a6e3a188af02a4af216
Maxime Noel <maxime.noel@suse.com>
2024-10-29 14:15:07 +1300

    Transform ssh module into a class and update the main program

--- jenkins_pipelines/scripts/api_program/main.py
+++ jenkins_pipelines/scripts/api_program/main.py
@@ -2,7 +2,7 @@
 import argparse
 import logging
 from suse_manager_api import ResourceManager
-from suse_manager_ssh import run_ssh_command, copy_file_over_ssh
+from suse_manager_ssh import SSHClientManager
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -12,55 +12,60 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Manage SUSE Manager API actions.")
     parser.add_argument("--url", required=True, help="The URL of the SUSE Manager XML-RPC API.")
     parser.add_argument("--mode", choices=["delete_users", "delete_activation_keys", "delete_config_projects", "delete_software_channels", "delete_systems", "delete_repositories", "full_cleanup", "delete_salt_keys", "delete_known_hosts", "update_custom_repositories", "delete_distributions"], help="The mode of operation.")
-    parser.add_argument("--tf-resources-to-delete", type=str, nargs='*', choices=['proxy', 'monitoring-server', 'retail'], default=[],
-                        help='List of default modules to force deletion')
-    parser.add_argument("--product_version", type=str, choices=['5.0','4.3'])
+    parser.add_argument("--tf-resources-to-delete", type=str, nargs='*', choices=['proxy', 'monitoring-server', 'retail'], default=[], help='List of default modules to force deletion')
+    parser.add_argument("--product_version", type=str, choices=['5.0', '4.3'])
 
     args = parser.parse_args()
-
     manager_url = args.url
 
+   # API part
     if args.mode in ["delete_users", "delete_activation_keys", "delete_config_projects", "delete_software_channels", "delete_systems", "delete_repositories", "full_cleanup", "delete_salt_keys"]:
         resource_manager = ResourceManager(manager_url, args.tf_resources_to_delete)
         resource_manager.get_session_key()
+        try:
+            if args.mode == "delete_users":
+                resource_manager.delete_users()
+            elif args.mode == "delete_activation_keys":
+                resource_manager.delete_activation_keys()
+            elif args.mode == "delete_config_projects":
+                resource_manager.delete_config_projects()
+            elif args.mode == "delete_software_channels":
+                resource_manager.delete_software_channels()
+            elif args.mode == "delete_systems":
+                resource_manager.delete_systems()
+            elif args.mode == "delete_repositories":
+                resource_manager.delete_channel_repos()
+            elif args.mode == "delete_salt_keys":
+                resource_manager.delete_salt_keys()
+            elif args.mode == "full_cleanup":
+                resource_manager.run()
+        finally:
+            resource_manager.logout_session()
+    # Server commands part
+    else:
+        # Initialize SSH Manager
+        ssh_manager = SSHClientManager(url=manager_url)
+        try:
+            if args.mode == "delete_known_hosts":
+                if args.product_version == "4.3":
+                    ssh_manager.run_command("rm /var/lib/salt/.ssh/known_hosts")
+                elif args.product_version == "5.0":
+                    result = ssh_manager.run_command("ls -la /var/lib/containers/storage/volumes/var-salt/_data/.ssh/known_hosts")
+                    logger.info(f"Files in salt ssh before cleanup : {result}")
+                    ssh_manager.run_command("rm /var/lib/containers/storage/volumes/var-salt/_data/.ssh/known_hosts")
+                    logger.info(f"Files in salt ssh after cleanup : {result}")
+                logger.info("Deleted known_hosts file on server")
 
-        if args.mode == "delete_users":
-            resource_manager.delete_users()
-        elif args.mode == "delete_activation_keys":
-            resource_manager.delete_activation_keys()
-        elif args.mode == "delete_config_projects":
-            resource_manager.delete_config_projects()
-        elif args.mode == "delete_software_channels":
-            resource_manager.delete_software_channels()
-        elif args.mode == "delete_systems":
-            resource_manager.delete_systems()
-        elif args.mode == "delete_repositories":
-            resource_manager.delete_channel_repos()
-        elif args.mode == "delete_salt_keys":
-            resource_manager.delete_salt_keys()
-        elif args.mode == "full_cleanup":
-            resource_manager.run()
+            elif args.mode == "delete_distributions":
+                if args.product_version == "4.3":
+                    logger.warning("Distribution delete to do 4.3")
+                elif args.product_version == "5.0":
+                    ssh_manager.run_command("rm -rf /var/lib/containers/storage/volumes/srv-www/_data/distributions/*")
+                    ssh_manager.run_command("rm -rf /var/lib/containers/storage/volumes/srv-www/_data/htdocs/pub/*iso")
+                logger.info("Deleted distributions directory on server")
 
-        resource_manager.logout_session()
-
-    elif args.mode == "delete_known_hosts":
-        if (args.product_version == "4.3"):
-            run_ssh_command(manager_url, "rm /var/lib/salt/.ssh/known_hosts")
-        elif (args.product_version == "5.0"):
-            result = run_ssh_command(manager_url, "ls -la /var/lib/containers/storage/volumes/var-salt/_data/.ssh/known_hosts")
-            logger.info(f"Files in salt ssh before cleanup : {result}")
-            run_ssh_command(manager_url, "rm /var/lib/containers/storage/volumes/var-salt/_data/.ssh/known_hosts")
-            logger.info(f"Files in salt ssh after cleanup : {result}")
-        logger.info("Deleted known_hosts file on server")
-
-    elif args.mode == "delete_distributions":
-        ## TODO: add delete distribution for 4.3
-        if (args.product_version == "4.3"):
-            logger.warning("Distribution delete to do 4.3")
-        elif (args.product_version == "5.0"):
-            run_ssh_command(manager_url, "rm -rf /var/lib/containers/storage/volumes/srv-www/_data/distributions/*")
-            run_ssh_command(manager_url, "rm -rf /var/lib/containers/storage/volumes/srv-www/_data/htdocs/pub/*iso")
-        logger.info("Deleted distributions directory on server")
-
-    elif args.mode == "update_custom_repositories":
-        copy_file_over_ssh(manager_url, "", "/root/spacewalk/testsuite/features/upload_files/custom_repositories.json")
+            elif args.mode == "update_custom_repositories":
+                ssh_manager.copy_file("", "/root/spacewalk/testsuite/features/upload_files/custom_repositories.json")
+        finally:
+            # Ensure the connection is closed
+            ssh_manager.close()
--- jenkins_pipelines/scripts/api_program/suse_manager_ssh.py
+++ jenkins_pipelines/scripts/api_program/suse_manager_ssh.py
@@ -7,112 +7,92 @@ from urllib.parse import urlparse
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Global variables
-username = "root"
-password = "linux"
-port = "22"
+class SSHClientManager:
+    def __init__(self, url, username="root", password="linux", port=22):
+        self.url = url
+        self.username = username
+        self.password = password
+        self.port = port
+        self.client = None
 
-def extract_ip_from_url(url):
-    # Parse the URL
-    parsed_url = urlparse(url)
+    def connect(self):
+        """Establishes an SSH connection to the server."""
+        if not self.client:
+            self.client = paramiko.SSHClient()
+            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            try:
+                logger.info(f"Connecting to server at: {self.url}")
+                self.client.connect(self.url, port=self.port, username=self.username, password=self.password)
+                logger.info("Connection successful.")
+            except paramiko.AuthenticationException:
+                logger.error("Authentication failed while connecting to the server.")
+                raise
+            except paramiko.SSHException as ssh_exception:
+                logger.error(f"SSH connection error: {str(ssh_exception)}")
+                raise
+            except Exception as e:
+                logger.error(f"Exception: {str(e)}")
+                raise
 
-    # Extract the hostname (which contains the IP address)
-    hostname = parsed_url.hostname
+    def close(self):
+        """Closes the SSH connection."""
+        if self.client:
+            self.client.close()
+            self.client = None
+            logger.info("Connection closed.")
 
-    # Validate and return the IP address
-    if hostname:
-        # Use a regular expression to validate the IP address format
-        ip_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
-        if ip_pattern.match(hostname):
-            return hostname
+    def run_command(self, command):
+        """
+        Runs a command on the remote server.
+
+        :param command: Command to execute on the server.
+        :return: Command output or error message.
+        """
+        try:
+            self.connect()
+            stdin, stdout, stderr = self.client.exec_command(command)
+            output = stdout.read().decode()
+            error = stderr.read().decode()
+
+            if error:
+                logger.error(f"Command error: {error}")
+                return f"Error: {error}"
+            logger.info(f"Command output: {output}")
+            return output
+        except Exception as e:
+            logger.error(f"Exception: {str(e)}")
+            return f"Exception: {str(e)}"
+
+    def copy_file(self, local_path, remote_path):
+        """
+        Copies a file to the remote server.
+
+        :param local_path: Path of the local file to copy.
+        :param remote_path: Destination path on the remote server.
+        :return: Success message or error details.
+        """
+        try:
+            self.connect()
+            sftp = self.client.open_sftp()
+            sftp.put(local_path, remote_path)
+            sftp.close()
+            logger.info(f"File {local_path} copied to {remote_path} on server {self.url}")
+            return "File copy successful"
+        except Exception as e:
+            logger.error(f"Exception during file copy: {str(e)}")
+            return f"Exception during file copy: {str(e)}"
+
+    @staticmethod
+    def extract_ip_from_url(url):
+        """Extracts and validates the IP address from a URL."""
+        parsed_url = urlparse(url)
+        hostname = parsed_url.hostname
+
+        if hostname:
+            ip_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
+            if ip_pattern.match(hostname):
+                return hostname
+            else:
+                return "Invalid IP address format"
         else:
-            return "Invalid IP address format"
-    else:
-        return "No hostname found in URL"
-
-def run_ssh_command(url, command):
-    """
-    Connects to a server using SSH and runs a command.
-
-    :param url: The server API URL.
-    :param command: The command to run on the remote server.
-    :return: The output of the command or an error message if connection fails.
-    """
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    private_key = paramiko.RSAKey(filename="/home/maxime/github/sumaform/salt/controller/id_rsa")
-
-    try:
-        logger.info(f"Connecting to server at: {url}")
-        # Attempt to connect to the server
-        client.connect(url, port=port, username=username, pkey=private_key)
-        logger.info("Connection successful.")
-
-        # Run the command
-        stdin, stdout, stderr = client.exec_command(command)
-        output = stdout.read().decode()
-        error = stderr.read().decode()
-
-        # Log and return the output or error
-        if error:
-            logger.error(f"Command error: {error}")
-            return f"Error: {error}"
-
-        logger.info(f"Command output: {output}")
-        return output
-
-    except paramiko.AuthenticationException:
-        logger.error("Authentication failed while connecting to the server.")
-        return "Authentication failed."
-
-    except paramiko.SSHException as ssh_exception:
-        logger.error(f"SSH connection error: {str(ssh_exception)}")
-        return f"SSH connection error: {str(ssh_exception)}"
-
-    except Exception as e:
-        logger.error(f"Exception: {str(e)}")
-        return f"Exception: {str(e)}"
-
-    finally:
-        client.close()
-
-def copy_file_over_ssh(url, local_path, remote_path):
-    """
-    Copy a file to a remote server via SSH.
-
-    :param url: The server API URL.
-    :param local_path: The local path of the file to copy.
-    :param remote_path: The destination path on the remote server.
-    :return: Success message or error details.
-    """
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    private_key = paramiko.RSAKey(filename="/home/maxime/github/sumaform/salt/controller/id_rsa")
-
-    try:
-        logger.info(f"Connecting to server at: {url} to copy file.")
-        # Attempt to connect to the server
-        client.connect(url, port=port, username=username, pkey=private_key)
-        logger.info("Connection successful for file transfer.")
-
-        # Open SFTP session and transfer the file
-        sftp = client.open_sftp()
-        sftp.put(local_path, remote_path)
-        sftp.close()
-        logger.info(f"File {local_path} copied to {remote_path} on server {url}")
-        return "File copy successful"
-
-    except paramiko.AuthenticationException:
-        logger.error("Authentication failed during file copy.")
-        return "Authentication failed during file copy."
-
-    except paramiko.SSHException as ssh_exception:
-        logger.error(f"SSH error during file copy: {str(ssh_exception)}")
-        return f"SSH error during file copy: {str(ssh_exception)}"
-
-    except Exception as e:
-        logger.error(f"Exception during file copy: {str(e)}")
-        return f"Exception during file copy: {str(e)}"
-
-    finally:
-        client.close()
+            return "No hostname found in URL"
