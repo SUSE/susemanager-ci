f9f129327687a697dac183803345f2bbf329c4dd
Maxime Noel <maxime.noel@suse.com>
2024-11-13 08:47:07 +1300

    Remove useless information

--- terracumber_config/tf_files/Uyuni-Master-NUE.tf
+++ terracumber_config/tf_files/Uyuni-Master-NUE.tf
@@ -234,16 +234,37 @@ module "cucumber_testsuite" {
         vcpu    = 4
         memory  = 4096
       }
-      additional_packages = [ "mkisofs" ]
     }
   }
-  
+
   provider_settings = {
     pool               = "ssd"
     network_name       = null
     bridge             = "br0"
     additional_network = "192.168.100.0/24"
   }
+}
+
+
+resource "null_resource" "configure_quality_intelligence" {
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ "echo export QUALITY_INTELLIGENCE=true >> ~/.bashrc",
+      "echo export PROMETHEUS_PUSH_GATEWAY_URL=${var.PROMETHEUS_PUSH_GATEWAY_URL} >> ~/.bashrc",
+      "source ~/.bashrc"
+    ]
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "linux"
+      host     = "${module.cucumber_testsuite.configuration.controller.hostname}"
+    }
+  }
+
 }
 
 output "configuration" {
