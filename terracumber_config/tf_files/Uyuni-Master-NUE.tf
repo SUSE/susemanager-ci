72a0217480fb445723bcf816ab5a1b2a6eae2d8d
Maxime Noel <maxime.noel@suse.com>
2024-11-12 14:47:37 +1300

    Align and rename

--- terracumber_config/tf_files/Uyuni-Master-NUE.tf
+++ terracumber_config/tf_files/Uyuni-Master-NUE.tf
@@ -104,113 +104,113 @@ module "cucumber_testsuite" {
 
   images = ["rocky8o", "opensuse155o", "ubuntu2204o", "sles15sp4o"]
 
-  use_avahi    = false
-  name_prefix  = "uyuni-ci-master-"
-  domain       = "mgr.suse.de"
-  from_email   = "root@suse.de"
+  use_avahi     = false
+  name_prefix   = "uyuni-ci-master-"
+  domain        = "mgr.suse.de"
+  from_email    = "root@suse.de"
 
-  no_auth_registry = "registry.mgr.suse.de"
-  auth_registry      = "registry.mgr.suse.de:5000/cucutest"
-  auth_registry_username = "cucutest"
-  auth_registry_password = "cucusecret"
-  git_profiles_repo      = "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles/internal_nue"
+  no_auth_registry          = "registry.mgr.suse.de"
+  auth_registry             = "registry.mgr.suse.de:5000/cucutest"
+  auth_registry_username    = "cucutest"
+  auth_registry_password    = "cucusecret"
+  git_profiles_repo         = "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles/internal_nue"
 
-  mirror                   = "minima-mirror-ci-bv.mgr.suse.de"
-  use_mirror_images        = true
-  server_http_proxy        = "http-proxy.mgr.suse.de:3128"
-  custom_download_endpoint = "ftp://minima-mirror-ci-bv.mgr.suse.de:445"
+  mirror                    = "minima-mirror-ci-bv.mgr.suse.de"
+  use_mirror_images         = true
+  server_http_proxy         = "http-proxy.mgr.suse.de:3128"
+  custom_download_endpoint  = "ftp://minima-mirror-ci-bv.mgr.suse.de:445"
 
   # when changing images, please also keep in mind to adjust the image matrix at the end of the README.
   host_settings = {
     controller = {
-      name = "controller"
+      name              = "controller"
       provider_settings = {
-        mac = "aa:b2:93:01:00:d0"
-        vcpu = 2
-        memory = 2048
+        mac     = "aa:b2:93:01:00:d0"
+        vcpu    = 2
+        memory  = 2048
       }
     }
     server = {
-      name = "server"
-      provider_settings = {
-        mac = "aa:b2:93:01:00:d1"
-        vcpu = 4
-        memory = 16384
+      name                  = "server"
+      provider_settings     = {
+        mac     = "aa:b2:93:01:00:d1"
+        vcpu    = 4
+        memory  = 16384
       }
-      main_disk_size       = 20
-      repository_disk_size = 300
-      database_disk_size   = 50
-      login_timeout        = 28800
+      main_disk_size        = 20
+      repository_disk_size  = 300
+      database_disk_size    = 50
+      login_timeout         = 28800
     }
     proxy = {
-      name = "proxy"
+      name              = "proxy"
       provider_settings = {
-        mac = "aa:b2:93:01:00:d2"
-        vcpu = 2
-        memory = 2048
+        mac     = "aa:b2:93:01:00:d2"
+        vcpu    = 2
+        memory  = 2048
       }
     }
     suse_minion = {
-      image = "opensuse155o"
-      name = "suse-minion"
+      name              = "suse-minion"
+      image             = "opensuse155o"
       provider_settings = {
-        mac = "aa:b2:93:01:00:d6"
-        vcpu = 2
-        memory = 2048
+        mac     = "aa:b2:93:01:00:d6"
+        vcpu    = 2
+        memory  = 2048
       }
     }
     suse_sshminion = {
-      image = "opensuse155o"
-      name = "suse-sshminion"
+      name              = "suse-sshminion"
+      image             = "opensuse155o"
       provider_settings = {
-        mac = "aa:b2:93:01:00:d8"
-        vcpu = 2
-        memory = 2048
+        mac     = "aa:b2:93:01:00:d8"
+        vcpu    = 2
+        memory  = 2048
       }
     }
     rhlike_minion = {
-      image = "rocky8o"
-      name = "rhlike-minion"
+      name              = "rhlike-minion"
+      image             = "rocky8o"
       provider_settings = {
-        mac = "aa:b2:93:01:00:d9"
+        mac     = "aa:b2:93:01:00:d9"
         // Since start of May we have problems with the instance not booting after a restart if there is only a CPU and only 1024Mb for RAM
         // Also, openscap cannot run with less than 1.25 GB of RAM
-        vcpu = 2
-        memory = 2048
+        vcpu    = 2
+        memory  = 2048
       }
     }
     deblike_minion = {
-      name = "deblike-minion"
-      image = "ubuntu2204o"
+      name              = "deblike-minion"
+      image             = "ubuntu2204o"
       provider_settings = {
-        mac = "aa:b2:93:01:00:db"
-        vcpu = 2
-        memory = 2048
+        mac     = "aa:b2:93:01:00:db"
+        vcpu    = 2
+        memory  = 2048
       }
     }
     build_host = {
-      image = "sles15sp4o"
-      name = "build-minion"
+      name              = "build-host"
+      image             = "sles15sp4o"
       provider_settings = {
-        mac = "aa:b2:93:01:00:dd"
-        vcpu = 2
-        memory = 2048
+        mac     = "aa:b2:93:01:00:dd"
+        vcpu    = 2
+        memory  = 2048
       }
     }
     pxeboot_minion = {
-      name = "pxeboot-minion"
-      image = "sles15sp4o"
-      additional_packages = [ "venv-salt-minion" ]
-      install_salt_bundle = true
+      name                  = "pxeboot-minion"
+      image                 = "sles15sp4o"
+      additional_packages   = [ "venv-salt-minion" ]
+      install_salt_bundle   = true
     }
     kvm-host = {
+      name  = "kvm-minion"
       image = "opensuse155o"
-      name = "kvm-minion"
       
       provider_settings = {
-        mac = "aa:b2:93:01:00:de"
-        vcpu = 4
-        memory = 4096
+        mac     = "aa:b2:93:01:00:de"
+        vcpu    = 4
+        memory  = 4096
       }
       additional_packages = [ "mkisofs" ]
     }
