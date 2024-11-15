efa1c1759f2f6dc7dfe89afff4af8a857237b997
Paolo Invernizzi <paolo.invernizzi@suse.com>
2024-11-15 12:10:09 +0100

    Check API key and add comments

--- jenkins_pipelines/scripts/bsc_list_generator/bugzilla_client.py
+++ jenkins_pipelines/scripts/bsc_list_generator/bugzilla_client.py
@@ -5,7 +5,10 @@ _SUSE_BUGZILLA_BASE_URL = "https://bugzilla.suse.com"
 
 class BugzillaClient:
 
+    # api_key is needed for actual API calls
     def __init__(self, api_key: str, base_url: str = _SUSE_BUGZILLA_BASE_URL, api_type: str = "rest"):
+        if not api_key:
+            raise ValueError("api_key is None or empty")
         # private
         self._api_key: str = api_key
         self._base_url: str = base_url
@@ -16,6 +19,7 @@ class BugzillaClient:
         self.show_bug_url: str = f"{base_url}/show_bug.cgi"
 
     def get_bugs(self, **kwargs) -> list[dict[str, Any]]:
+        # drops CLI args that have not beend used and have no default
         additional_params: dict[str, Any] = { k: v for k, v in kwargs.items() if v is not None }
         response: requests.Response = requests.get(self._bugs_endpoint, params={**self._params, **additional_params})
         if not response.ok:
