647cb9baf9fba6b53e56862cdc9473e8254d321f
Paolo Invernizzi <paolo.invernizzi@suse.com>
2024-11-15 12:10:09 +0100

    Add _bug_under_embargo unit tests

--- jenkins_pipelines/scripts/tests/test_bugzilla_client.py
+++ jenkins_pipelines/scripts/tests/test_bugzilla_client.py
@@ -20,6 +20,7 @@ class BugzillaClientTestCase(unittest.TestCase):
         mock_api_call.assert_called_once()
         # check None keys are dropped
         mock_api_call.assert_called_with(self.bugzilla_client._bugs_endpoint, params = {'Bugzilla_api_key': 'test_key', "product": "Test Product"})
+        # embargoed bug should have been dropped
         self.assertEqual(len(bugs), 3)
         for i in range(len(bugs)):
             bug: dict[str, Any] = bugs[i]
@@ -41,6 +42,38 @@ class BugzillaClientTestCase(unittest.TestCase):
 
         # first line is not ---------------- 
         self.assertRaises(ValueError, self.bugzilla_client._parse_release_notes, './tests/testdata/test_invalid_release_notes.changes')
+
+    def test_bug_under_embargo(self):
+        bsc: dict[str, any] = {
+            "classification": "Test",
+            "component": "Test components",
+            "creation_time": "2024-03-03T00:00:00Z",
+            "creator": "tester@suse.com",
+            "deadline": None,
+            "depends_on": [],
+            "id": 3,
+            "is_cc_accessible": True,
+            "is_confirmed": True,
+            "is_creator_accessible": True,
+            "is_open": True,
+            "priority": "P0 - Critical",
+            "product": "Test Product",
+            "remaining_time": 0,
+            "resolution": "",
+            "severity": "High",
+            "status": "CONFIRMED",
+            "summary": "VUL-0: EMBARGOED: Test BSC",
+            "version": "Test"
+        }
+        # explicit mention in the summary
+        self.assertTrue(self.bugzilla_client._bug_under_embargo(bsc))
+        # no embargo mention
+        bsc['summary'] = "CVE 123456789: Test BSC"
+        self.assertFalse(self.bugzilla_client._bug_under_embargo(bsc))
+        bsc['summary'] = "Test BSC"
+        self.assertFalse(self.bugzilla_client._bug_under_embargo(bsc))
+
+        
 
 if __name__ == '__main__':
     unittest.main()
