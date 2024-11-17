4e3656f3543c2d5bd26e2e8a1c6af94a50aa1cc9
Maxime Noel <55169628+maximenoel8@users.noreply.github.com>
2024-11-18 09:25:44 +1300

    Rename uyuni master podman nue modules and hostnames (#1464)

--- terracumber_config/tf_files/Uyuni-Master-podman.tf
+++ terracumber_config/tf_files/Uyuni-Master-podman.tf
@@ -107,26 +107,26 @@ module "cucumber_testsuite" {
   cc_username = var.SCC_USER
   cc_password = var.SCC_PASSWORD
 
-  images = ["rocky8o", "opensuse155o", "leapmicro55o", "ubuntu2204o", "sles15sp4o"]
+  images        = ["rocky8o", "opensuse155o", "leapmicro55o", "ubuntu2204o", "sles15sp4o"]
 
-  use_avahi    = false
-  name_prefix  = "uyuni-master-podman-"
-  domain       = "mgr.suse.de"
-  from_email   = "root@suse.de"
+  use_avahi     = false
+  name_prefix   = "uyuni-ci-master-podman-"
+  domain        = "mgr.suse.de"
+  from_email    = "root@suse.de"
 
-  no_auth_registry       = "registry.mgr.suse.de"
-  auth_registry          = "registry.mgr.suse.de:5000/cucutest"
-  auth_registry_username = "cucutest"
-  auth_registry_password = "cucusecret"
-  git_profiles_repo      = "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles/internal_nue"
+  no_auth_registry         = "registry.mgr.suse.de"
+  auth_registry            = "registry.mgr.suse.de:5000/cucutest"
+  auth_registry_username   = "cucutest"
+  auth_registry_password   = "cucusecret"
+  git_profiles_repo        = "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles/internal_nue"
 
-  container_server = true
-  container_proxy  = true
+  container_server         = true
+  container_proxy          = true
 
   mirror                   = "minima-mirror-ci-bv.mgr.suse.de"
   use_mirror_images        = true
 
-  server_http_proxy          = "http-proxy.mgr.suse.de:3128"
+  server_http_proxy        = "http-proxy.mgr.suse.de:3128"
   custom_download_endpoint = "ftp://minima-mirror-ci-bv.mgr.suse.de:445"
 
   # when changing images, please also keep in mind to adjust the image matrix at the end of the README.
@@ -141,43 +141,37 @@ module "cucumber_testsuite" {
         mac = "aa:b2:93:01:00:21"
         memory = 16384
       }
-      runtime = "podman"
-      container_repository = "registry.opensuse.org/systemsmanagement/uyuni/master/containers"
-      container_tag = "latest"
-      helm_chart_url = "oci://registry.opensuse.org/systemsmanagement/uyuni/master/charts/uyuni/server"
-      login_timeout = 28800
+      runtime               = "podman"
+      container_repository  = "registry.opensuse.org/systemsmanagement/uyuni/master/containers"
+      container_tag         = "latest"
+      helm_chart_url        = "oci://registry.opensuse.org/systemsmanagement/uyuni/master/charts/uyuni/server"
+      main_disk_size        = 50
+      repository_disk_size  = 150
+      database_disk_size    = 50
+      login_timeout         = 28800
     }
     proxy_containerized = {
       provider_settings = {
         mac = "aa:b2:93:01:00:22"
       }
-      additional_packages = [ "venv-salt-minion" ]
-      install_salt_bundle = true
-      runtime = "podman"
-      container_repository = "registry.opensuse.org/systemsmanagement/uyuni/master/containers"
-      container_tag = "latest"
+      runtime               = "podman"
+      container_repository  = "registry.opensuse.org/systemsmanagement/uyuni/master/containers"
+      container_tag         = "latest"
     }
-    suse-minion = {
-      image = "opensuse155o"
-      name = "min-suse"
+    suse_minion = {
+      image             = "opensuse155o"
       provider_settings = {
         mac = "aa:b2:93:01:00:26"
       }
-      additional_packages = [ "venv-salt-minion" ]
-      install_salt_bundle = true
     }
-    suse-sshminion = {
-      image = "opensuse155o"
-      name = "minssh-suse"
+    suse_sshminion = {
+      image             = "opensuse155o"
       provider_settings = {
         mac = "aa:b2:93:01:00:28"
       }
-      additional_packages = [ "venv-salt-minion", "iptables" ]
-      install_salt_bundle = true
     }
-    redhat-minion = {
-      image = "rocky8o"
-      name = "min-rocky8"
+    rhlike_minion = {
+      image             = "rocky8o"
       provider_settings = {
         mac = "aa:b2:93:01:00:2a"
         // Since start of May we have problems with the instance not booting after a restart if there is only a CPU and only 1024Mb for RAM
@@ -185,54 +179,39 @@ module "cucumber_testsuite" {
         vcpu = 2
         memory = 2048
       }
-      additional_packages = [ "venv-salt-minion" ]
-      install_salt_bundle = true
     }
-    debian-minion = {
-      image = "ubuntu2204o"
-      name = "min-ubuntu2204"
+    deblike_minion = {
+      image             = "ubuntu2204o"
       provider_settings = {
         mac = "aa:b2:93:01:00:2b"
       }
-      additional_packages = [ "venv-salt-minion" ]
-      install_salt_bundle = true
     }
-    build-host = {
-      image = "sles15sp4o"
-      name = "min-build"
+    build_host = {
+      image             = "sles15sp4o"
       provider_settings = {
         mac = "aa:b2:93:01:00:2d"
         memory = 2048
       }
-      additional_packages = [ "venv-salt-minion" ]
-      install_salt_bundle = true
     }
-    pxeboot-minion = {
+    pxeboot_minion = {
       image = "sles15sp4o"
-      additional_packages = [ "venv-salt-minion" ]
-      install_salt_bundle = true
     }
-    dhcp-dns = {
-      name = "dhcp-dns"
-      image = "opensuse155o"
+    dhcp_dns = {
+      name       = "dhcp-dns"
+      image      = "opensuse155o"
       hypervisor = {
         host        = "suma-01.mgr.suse.de"
         user        = "root"
         private_key = file("~/.ssh/id_rsa")
       }
     }
-    kvm-host = {
-      image = "opensuse155o"
-      name = "min-kvm"
-
+    kvm_host = {
+      image             = "opensuse155o"
       provider_settings = {
         mac = "aa:b2:93:01:00:2e"
       }
-      additional_packages = [ "venv-salt-minion" ]
-      install_salt_bundle = true
     }
   }
-
   provider_settings = {
     pool               = "ssd"
     network_name       = null
