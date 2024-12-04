8f2d4113ae891e497efa5e30f9a64b226d27682e
Maxime Noel <maxime.noel@suse.com>
2024-12-05 09:48:28 +1300

    update public ip jenkins

--- terracumber_config/tf_files/aws_mirror.tf
+++ terracumber_config/tf_files/aws_mirror.tf
@@ -126,7 +126,7 @@ provider "aws" {
 
 module "base" {
   source = "./modules/base"
-
+  product_version           = "5.0-released"
   cc_username = var.SCC_USER
   cc_password = var.SCC_PASSWORD
   name_prefix = var.NAME_PREFIX
