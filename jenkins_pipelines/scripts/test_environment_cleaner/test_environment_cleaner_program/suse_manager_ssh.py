ba80a92d8048c9b0c0288e559929be9e5aa769d3
Maxime Noel <maxime.noel@suse.com>
2024-12-11 13:17:44 +1300

    Refactor ssh python program

--- jenkins_pipelines/scripts/test_environment_cleaner/test_environment_cleaner_program/suse_manager_ssh.py
+++ jenkins_pipelines/scripts/test_environment_cleaner/test_environment_cleaner_program/suse_manager_ssh.py
@@ -9,78 +9,118 @@ logger = logging.getLogger(__name__)
 
 class SSHClientManager:
     def __init__(self, url, username="root", password="linux", port=22):
-        self.url = url
+        self.url = self.extract_ip_from_url(url)
         self.username = username
         self.password = password
         self.port = port
-        self.client = None
+        self._client = None
 
-    def connect(self):
+    def _connect(self):
         """Establishes an SSH connection to the server."""
-        if not self.client:
-            self.client = paramiko.SSHClient()
-            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        if not self._client:
+            self._client = paramiko.SSHClient()
+            self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             try:
-                logger.info(f"Connecting to server at: {self.url}")
-                self.client.connect(self.url, port=self.port, username=self.username, password=self.password)
+                logger.info(f"Connecting to server at: {self.url}:{self.port}")
+                self._client.connect(self.url, port=self.port, username=self.username, password=self.password)
                 logger.info("Connection successful.")
             except paramiko.AuthenticationException:
                 logger.error("Authentication failed while connecting to the server.")
                 raise
             except paramiko.SSHException as ssh_exception:
-                logger.error(f"SSH connection error: {str(ssh_exception)}")
+                logger.error(f"SSH connection error: {ssh_exception}")
                 raise
             except Exception as e:
-                logger.error(f"Exception: {str(e)}")
+                logger.error(f"Unexpected exception during SSH connection: {e}")
                 raise
 
-    def close(self):
+    def _close(self):
         """Closes the SSH connection."""
-        if self.client:
-            self.client.close()
-            self.client = None
-            logger.info("Connection closed.")
+        if self._client:
+            self._client.close()
+            self._client = None
+            logger.info("SSH connection closed.")
 
-    def run_command(self, command):
+    def _run_command(self, command):
         """
         Runs a command on the remote server.
 
         :param command: Command to execute on the server.
         :return: Command output or error message.
         """
+        self._connect()
         try:
-            self.connect()
-            stdin, stdout, stderr = self.client.exec_command(command)
-            output = stdout.read().decode()
-            error = stderr.read().decode()
+            logger.info(f"Executing command: {command}")
+            stdin, stdout, stderr = self._client.exec_command(command)
+            output = stdout.read().decode().strip()
+            error = stderr.read().decode().strip()
 
             if error:
                 logger.error(f"Command error: {error}")
-                return f"Error: {error}"
+                return error
             logger.info(f"Command output: {output}")
             return output
         except Exception as e:
-            logger.error(f"Exception: {str(e)}")
-            return f"Exception: {str(e)}"
+            logger.error(f"Error while executing command: {e}")
+            raise
+        finally:
+            stdin.close()
 
-    def copy_file(self, local_path, remote_path):
+    def delete_known_hosts(self, version):
         """
-        Copies a file to the remote server.
+        Deletes the known_hosts file based on the product version.
 
-        :param local_path: Path of the local file to copy.
-        :param remote_path: Destination path on the remote server.
-        :return: Success message or error details.
+        :param version: Product version to determine file path.
         """
         try:
-            self.connect()
-            sftp = self.client.open_sftp()
-            sftp.put(local_path, remote_path)
-            sftp.close()
-            logger.info(f"File {local_path} copied to {remote_path} on server {self.url}")
-            return "File copy successful"
+            self._connect()
+            if version == "4.3":
+                self._run_command("rm /var/lib/salt/.ssh/known_hosts")
+            elif version in ["5.0", "head", "5.1", "uyuni"]:
+                logger.info("Checking files before cleanup...")
+                self._run_command("rm /var/lib/containers/storage/volumes/var-salt/_data/.ssh/known_hosts")
+                logger.info("Known_hosts file cleaned up.")
+            else:
+                logger.warning(f"Unsupported version for known_hosts deletion: {version}")
+        finally:
+            self._close()
+
+    def delete_distributions(self, version):
+        """
+        Deletes distributions directories based on the product version.
+
+        :param version: Product version to determine file paths.
+        """
+        try:
+            self._connect()
+            if version == "4.3":
+                logger.warning("Distribution deletion for version 4.3 is not implemented.")
+            elif version in ["5.0", "head", "5.1"]:
+                self._run_command("rm -rf /var/lib/containers/storage/volumes/srv-www/_data/distributions/*")
+                self._run_command("rm -rf /var/lib/containers/storage/volumes/srv-www/_data/htdocs/pub/*iso")
+                logger.info("Distributions directories deleted.")
+            else:
+                logger.error(f"Unsupported product version: {version}")
+        finally:
+            self._close()
+
+    def update_custom_repositories(self):
+        """
+        Updates custom repositories by copying a configuration file to the server.
+        """
+        try:
+            self._connect()
+            with self._client.open_sftp() as sftp:
+                local_path = "./custom_repositories.json"
+                remote_path = "/root/spacewalk/testsuite/features/upload_files/custom_repositories.json"
+                logger.info(f"Copying file from {local_path} to {remote_path}")
+                sftp.put(local_path, remote_path)
+                logger.info("Custom repositories updated successfully.")
         except Exception as e:
-            logger.error(f"Exception during file copy: {str(e)}")
-            return f"Exception during file copy: {str(e)}"
+            logger.error(f"Error updating custom repositories: {e}")
+            raise
+        finally:
+            self._close()
 
     @staticmethod
     def extract_ip_from_url(url):
@@ -88,36 +128,11 @@ class SSHClientManager:
         parsed_url = urlparse(url)
         hostname = parsed_url.hostname
 
-        if hostname:
-            ip_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
-            if ip_pattern.match(hostname):
-                return hostname
-            else:
-                return "Invalid IP address format"
+        if not hostname:
+            raise ValueError("No hostname found in the provided URL.")
+
+        ip_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
+        if ip_pattern.match(hostname):
+            return hostname
         else:
-            return "No hostname found in URL"
-
-    def delete_known_hosts(self, version):
-        if version == "4.3":
-            self.run_command("rm /var/lib/salt/.ssh/known_hosts")
-        elif version in ["5.0", "head", "5.1", "uyuni"]:
-            result = self.run_command("ls -la /var/lib/containers/storage/volumes/var-salt/_data/.ssh/known_hosts")
-            logger.info(f"Files in salt ssh before cleanup : {result}")
-            self.run_command("rm /var/lib/containers/storage/volumes/var-salt/_data/.ssh/known_hosts")
-            logger.info(f"Files in salt ssh after cleanup : {result}")
-        logger.info("Deleted known_hosts file on server")
-
-    def delete_distributions(self, version):
-        if version == "4.3":
-            logger.warning("Distribution delete to do 4.3")
-        elif version in ["5.0", "head", "5.1"]:
-            self.run_command("rm -rf /var/lib/containers/storage/volumes/srv-www/_data/distributions/*")
-            self.run_command("rm -rf /var/lib/containers/storage/volumes/srv-www/_data/htdocs/pub/*iso")
-        logger.info("Deleted distributions directory on server")
-
-    def update_custom_repositories(self):
-        self.copy_file(
-            "./custom_repositories.json",
-            "/root/spacewalk/testsuite/features/upload_files/custom_repositories.json"
-        )
-        logger.info("Custom repositories updated.")
+            raise ValueError(f"Invalid IP address format in URL: {hostname}")
