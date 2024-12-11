a4208c4dc5b2d66f7ecc587e5033dbd6132d5bea
Maxime Noel <maxime.noel@suse.com>
2024-12-11 13:24:19 +1300

    Add action using the new ssh method

--- jenkins_pipelines/scripts/test_environment_cleaner/test_environment_cleaner_program/TestEnvironmentCleaner.py
+++ jenkins_pipelines/scripts/test_environment_cleaner/test_environment_cleaner_program/TestEnvironmentCleaner.py
@@ -23,7 +23,7 @@ def main():
     parser.add_argument("--default-resources-to-delete", type=str, nargs='*',
                         choices=['proxy', 'monitoring-server', 'retail'],
                         default=[], help='List of default modules to force deletion')
-    parser.add_argument("--product_version", required=True, type=str, choices=['5.0', '4.3', 'uyuni'])
+    parser.add_argument("--product_version", required=True, type=str, choices=['head', '5.1', '5.0', '4.3', 'uyuni'])
 
     args = parser.parse_args()
     manager_url = args.url
@@ -56,29 +56,17 @@ def main():
     # Server commands part
     else:
         ssh_manager = SSHClientManager(url=manager_url)
-        try:
-            if args.mode == "delete_known_hosts":
-                if args.product_version == "4.3":
-                    ssh_manager.run_command("rm /var/lib/salt/.ssh/known_hosts")
-                elif args.product_version == "5.0" or args.product_version == "uyuni":
-                    result = ssh_manager.run_command("ls -la /var/lib/containers/storage/volumes/var-salt/_data/.ssh/known_hosts")
-                    logger.info(f"Files in salt ssh before cleanup : {result}")
-                    ssh_manager.run_command("rm /var/lib/containers/storage/volumes/var-salt/_data/.ssh/known_hosts")
-                    logger.info(f"Files in salt ssh after cleanup : {result}")
-                logger.info("Deleted known_hosts file on server")
+        ssh_actions = {
+            "delete_known_hosts": ssh_manager.delete_known_hosts,
+            "delete_distributions": ssh_manager.delete_distributions,
+            "update_custom_repositories": ssh_manager.update_custom_repositories,
+        }
+        action = ssh_actions.get(args.mode)
+        if action:
+            action()
+        else:
+            logger.error(f"Mode '{args.mode}' is not recognized.")
 
-            elif args.mode == "delete_distributions":
-                if args.product_version == "4.3":
-                    logger.warning("Distribution delete to do 4.3")
-                elif args.product_version == "5.0":
-                    ssh_manager.run_command("rm -rf /var/lib/containers/storage/volumes/srv-www/_data/distributions/*")
-                    ssh_manager.run_command("rm -rf /var/lib/containers/storage/volumes/srv-www/_data/htdocs/pub/*iso")
-                logger.info("Deleted distributions directory on server")
-
-            elif args.mode == "update_custom_repositories":
-                ssh_manager.copy_file("./custom_repositories.json", "/root/spacewalk/testsuite/features/upload_files/custom_repositories.json")
-        finally:
-            ssh_manager.close()
 
 if __name__ == "__main__":
     main()
