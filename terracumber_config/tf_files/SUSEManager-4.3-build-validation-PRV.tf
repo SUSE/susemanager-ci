db56ee72e13b185feafab853abf0db03b16f9806
Eric Bischoff <ebischoff@suse.com>
2025-01-13 22:00:56 +0100

    z/VM connector on zl41

--- terracumber_config/tf_files/SUSEManager-4.3-build-validation-PRV.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-build-validation-PRV.tf
@@ -122,7 +122,7 @@ provider "libvirt" {
 }
 
 provider "feilong" {
-  connector   = "https://10.144.68.9"
+  connector   = "https://feilong.mgr.suse.de"
   admin_token = var.ZVM_ADMIN_TOKEN
   local_user  = "jenkins@jenkins-worker.mgr.prv.suse.net"
 }
