fbfd1ecb1bfd03d8f7972a1c13952a0a54695136
szachovy <wiktor.maj@suse.com>
2025-01-14 10:29:53 +0100

    Build validation with SL Micro 6.1

--- terracumber_config/tf_files/SUSEManager-4.3-build-validation-PRV.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-build-validation-PRV.tf
@@ -215,7 +215,7 @@ module "base_new_sle" {
   name_prefix       = "suma-bv-43-"
   use_avahi         = false
   domain            = "mgr.prv.suse.net"
-  images            = [ "sles15sp2o", "sles15sp3o", "sles15sp4o", "sles15sp5o", "sles15sp6o", "slemicro51-ign", "slemicro52-ign", "slemicro53-ign" , "slemicro54-ign", "slemicro55o", "slmicro60o" ]
+  images            = [ "sles15sp2o", "sles15sp3o", "sles15sp4o", "sles15sp5o", "sles15sp6o", "slemicro51-ign", "slemicro52-ign", "slemicro53-ign" , "slemicro54-ign", "slemicro55o", "slmicro60o", "slmicro61o" ]
 
   mirror            = "minima-mirror-ci-bv.mgr.prv.suse.net"
   use_mirror_images = true
@@ -1156,6 +1156,28 @@ module "slmicro60_minion" {
   install_salt_bundle = false
 }
 
+module "slmicro61_minion" {
+  providers = {
+    libvirt = libvirt.moscowmule
+  }
+  source             = "./modules/minion"
+  base_configuration = module.base_new_sle.configuration
+  name               = "slmicro61-minion"
+  image              = "slmicro61o"
+  provider_settings = {
+    mac                = "..."
+    memory             = 2048
+  }
+
+  server_configuration = {
+    hostname = "suma-bv-43-proxy.mgr.prv.suse.net"
+  }
+  auto_connect_to_master  = false
+  use_os_released_updates = false
+  ssh_key_path            = "./salt/controller/id_rsa.pub"
+  install_salt_bundle = false
+}
+
 module "sle12sp5_sshminion" {
   providers = {
     libvirt = libvirt.endor
@@ -1632,6 +1654,23 @@ module "sle15sp5s390_sshminion" {
 //   ssh_key_path            = "./salt/controller/id_rsa.pub"
 // }
 
+//  WORKAROUND until https://bugzilla.suse.com/show_bug.cgi?id=1208045 gets fixed
+// module "slmicro61_sshminion" {
+//  providers = {
+//     libvirt = libvirt.moscowmule
+//   }
+//   source             = "./modules/sshminion"
+//   base_configuration = module.base_new_sle.configuration
+//   name               = "slmicro61-sshminion"
+//   image              = "slmicro61o"
+//   provider_settings = {
+//     mac                = "..."
+//     memory             = 2048
+//   }
+//   use_os_released_updates = false
+//   ssh_key_path            = "./salt/controller/id_rsa.pub"
+// }
+
 module "sle12sp5_buildhost" {
   providers = {
     libvirt = libvirt.coruscant
@@ -1849,6 +1888,10 @@ module "controller" {
   slmicro60_minion_configuration    = module.slmicro60_minion.configuration
 //  WORKAROUND until https://bugzilla.suse.com/show_bug.cgi?id=1208045 gets fixed
 //  slmicro60_sshminion_configuration = module.slmicro60_sshminion.configuration
+
+  slmicro61_minion_configuration    = module.slmicro61_minion.configuration
+//  WORKAROUND until https://bugzilla.suse.com/show_bug.cgi?id=1208045 gets fixed
+//  slmicro61_sshminion_configuration = module.slmicro61_sshminion.configuration
 
   sle12sp5_buildhost_configuration = module.sle12sp5_buildhost.configuration
   sle15sp4_buildhost_configuration = module.sle15sp4_buildhost.configuration
