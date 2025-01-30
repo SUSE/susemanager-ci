185ded10ab1c98102e2a77a2dcb707a38073a765
Maxime Noel <55169628+maximenoel8@users.noreply.github.com>
2025-01-30 21:23:38 +1300

    Add server hostname to main.tf output for BV continuous (#1527)

--- terracumber_config/tf_files/SUSEManager-continuous-build-validation-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-continuous-build-validation-NUE.tf
@@ -1315,6 +1315,7 @@ module "controller" {
 
 output "configuration" {
   value = {
-    controller = module.controller.configuration
+    controller  = module.controller.configuration
+    server      = module.server_containerized.configuration
   }
 }
