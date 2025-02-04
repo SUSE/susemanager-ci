635c0afd64257f03a443ae1b3dfeba249deb7a03
Paolo Invernizzi <paolo.invernizzi@suse.com>
2025-02-04 17:00:28 +0100

    Adjust SL Micro 6 custom repos

--- jenkins_pipelines/scripts/json_generator/maintenance_json_generator.py
+++ jenkins_pipelines/scripts/json_generator/maintenance_json_generator.py
@@ -272,11 +272,9 @@ def get_version_nodes(version: str) -> dict[str, list[str]]:
     return version_nodes
 
 def init_custom_repositories() -> dict[str, dict[str, str]]:
-    custom_repositories = {
-        'slmicro60_minion': { 
-            'slmicro60_staging' : "http://download.suse.de/ibs/SUSE:/ALP:/Source:/Standard:/1.0:/Staging:/Z/standard/" 
-        }
-    }
+    custom_repositories = {}
+    custom_repositories['slmicro60_minion'] = { 'slmicro60_staging' : "http://download.suse.de/ibs/SUSE:/ALP:/Source:/Standard:/1.0:/Staging:/Z/standard/" }
+    custom_repositories['slmicro61_minion'] = { 'slmicro61_staging' : "http://download.suse.de/ibs/SUSE:/ALP:/Source:/Standard:/1.0:/Staging:/Z/standard/" }
     return custom_repositories
 
 def update_custom_repositories(custom_repositories: dict[str, dict[str, str]], node: str, mi_id: str, url: str):
