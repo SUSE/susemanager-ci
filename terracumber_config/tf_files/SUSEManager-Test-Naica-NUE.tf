b4138a7f6ec35793c8bc129358d08e17593e278c
Konstantinos Tsamis <ktsamis@suse.de>
2025-02-19 15:31:51 +0100

    Add ubuntu 24 image since we use it

--- terracumber_config/tf_files/SUSEManager-Test-Naica-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Test-Naica-NUE.tf
@@ -102,7 +102,7 @@ module "cucumber_testsuite" {
   cc_username = var.SCC_USER
   cc_password = var.SCC_PASSWORD
 
-  images = ["rocky8o", "opensuse155o", "ubuntu2204o", "sles15sp4o", "slmicro61o"]
+  images = ["rocky8o", "opensuse155o", "ubuntu2404o", "sles15sp4o", "slmicro61o"]
 
   use_avahi    = false
   name_prefix  = "suma-test-naica-"
