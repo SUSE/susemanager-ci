745bbc983f2efce52ea8f80097ae7d35fe1b4f5d
mbussolotto <michele.bussolotto@suse.com>
2025-03-10 16:01:25 +0100

    fix hexagon pipeline

--- terracumber_config/tf_files/SUSEManager-Test-Hexagon-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Test-Hexagon-NUE.tf
@@ -74,6 +74,11 @@ variable "GIT_PASSWORD" {
   default = null // Not needed for master, as it is public
 }
 
+variable "PROMETHEUS_PUSH_GATEWAY_URL" {
+  type = string
+  default = null
+}
+
 terraform {
   required_version = "1.0.10"
   required_providers {
@@ -85,14 +90,13 @@ terraform {
 }
 
 provider "libvirt" {
-  //uri = "qemu+tcp://cthulhu.mgr.suse.de/system"
   uri = "qemu+tcp://suma-03.mgr.suse.de/system"
 }
 
 module "cucumber_testsuite" {
   source = "./modules/cucumber_testsuite"
 
-  product_version = "head"
+  product_version = "uyuni-master"
 
   // Cucumber repository configuration for the controller
   git_username = var.GIT_USER
@@ -100,128 +104,149 @@ module "cucumber_testsuite" {
   git_repo     = var.CUCUMBER_GITREPO
   branch       = var.CUCUMBER_BRANCH
 
-  cc_username = var.SCC_USER
-  cc_password = var.SCC_PASSWORD
+  cc_username   = var.SCC_USER
+  cc_password   = var.SCC_PASSWORD
 
-  images = ["rocky8o", "opensuse155o", "opensuse156o", "ubuntu2404o", "sles15sp4o", "slmicro61o"]
+  images = ["rocky8o", "opensuse155o", "opensuse156o", "leapmicro55o", "ubuntu2404o", "sles15sp4o", "slmicro61o"]
 
-  use_avahi    = false
-  name_prefix  = "suma-test-hexagon-"
-  domain       = "mgr.suse.de"
-  from_email   = "root@suse.de"
+  use_avahi     = false
+  name_prefix   = "suma-test-hexagon-"
+  domain        = "mgr.suse.de"
+  from_email    = "root@suse.de"
 
-  no_auth_registry = "registry.mgr.suse.de"
-  auth_registry = "registry.mgr.suse.de:5000/cucutest"
-  auth_registry_username = "cucutest"
-  auth_registry_password = "cucusecret"
-  git_profiles_repo = "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles/internal_nue"
-  
-  container_server = true
-  container_proxy = true
-  
-  mirror                   = "minima-mirror-ci-bv.mgr.suse.de"
-  use_mirror_images        = true
+  no_auth_registry          = "registry.mgr.suse.de"
+  auth_registry             = "registry.mgr.suse.de:5000/cucutest"
+  auth_registry_username    = "cucutest"
+  auth_registry_password    = "cucusecret"
+  git_profiles_repo         = "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles/internal_nue"
 
-  server_http_proxy = "http-proxy.mgr.suse.de:3128"
-  custom_download_endpoint = "ftp://minima-mirror-ci-bv.mgr.suse.de:445"
+  container_server          = true
+  container_proxy           = true
+
+  mirror                    = "minima-mirror-ci-bv.mgr.suse.de"
+  use_mirror_images         = true
+  server_http_proxy         = "http-proxy.mgr.suse.de:3128"
+  custom_download_endpoint  = "ftp://minima-mirror-ci-bv.mgr.suse.de:445"
 
   host_settings = {
     controller = {
       provider_settings = {
-        mac = "aa:b2:93:01:00:50"
+        mac     = "aa:b2:93:01:00:50"
+        vcpu    = 2
+        memory  = 2048
       }
     }
     server_containerized = {
-      provider_settings = {
-        mac = "aa:b2:93:01:00:51"
-        vcpu = 8
-        memory = 32768
+      provider_settings     = {
+        mac     = "aa:b2:93:01:00:51"
+        vcpu    = 4
+        memory  = 16384
       }
-      main_disk_size = 500
-      login_timeout = 28800
       runtime = "podman"
-      container_repository = "registry.suse.de/devel/galaxy/manager/test/hexagon/containerfile"
-      additional_repos = {
-        Test_repo = "http://download.suse.de/ibs/Devel:/Galaxy:/Manager:/TEST:/Hexagon/SLE_15_SP6/"
+      container_repository = "registry.opensuse.org/systemsmanagement/uyuni/master/containers"
+      helm_chart_url = "oci://registry.opensuse.org/systemsmanagement/uyuni/master/charts/uyuni/server"  
+      container_tag = "latest"
+      helm_chart_url = "oci://registry.opensuse.org/systemsmanagement/uyuni/master/charts/uyuni/server"
+      main_disk_size        = 80
+      repository_disk_size  = 200
+      database_disk_size    = 30
+      login_timeout         = 28800
+      large_deployment      = true
+    }
+    proxy_containerized = {
+      provider_settings = {
+        mac     = "aa:b2:93:01:00:52"
+        vcpu    = 2
+        memory  = 2048
       }
+      runtime = "podman"
+      container_repository = "registry.opensuse.org/systemsmanagement/uyuni/master/containers"
       container_tag = "latest"
     }
-
     suse_minion = {
-      image = "sles15sp4o"
+      image             = "opensuse155o"
       provider_settings = {
-        mac = "aa:b2:93:01:00:56"
+        mac     = "aa:b2:93:01:00:56"
+        vcpu    = 2
+        memory  = 2048
       }
     }
     suse_sshminion = {
-      image = "sles15sp4o"
+      image             = "opensuse155o"
       provider_settings = {
-        mac = "aa:b2:93:01:00:58"
-        vcpu = 2
-        memory = 2048
+        mac     = "aa:b2:93:01:00:58"
+        vcpu    = 2
+        memory  = 2048
       }
-      additional_packages = [ "iptables" ]
     }
     rhlike_minion = {
-      image = "rocky8o"
+      image             = "rocky8o"
       provider_settings = {
-        mac = "aa:b2:93:01:00:5a"
+        mac     = "aa:b2:93:01:00:5a"
         // Since start of May we have problems with the instance not booting after a restart if there is only a CPU and only 1024Mb for RAM
-        // Still researching, but it will do it for now
-        memory = 2048
+        // Also, openscap cannot run with less than 1.25 GB of RAM
+        vcpu    = 2
+        memory  = 2048
       }
     }
     deblike_minion = {
-      image = "ubuntu2404o"
+      image             = "ubuntu2404o"
       provider_settings = {
-        mac = "aa:b2:93:01:00:5b"
-        vcpu = 2
-        memory = 2048
+        mac     = "aa:b2:93:01:00:5b"
+        vcpu    = 2
+        memory  = 2048
       }
     }
     build_host = {
-      image = "sles15sp4o"
+      image             = "sles15sp4o"
       provider_settings = {
-        mac = "aa:b2:93:01:00:5d"
-        vcpu = 4
-        memory = 8192
+        mac     = "aa:b2:93:01:00:5d"
+        vcpu    = 2
+        memory  = 2048
       }
     }
     pxeboot_minion = {
-      image = "sles15sp4o"
-      provider_settings = {
-        vcpu = 2
-        memory = 2048
-      }
+      image  = "sles15sp4o"
     }
-    kvm_host = {
-      image = "sles15sp4o"
-      provider_settings = {
-        mac = "aa:b2:93:01:00:5e"
-        vcpu = 4
-        memory = 8192
+    dhcp_dns = {
+      name = "dhcp-dns"
+      image = "opensuse155o"
+      hypervisor = {
+        host        = "suma-03.mgr.suse.de"
+        user        = "root"
+        private_key = file("~/.ssh/id_rsa")
       }
     }
   }
-  
   provider_settings = {
-    pool         = "ssd"
-    network_name = null
-    bridge       = "br0"
+    pool               = "ssd"
+    network_name       = null
+    bridge             = "br0"
   }
 }
 
-resource "null_resource" "cdn_workaround" {
- provisioner "remote-exec" {
-    inline = [ "echo techpreview.ZYPP_MEDIANETWORK=1 >> /etc/zypp/zypp.conf" ]
+
+resource "null_resource" "configure_quality_intelligence" {
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ "echo export QUALITY_INTELLIGENCE=true >> ~/.bashrc",
+      "echo export PROMETHEUS_PUSH_GATEWAY_URL=${var.PROMETHEUS_PUSH_GATEWAY_URL} >> ~/.bashrc",
+      "source ~/.bashrc"
+    ]
     connection {
       type     = "ssh"
       user     = "root"
       password = "linux"
-      host     = "${module.cucumber_testsuite.configuration.server.hostname}"
+      host     = "${module.cucumber_testsuite.configuration.controller.hostname}"
     }
   }
+
 }
+
 output "configuration" {
   value = module.cucumber_testsuite.configuration
 }
