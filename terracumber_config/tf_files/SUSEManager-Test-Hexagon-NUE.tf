078a464329d9aed3e3a520d25be78137d9fe8417
mbussolotto <michele.bussolotto@suse.com>
2025-03-11 11:47:09 +0100

    fixup! fixup! fixup! fix hexagon pipeline

--- terracumber_config/tf_files/SUSEManager-Test-Hexagon-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Test-Hexagon-NUE.tf
@@ -107,7 +107,7 @@ module "cucumber_testsuite" {
   cc_username   = var.SCC_USER
   cc_password   = var.SCC_PASSWORD
 
-  images = ["rocky8o", "opensuse155o", "opensuse156o", "ubuntu2404o", "sles15sp4o", "slmicro61o"]
+  images = ["rocky8o", "opensuse155o", "opensuse156o", "ubuntu2404o", "sles15sp4o", "slmicro61o", "slemicro55o"]
 
   use_avahi     = false
   name_prefix   = "suma-test-hexagon-"
@@ -137,7 +137,7 @@ module "cucumber_testsuite" {
       }
     }
     server_containerized = {
-      image = "slmicro61o"
+      image = "slemicro55o"
       provider_settings     = {
         mac     = "aa:b2:93:01:00:51"
         vcpu    = 4
