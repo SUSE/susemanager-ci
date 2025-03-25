b3361897700eaf707910f5eaed8f56a351840230
Konstantinos Tsamis <ktsamis@suse.de>
2025-03-25 16:12:07 +0100

    Switch Uyuni and Head to large_deployment

--- terracumber_config/tf_files/SUSEManager-Head-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Head-NUE.tf
@@ -145,6 +145,7 @@ module "cucumber_testsuite" {
       runtime = "podman"
       container_repository = "registry.suse.de/devel/galaxy/manager/head/containerfile"
       container_tag = "latest"
+      large_deployment      = true
     }
     proxy_containerized = {
       provider_settings = {
