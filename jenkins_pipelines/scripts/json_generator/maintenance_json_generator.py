653c626cbe9e87b2e9d2d08174d27b5ad8d73071
Konstantinos Tsamis <ktsamis@suse.de>
2025-04-03 11:46:26 +0200

    Change names of url so they do not contain minion (#1619)

--- jenkins_pipelines/scripts/json_generator/maintenance_json_generator.py
+++ jenkins_pipelines/scripts/json_generator/maintenance_json_generator.py
@@ -269,8 +269,8 @@ def get_version_nodes(version: str) -> dict[str, list[str]]:
 
 def init_custom_repositories(version: str) -> dict[str, dict[str, str]]:
     custom_repositories = {}
-    custom_repositories['slmicro60_minion'] = { 'slmicro60_alp' : "http://download.suse.de/ibs/SUSE:/ALP:/Source:/Standard:/1.0:/Staging:/Z/standard/" }
-    custom_repositories['slmicro61_minion'] = { 'slmicro60_alp' : "http://download.suse.de/ibs/SUSE:/ALP:/Source:/Standard:/1.0:/Staging:/Z/standard/", 'slmicro61_slfo' : "http://download.suse.de/ibs/SUSE:/SLFO:/1.1:/Staging:/I/standard/" }
+    custom_repositories['slmicro60_minion'] = { 'alp_staging' : "http://download.suse.de/ibs/SUSE:/ALP:/Source:/Standard:/1.0:/Staging:/Z/standard/" }
+    custom_repositories['slmicro61_minion'] = { 'alp_staging' : "http://download.suse.de/ibs/SUSE:/ALP:/Source:/Standard:/1.0:/Staging:/Z/standard/", 'slfo_staging' : "http://download.suse.de/ibs/SUSE:/SLFO:/1.1:/Staging:/I/standard/" }
     return custom_repositories
 
 def update_custom_repositories(custom_repositories: dict[str, dict[str, str]], node: str, mi_id: str, url: str):
