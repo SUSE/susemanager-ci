a9212117b8e50ee203167559def84f80d6186876
Maxime Noel <55169628+maximenoel8@users.noreply.github.com>
2025-04-16 20:38:41 +1200

    Use salt bundle for slmicro60 and 61 for all BVs  (#1637)

--- terracumber_config/tf_files/Uyuni-Master-build-validation-PRV.tf
+++ terracumber_config/tf_files/Uyuni-Master-build-validation-PRV.tf
@@ -866,9 +866,6 @@ module "slmicro60_minion" {
   auto_connect_to_master  = false
   use_os_released_updates = false
   ssh_key_path            = "./salt/controller/id_ed25519.pub"
-// WORKAROUND: Does not work in sumaform, yet
-//  additional_packages = [ "venv-salt-minion" ]
-  install_salt_bundle = false
 }
 
 module "slmicro61_minion" {
@@ -887,9 +884,6 @@ module "slmicro61_minion" {
   auto_connect_to_master  = false
   use_os_released_updates = false
   ssh_key_path            = "./salt/controller/id_ed25519.pub"
-// WORKAROUND: Does not work in sumaform, yet
-//  additional_packages = [ "venv-salt-minion" ]
-  install_salt_bundle = false
 }
 
 module "sles12sp5_sshminion" {
