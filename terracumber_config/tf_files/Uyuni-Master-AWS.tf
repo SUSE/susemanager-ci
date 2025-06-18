02dcb95493cd40a01aeba8d8f558e70972e86d23
Paolo Invernizzi <paolo.invernizzi@suse.com>
2025-06-18 15:45:02 +0200

    Increase server volume size

--- terracumber_config/tf_files/Uyuni-Master-AWS.tf
+++ terracumber_config/tf_files/Uyuni-Master-AWS.tf
@@ -152,7 +152,7 @@ module "cucumber_testsuite" {
       image = "opensuse156o"
       provider_settings = {
         instance_type = "m6a.xlarge"
-        volume_size = "100"
+        volume_size = "200"
         private_ip = "172.16.3.6"
         overwrite_fqdn = "uyuni-master-server.sumaci.aws"
       }
