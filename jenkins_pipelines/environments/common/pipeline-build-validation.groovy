581a44f5698e2d38149c6f3e62917eab1cc8aa4b
Maxime Noel <maxime.noel@suse.com>
2025-07-03 15:40:12 +1200

    Remove use findAll

--- jenkins_pipelines/environments/common/pipeline-build-validation.groovy
+++ jenkins_pipelines/environments/common/pipeline-build-validation.groovy
@@ -664,22 +664,42 @@ def nameDisplay(params) {
     if (params.must_run_core) buildLabel << 'core'
     if (params.must_sync) buildLabel << 'reposync'
 
-    buildLabel << buildComponentLabel("proxy", params, ['must_add_MU_repositories': 'AddMU', 'must_add_keys': 'ActKeys', 'must_create_bootstrap_repos': 'CrBoot', 'must_boot_node': 'Boot'], params.enable_proxy_stages)
-    buildLabel << buildComponentLabel("monitoring", params, ['must_add_MU_repositories': 'AddMU', 'must_add_keys': 'ActKeys', 'must_create_bootstrap_repos': 'CrBoot', 'must_boot_node': 'Boot'], params.enable_monitoring_stages)
+    buildLabel << buildComponentLabel("proxy", params, [
+            'must_add_MU_repositories'     : 'AddMU',
+            'must_add_keys'                : 'ActKeys',
+            'must_create_bootstrap_repos'  : 'CrBoot',
+            'must_boot_node'               : 'Boot'
+    ], params.enable_proxy_stages)
+
+    buildLabel << buildComponentLabel("monitoring", params, [
+            'must_add_MU_repositories'     : 'AddMU',
+            'must_add_keys'                : 'ActKeys',
+            'must_create_bootstrap_repos'  : 'CrBoot',
+            'must_boot_node'               : 'Boot'
+    ], params.enable_monitoring_stages)
+
     buildLabel << buildComponentLabel("client", params, [
-            'must_add_MU_repositories': 'AddMU',
-            'must_add_non_MU_repositories': 'AddNonMU',
-            'must_add_keys': 'ActKeys',
-            'must_create_bootstrap_repos': 'CrBoot',
-            'must_boot_node': 'Boot',
-            'must_run_tests': 'Smoke'
+            'must_add_MU_repositories'     : 'AddMU',
+            'must_add_non_MU_repositories' : 'AddNonMU',
+            'must_add_keys'                : 'ActKeys',
+            'must_create_bootstrap_repos'  : 'CrBoot',
+            'must_boot_node'               : 'Boot',
+            'must_run_tests'               : 'Smoke'
     ], params.enable_client_stages)
 
     if (params.must_run_products_and_salt_migration_tests) buildLabel << 'migration'
     if (params.must_prepare_retail) buildLabel << 'retail'
 
+    // Manually filter null/empty items to avoid findAll()
+    def filteredLabel = []
+    for (item in buildLabel) {
+        if (item) {
+            filteredLabel << item
+        }
+    }
+
     def baseOs = params.base_os ?: ''
-    def fullLabel = "${params.product_version}${baseOs ? "_${baseOs}" : ""} - ${buildLabel.findAll().join(' ')}"
+    def fullLabel = "${params.product_version}${baseOs ? "_${baseOs}" : ""} - ${filteredLabel.join(' ')}"
 
     if (fullLabel.length() > 160) {
         return "#${env.BUILD_NUMBER} - ${params.product_version}${baseOs ? "_${baseOs}" : ""}"
@@ -691,8 +711,10 @@ def buildComponentLabel(component, params, conditionsMap, isEnabled) {
     if (!isEnabled) return null
 
     def options = []
-    conditionsMap.each { key, label ->
-        if (params.get(key)) options << label
+    for (entry in conditionsMap) {
+        if (params.get(entry.key)) {
+            options << entry.value
+        }
     }
     return options ? "${component}[${options.join(' ')}]" : null
 }
