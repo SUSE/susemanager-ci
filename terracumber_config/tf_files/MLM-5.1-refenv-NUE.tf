ac1e3e719b43363c9e36ba8fd0cae51800f992e2
Michael Calmer <michael.calmer@suse.com>
2025-08-17 15:38:06 +0200

    set container_repository to official 5.1 namespace

--- terracumber_config/tf_files/MLM-5.1-refenv-NUE.tf
+++ terracumber_config/tf_files/MLM-5.1-refenv-NUE.tf
@@ -144,8 +144,7 @@ module "cucumber_testsuite" {
       main_disk_size = 500
       login_timeout = 28800
       runtime = "podman"
-      #container_repository = "registry.suse.de/devel/galaxy/manager/5.1/containerfile"
-      container_repository = "registry.suse.de/devel/galaxy/manager/test/orion/containerfile"
+      container_repository = "registry.suse.de/devel/galaxy/manager/5.1/containerfile"
       container_tag = "latest"
     }
     proxy_containerized = {
@@ -157,7 +156,7 @@ module "cucumber_testsuite" {
       }
       main_disk_size = 200
       runtime = "podman"
-      container_repository = "registry.suse.de/devel/galaxy/manager/test/orion/containerfile"
+      container_repository = "registry.suse.de/devel/galaxy/manager/5.1/containerfile"
       container_tag = "latest"
     }
     suse_minion = {
