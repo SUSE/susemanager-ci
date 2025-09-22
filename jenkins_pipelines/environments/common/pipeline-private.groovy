12677c9478aa8a6677f65cecf7d2302cb63c0b90
Maxime Noel <maxime.noel@suse.com>
2025-09-23 07:10:28 +1200

    Disable whatchanged

--- jenkins_pipelines/environments/common/pipeline-private.groovy
+++ jenkins_pipelines/environments/common/pipeline-private.groovy
@@ -54,12 +54,19 @@ def run(params) {
 
                 // Create a directory for  to place the directory with the build results (if it does not exist)
                 sh "mkdir -p ${resultdir}"
-                git url: params.terracumber_gitrepo, branch: params.terracumber_ref
+//                git url: params.terracumber_gitrepo, branch: params.terracumber_ref
                 dir("susemanager-ci") {
-                    checkout scm
+                    checkout([
+                            $class: 'GitSCM',
+                            branches: [[name: params.terracumber_ref]],
+                            userRemoteConfigs: [[url: params.terracumber_gitrepo]],
+                            extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "susemanager-ci"]],
+                            changelog: false,   // 🔑 disables 'git whatchanged'
+                            poll: false
+                    ])
                 }
                 // Clone sumaform
-//                sh "set +x; source /home/jenkins/.credentials set -x; ./terracumber-cli ${common_params} --gitrepo ${params.sumaform_gitrepo} --gitref ${params.sumaform_ref} --tf_variables_description_file ${tfvariables_file} --tf_configuration_files ${tfvars_infra_description} --runstep gitsync"
+                sh "set +x; source /home/jenkins/.credentials set -x; ./terracumber-cli ${common_params} --gitrepo ${params.sumaform_gitrepo} --gitref ${params.sumaform_ref} --tf_variables_description_file ${tfvariables_file} --tf_configuration_files ${tfvars_infra_description} --runstep gitsync"
 
                 // Restore Terraform states from artifacts
                 if (params.use_previous_terraform_state) {
