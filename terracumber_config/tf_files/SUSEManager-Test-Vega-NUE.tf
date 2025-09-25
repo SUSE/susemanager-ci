b1b18cedb1834df4cd21143dbe8d39aa6f917d0b
Witek Bedyk <wbedyk@suse.com>
2025-09-25 11:29:08 +0200

    Revert "Change vega environment to 5.0"
    This reverts commit 87621c4157067f5e3991467c255af6b62237a035.

--- terracumber_config/tf_files/SUSEManager-Test-Vega-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-Test-Vega-NUE.tf
@@ -12,12 +12,12 @@ variable "CUCUMBER_COMMAND" {
 
 variable "CUCUMBER_GITREPO" {
   type = string
-  default = "https://github.com/SUSE/spacewalk.git"
+  default = "https://github.com/uyuni-project/uyuni.git"
 }
 
 variable "CUCUMBER_BRANCH" {
   type = string
-  default = "Manager-5.0"
+  default = "master"
 }
 
 variable "CUCUMBER_RESULTS" {
@@ -91,7 +91,7 @@ provider "libvirt" {
 module "cucumber_testsuite" {
   source = "./modules/cucumber_testsuite"
 
-  product_version = "5.0-nightly"
+  product_version = "head"
 
   // Cucumber repository configuration for the controller
   git_username = var.GIT_USER
