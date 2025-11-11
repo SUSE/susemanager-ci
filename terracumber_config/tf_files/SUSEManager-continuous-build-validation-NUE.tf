dae72e313d5897025343b7184da3e035a94449b3
Paolo Invernizzi <paolo.invernizzi@suse.com>
2025-11-11 09:41:51 +0100

    Update required Terraform and libvirt-provider versions

--- terracumber_config/tf_files/SUSEManager-continuous-build-validation-NUE.tf
+++ terracumber_config/tf_files/SUSEManager-continuous-build-validation-NUE.tf
@@ -102,11 +102,11 @@ variable "ZVM_ADMIN_TOKEN" {
 }
 
 terraform {
-  required_version = "1.0.10"
+  required_version = ">= 1.6.0"
   required_providers {
     libvirt = {
       source = "dmacvicar/libvirt"
-      version = "0.8.1"
+      version = "0.8.3"
     }
     feilong = {
       source = "bischoff/feilong"
