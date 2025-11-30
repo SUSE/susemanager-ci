1c4873438deef33ff5f6f91d9165d3b807d408b0
Maxime Noel <maxime.noel@suse.com>
2025-12-01 10:47:28 +1300

    Fix path

--- jenkins_pipelines/environments/common/pipeline-build-validation.groovy
+++ jenkins_pipelines/environments/common/pipeline-build-validation.groovy
@@ -15,7 +15,7 @@ def run(params) {
         // The junit plugin doesn't affect full paths
         GString junit_resultdir = "results/${BUILD_NUMBER}/results_junit"
         env.exports = "export BUILD_NUMBER=${BUILD_NUMBER}; export BUILD_VALIDATION=true; export CAPYBARA_TIMEOUT=${capybara_timeout}; export DEFAULT_TIMEOUT=${default_timeout}; export CUCUMBER_PUBLISH_QUIET=true;"
-        tfvariables_file  = 'susemanager-ci/terracumber_config/project/variables.tf'
+        tfvariables_file  = 'susemanager-ci/terracumber_config/tf_files/project/variables.tf'
 
         // Declare lock resource use during node bootstrap
         mgrCreateBootstrapRepo = 'share resource to avoid running mgr create bootstrap repo in parallel'
