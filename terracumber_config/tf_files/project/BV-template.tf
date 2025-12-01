607e14e8c8a29479bf028294d381d11b549bbb36
Maxime Noel <maxime.noel@suse.com>
2025-12-02 12:00:18 +1300

    Change to have two separate empty variables.

--- terracumber_config/tf_files/project/BV-template.tf
+++ terracumber_config/tf_files/project/BV-template.tf
@@ -18,7 +18,11 @@ locals {
   proxy_configuration       = strcontains(local.product_version, "4.3") ? module.proxy[0].configuration : module.proxy_containerized[0].configuration
   product_version           = var.PRODUCT_VERSION != null ? var.PRODUCT_VERSION : var.ENVIRONMENT_CONFIGURATION.product_version
   empty_minion_config = {
-    hostnames    = []
+    ids       = []
+    hostnames = []
+    macaddrs  = []
+    private_macs = []
+    ipaddrs  = []
   }
   empty_terminal_config ={
     private_mac = null
