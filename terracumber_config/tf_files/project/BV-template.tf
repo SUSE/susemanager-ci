3982d9a0502ec139747647b1a5d8e53475a6c8bd
Maxime Noel <maxime.noel@suse.com>
2025-12-01 14:19:04 +1300

    Add base os and product version only when needed

--- terracumber_config/tf_files/project/BV-template.tf
+++ terracumber_config/tf_files/project/BV-template.tf
@@ -16,7 +16,7 @@ terraform {
 locals {
   server_configuration      = strcontains(local.product_version, "4.3") ? module.server[0].configuration : module.server_containerized[0].configuration
   proxy_configuration       = strcontains(local.product_version, "4.3") ? module.proxy[0].configuration : module.proxy_containerized[0].configuration
-  product_version           = var.PRODUCT_VERSION != null ? var.PRODUCT_VERSION : local.product_version
+  product_version           = var.PRODUCT_VERSION != null ? var.PRODUCT_VERSION : var.ENVIRONMENT_CONFIGURATION.product_version
   empty_minion_config = {
     hostnames    = []
     ids          = []
