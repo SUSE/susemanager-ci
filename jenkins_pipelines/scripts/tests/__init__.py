daff4f63dfa723ce2a80dda5de9969fed7f965cc
Paolo Invernizzi <paolo.invernizzi@suse.com>
2026-01-13 10:21:00 +0100

    Add bsc_list_generator path for tests

--- jenkins_pipelines/scripts/tests/__init__.py
+++ jenkins_pipelines/scripts/tests/__init__.py
@@ -3,7 +3,9 @@ import sys
 
 
 PROJECT_PATH = os.getcwd()
-SOURCE_PATH = os.path.join(
-    PROJECT_PATH,"json_generator"
-)
-sys.path.append(SOURCE_PATH)
+
+JSON_GENERATOR_SOURCE_PATH = os.path.join(PROJECT_PATH,"json_generator")
+sys.path.append(JSON_GENERATOR_SOURCE_PATH)
+
+BSC_FINDER_SOURCE_PATH = os.path.join(PROJECT_PATH,"bsc_list_generator")
+sys.path.append(BSC_FINDER_SOURCE_PATH)
