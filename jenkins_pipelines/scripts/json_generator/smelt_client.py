60e64a14f3000dd6a1b4b0f334cf605c896d104a
Paolo Invernizzi <paolo.invernizzi@suse.com>
2026-01-13 09:53:44 +0100

    Starting SMELT GraphQL API client implementation

---
+++ jenkins_pipelines/scripts/json_generator/smelt_client.py
@@ -0,0 +1,74 @@
+import json
+import logging
+from typing import Any
+import requests
+
+# an online tool, GraphiQL, is available at the same URL and allows to see the API Schema and
+# write, validate and test GraphQL queries
+SMELT_GRAPHQL_API_URL: str = "https://smelt.suse.de/graphql/"
+
+class SmeltGraphQLClient():
+
+    def __init__(self):
+        self._api_url: str = SMELT_GRAPHQL_API_URL
+
+    def find_products(self, contains) -> list[dict, Any]:
+        query: str = f'''{{
+            products(name_Icontains: "{contains}"){{
+                edges {{
+                    node {{
+                        name
+                        friendlyName
+                        baseVersion
+                    }}
+                }}
+            }}
+        }}
+        '''
+
+        res: dict[str, Any] = self._execute_graphql_query(query)
+        edges: list[Any] = res['products']['edges']
+        if len(edges) == 0:
+            logging.error("No products have been found")
+        
+        products: list[dict[str, str]] = [ edge['node'] for edge in edges ]
+        return products
+
+    def find_incidents(self, **kwargs) -> list[dict[str, Any]]:
+        filters: str = ", ".join(f"{k}: {v}" for k, v in kwargs.items())
+
+        query: str = f'''{{
+            incidents({filters}) {{
+                edges {{
+                    node {{
+                        incidentId
+                        project
+                        priority
+                        status {{
+                            name
+                        }}
+                        severity {{
+                            name
+                        }}
+                        created
+                    }}
+                }}
+            }}
+        }}'''
+        
+        res: dict[str, Any] = self._execute_graphql_query(query)
+        edges: list[Any] = res['incidents']['edges']
+        if len(edges) == 0:
+            logging.error("No incidents have been found")
+
+        incidents: list[dict[str, Any]] = [ edge['node'] for edge in edges ]
+        return incidents
+
+    def _execute_graphql_query(self, query:str) -> dict[str, Any] :
+        response = requests.post(self._api_url, json={'query': query})
+        if not response.ok:
+            response.raise_for_status()
+
+        return response.json()['data']
+        
+        
