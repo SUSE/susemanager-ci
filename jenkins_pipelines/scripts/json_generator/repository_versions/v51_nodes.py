993791557dd7fde95102691e92f846ffaa9826e2
Maxime Noel <55169628+maximenoel8@users.noreply.github.com>
2026-02-26 13:24:37 +1300

    Add missing channels for 5.1 and remove unsupported minion for 4.3 tools (#1888)
    * Add missing channels for 5.1 and remove unsupported 4.3
    
    * Remove /SUSE_SLE-15-SP7_Update/ channel

--- jenkins_pipelines/scripts/json_generator/repository_versions/v51_nodes.py
+++ jenkins_pipelines/scripts/json_generator/repository_versions/v51_nodes.py
@@ -8,14 +8,18 @@ v51_uyuni_tools_sles_repos: Dict[str, Set[str]] = {
                 "/SUSE_Updates_SLE-Module-Server-Applications_15-SP7_x86_64/",
                 "/SUSE_Updates_SLE-Module-Basesystem_15-SP7_x86_64/",
                 "/SUSE_Updates_SLE-Module-Containers_15-SP7_x86_64/",
-                "/SUSE_SLE-15-SP7_Update/"},
+                "/SUSE_Updates_SLE-Module-Server-Applications_15-SP7_x86_64/",
+                "/SUSE_Updates_SLE-Module-Python3_15-SP7_x86_64/",
+                "/SUSE_Updates_SLE-Module-Basesystem_15-SP7_x86_64/"},
     "proxy" : { "/SUSE_Updates_Multi-Linux-Manager-Proxy-SLE_5.1_x86_64/",
                 "/SUSE_Updates_Multi-Linux-Manager-Retail-Branch-Server-SLE_5.1_x86_64/",
                 "/SUSE_Updates_MultiLinuxManagerTools_SLE-15_x86_64/",
                 "/SUSE_Updates_SLE-Module-Server-Applications_15-SP7_x86_64/",
                 "/SUSE_Updates_SLE-Module-Basesystem_15-SP7_x86_64/",
                 "/SUSE_Updates_SLE-Module-Containers_15-SP7_x86_64/",
-                "/SUSE_SLE-15-SP7_Update/"}
+                "/SUSE_Updates_SLE-Module-Server-Applications_15-SP7_x86_64/",
+                "/SUSE_Updates_SLE-Module-Python3_15-SP7_x86_64/",
+                "/SUSE_Updates_SLE-Module-Basesystem_15-SP7_x86_64/"},
 }
 
 v51_uyuni_tools_micro_repos: Dict[str, Dict[str, str]] = {
@@ -64,6 +68,11 @@ v51_nodes_dynamic_client_tools_repos: Dict[str, Set[str]] = {
     "sle15sp6_minion": { "/SUSE_Updates_MultiLinuxManagerTools_SLE-15_x86_64/",
                          "/SUSE_Updates_SLE-Module-Development-Tools_15-SP6_x86_64/"},
     "sle15sp7_minion": { "/SUSE_Updates_MultiLinuxManagerTools_SLE-15_x86_64/",
+                         "/SUSE_Updates_SLE-Module-Server-Applications_15-SP7_x86_64/",
+                         "/SUSE_Updates_SLE-Module-Basesystem_15-SP7_x86_64/",
+                         "/SUSE_Updates_SLE-Module-Server-Applications_15-SP7_x86_64/",
+                         "/SUSE_Updates_SLE-Module-Python3_15-SP7_x86_64/",
+                         "/SUSE_Updates_SLE-Module-Basesystem_15-SP7_x86_64/",
                          "/SUSE_Updates_SLE-Module-Development-Tools_15-SP7_x86_64/"},
     "slemicro52_minion": { "/SUSE_Updates_MultiLinuxManagerTools_SLE-Micro-5_x86_64/" },
     "slemicro53_minion": { "/SUSE_Updates_MultiLinuxManagerTools_SLE-Micro-5_x86_64/" },
