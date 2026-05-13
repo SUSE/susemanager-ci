a2548941172f7ced6d2667c1631ce589b5c453d5
Maxime Noel <maxime.noel@suse.com>
2026-05-14 09:30:14 +1200

    Add curl command to send RRTG ingest requests

--- jenkins_pipelines/environments/common/pipeline.groovy
+++ jenkins_pipelines/environments/common/pipeline.groovy
@@ -31,6 +31,15 @@ def run(params) {
                 'uyuni': 'uyuni_podman_ci'
         ]
         def ci_label = ci_label_map.find { k, v -> env.JOB_BASE_NAME.contains(k) }?.value ?: ''
+        def rrtg_version_map = [
+                '4.3'  : '4.3',
+                '5.0'  : '5.0',
+                '5.1'  : '5.1',
+                'Head' : 'head',
+                'uyuni': 'uyuni'
+        ]
+        def rrtg_version = rrtg_version_map.find { k, v -> env.JOB_BASE_NAME.contains(k) }?.value
+
         if (params.show_product_changes) {
             // Retrieve the hash commit of the last product built in OBS/IBS and previous job
             def prefix = env.JOB_BASE_NAME.split('-acceptance-tests')[0]
@@ -209,6 +218,18 @@ def run(params) {
                 }
                 // Send email
                 sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/mail.log --runstep mail"
+
+                if (rrtg_version) {
+                    sh """
+                      set +x; source /home/jenkins/.credentials; set -x
+                      curl -sf -X POST \
+                        "https://su-agent.your-host.com/api/rrtg/ingest/${rrtg_version}?build=${BUILD_NUMBER}" \
+                        -u "\${RRTG_USER}:\${RRTG_PASS}" \
+                        -H "Content-Type: application/json" \
+                      || echo "RRTG ingest failed (non-fatal)"
+                    """
+                }
+
                 // Clean up old results
                 sh "./clean-old-results -r ${resultdir}"
                 sh "exit ${error}"
