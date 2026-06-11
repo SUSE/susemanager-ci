a50ea69347eb56cfd9be550db3fff64581383b17
Pablo Gomez Oliva <pablo.gomez@suse.com>
2026-06-11 14:56:32 +0200

    QE: Move uyuni pipeline to suma-13

--- terracumber_config/tf_files/Uyuni-Master-podman-NUE.tf
+++ terracumber_config/tf_files/Uyuni-Master-podman-NUE.tf
@@ -90,7 +90,7 @@ terraform {
 }
 
 provider "libvirt" {
-  uri = "qemu+tcp://suma-01.mgr.suse.de/system"
+  uri = "qemu+tcp://suma-13.mgr.suse.de/system"
 }
 
 module "cucumber_testsuite" {
@@ -201,7 +201,7 @@ module "cucumber_testsuite" {
       name       = "dhcp-dns"
       image      = "opensuse156o"
       hypervisor = {
-        host        = "suma-01.mgr.suse.de"
+        host        = "suma-13.mgr.suse.de"
         user        = "root"
         private_key = file("~/.ssh/id_ed25519")
       }
