e527adeb34f1706e8df59aa5d7d1e66b1a274acb
Pablo Gomez Oliva <pablo.gomez@suse.com>
2026-07-16 14:13:21 +0200

    QE: Add correct path to container_registry variable

--- terracumber_config/tf_files/personal/main_rke2_uyuni.tf
+++ terracumber_config/tf_files/personal/main_rke2_uyuni.tf
@@ -92,7 +92,7 @@ module "cucumber_testsuite" {
       login_timeout         = 28800
       large_deployment      = true
 
-      container_registry    = "registry.opensuse.org/systemsmanagement/uyuni/master/containerfile"
+      container_registry    = "registry.opensuse.org/systemsmanagement/uyuni/master/containerfile/uyuni"
       helm_chart_name       = "server-helm"
       helm_chart_url        = "oci://registry.opensuse.org/systemsmanagement/uyuni/master/charts/uyuni"
     }
@@ -102,7 +102,7 @@ module "cucumber_testsuite" {
       }
       runtime              = "rke2"
       container_tag        = "latest"
-      container_registry   = "registry.opensuse.org/systemsmanagement/uyuni/master/containerfile"
+      container_registry   = "registry.opensuse.org/systemsmanagement/uyuni/master/containerfile/uyuni"
       helm_chart_name      = "proxy-helm"
       helm_chart_url       = "oci://registry.opensuse.org/systemsmanagement/uyuni/master/charts/uyuni"
     }
