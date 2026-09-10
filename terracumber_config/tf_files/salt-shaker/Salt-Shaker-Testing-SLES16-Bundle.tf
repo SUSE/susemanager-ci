fb7c9fc0c50c9dcc7642d503e0bb7b5df5767fa2
Marek Czernek <marek.czernek@suse.com>
2026-09-10 09:40:33 +0200

    Update all Salt Shaker jobs before executing tests

--- terracumber_config/tf_files/salt-shaker/Salt-Shaker-Testing-SLES16-Bundle.tf
+++ terracumber_config/tf_files/salt-shaker/Salt-Shaker-Testing-SLES16-Bundle.tf
@@ -102,6 +102,7 @@ module "base" {
 }
 
 module "salt-shaker-products-testing" {
+  use_os_released_updates = true
   source             = "./modules/salt_testenv"
   base_configuration = module.base.configuration
 
